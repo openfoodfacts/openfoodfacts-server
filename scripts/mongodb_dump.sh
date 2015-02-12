dc8d506d7bd1cb787ae0a019125c95f219b38934
Florentin Raud <florentin.raud@gmail.com>
2015-02-12 13:05:51 +0000

    Adding checksum to the db dump

--- scripts/mongodb_dump.sh
+++ scripts/mongodb_dump.sh
@@ -3,3 +3,5 @@
 cd /data/off-fr/html
 mongodump --collection products --db off-fr
 tar cvfz data/openfoodfacts-mongodbdump.tar.gz dump
+sha256sum data/openfoodfacts-mongodbdump.tar.gz > data/sha256sum
+md5sum data/openfoodfacts-mongodbdump.tar.gz > data/md5sum
