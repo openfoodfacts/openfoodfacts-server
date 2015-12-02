025d57f6d26171ce84c50ba0c5683c5a96bd16d1
Stephane Gigandet <stephane@openfoodfacts.org>
2015-12-02 16:56:17 +0100

    add more admins

--- cgi/Display.pm
+++ cgi/Display.pm
@@ -260,6 +260,7 @@ sub init()
 	}
 	
 	if (($User_id eq 'stephane') or ($User_id eq 'tacite') or ($User_id eq 'teolemon') or ($User_id eq 'bcatelin')
+		or ($User_id eq 'twoflower') or ($User_id eq 'hangy') or ($User_id eq 'javichu') or ($User_id eq 'segundo') 
 		or ($User_id eq 'tacinte') or ($User_id eq 'kyzh') or ($User_id eq 'sebleouf')) {
 		$admin = 1;
 	}
