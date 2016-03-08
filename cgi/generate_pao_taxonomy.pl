594807f3232c32935871f768916f159c73e31503
Stephane Gigandet <stephane@openfoodfacts.org>
2016-03-08 14:31:24 +0100

    generate SVG logos

--- cgi/generate_pao_taxonomy.pl
+++ cgi/generate_pao_taxonomy.pl
@@ -2,6 +2,10 @@
 
 use strict;
 
+open (IN, "<periods_after_opening_logo.svg") or die ;
+my $svg = join ("",(<IN>));
+close(IN);
+
 my $entry_format = <<TXT
 en:<i> months, <i>months, <i> M, <i>M, <i>
 fr:<i> mois, <i>mois
@@ -9,9 +13,23 @@ numeric_number_of_months_after_opening:en:<i>
 TXT
 ;
 
+my $image_path = "/home/obf/html/images/lang/en/periods_after_opening";
+
 for (my $i = 1; $i <= 4 * 12; $i++) {
 
 	my $entry = $entry_format;
 	$entry =~ s/<i>/$i/g;
 	print $entry . "\n";
+	
+	# generate SVG logo
+	my $isvg = $svg;
+	$isvg =~ s/49 M/$i M/;
+	
+	my $file = "$image_path/$i-months.90x90.svg";
+	$file =~ s/^1-months/1-month/;
+	
+	open (OUT, "> $file") or die;
+	print OUT $isvg;
+	close OUT;
+	
 }
