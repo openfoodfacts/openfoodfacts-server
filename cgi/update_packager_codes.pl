b04ccf4e0ce3be1db70dd2e7b3764d4570f4e9d9
hangy <hangy@hangy.de>
2016-07-03 15:19:54 +0200

    Added DE packager codes from https://github.com/openfoodfacts/eu-food-data/blob/master/de/export.csv

--- cgi/update_packager_codes.pl
+++ cgi/update_packager_codes.pl
@@ -114,6 +114,10 @@ if (opendir (DH, "$data_root/packager-codes")) {
 					$code = $fields[$headers{n_rgseaa}];
 					$code = normalize_packager_codes("ES $code CE");
 				}
+				elsif ($country eq 'de') {
+					$code = $fields[$headers{code}];
+					$code = normalize_packager_codes("DE $code EC");
+				}
 				
 				$code = get_fileid($code);
 				$code =~ s/-(eg|ce|ew|we|eec)$/-ec/i;
