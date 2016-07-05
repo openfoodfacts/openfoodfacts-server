aeea530c8befb72ea3d05d01e4a48439aa3a71c2
Stephane Gigandet <stephane@openfoodfacts.org>
2016-07-05 13:48:30 +0200

    special startup file to load modules for Apache 2.4

---
+++ cgi/startup_apache2.pl
@@ -0,0 +1,96 @@
+# This file is part of Product Opener.
+# 
+# Product Opener
+# Copyright (C) 2011-2015 Association Open Food Facts
+# Contact: contact@openfoodfacts.org
+# Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
+# 
+# Product Opener is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# startup file for preloading modules into Apache/mod_perl when the server starts
+# (instead of when each httpd child starts)
+# see http://apache.perl.org/docs/1.0/guide/performance.html#Code_Profiling_Techniques
+#
+use strict;
+
+use Carp ();
+
+eval { Carp::confess("init") };
+
+# used for debugging hanging httpd processes
+# http://perl.apache.org/docs/1.0/guide/debug.html#Detecting_hanging_processes
+$SIG{'USR2'} = sub { 
+   Carp::confess("caught SIGUSR2!");
+};
+
+use CGI ();
+CGI->compile(':all');
+
+use Storable ();
+use LWP::Simple ();
+use LWP::UserAgent ();
+use Image::Magick ();
+use File::Copy ();
+use XML::Encoding ();
+use Encode ();
+use Cache::Memcached::Fast ();
+use URI::Escape::XS ();
+
+# Needs to be configured
+use lib "/home/obf/cgi/";
+
+use Blogs::Lang qw/:all/;
+
+use Blogs::Store qw/:all/;
+use Blogs::Config qw/:all/;
+use Blogs::Display qw/:all/;
+use Blogs::Products qw/:all/;
+use Blogs::Food qw/:all/;
+use Blogs::Images qw/:all/;
+use Blogs::Index qw/:all/;
+use Blogs::Version qw/:all/;
+
+use Apache2::Const -compile => qw(OK);
+use Apache2::Connection ();
+use Apache2::RequestRec ();
+use APR::Table ();
+
+
+$Apache::Registry::NameWithVirtualHost = 0; 
+
+sub My::ProxyRemoteAddr ($) {
+  my $r = shift;
+
+  # we'll only look at the X-Forwarded-For header if the requests
+  # comes from our proxy at localhost
+  return Apache2::Const::OK
+      unless (($r->useragent_ip eq "127.0.0.1") 
+	or 1	# all IPs
+)
+          and $r->headers_in->get('X-Forwarded-For');
+
+  # Select last value in the chain -- original client's ip
+  if (my ($ip) = $r->headers_in->get('X-Forwarded-For') =~ /([^,\s]+)$/) {
+    $r->useragent_ip($ip);
+  }
+
+  return Apache2::Const::OK;
+}
+
+print STDERR "version: $Blogs::Version::version\n";
+
+open (*STDERR,'>',"/$data_root/logs/modperl_error_log") or die ($!);
+
+
+1;
