865f8bdb2d15f0ee1c178ba86266b774bc8395a4
hangy <hangy@hangy.de>
2016-10-08 19:20:35 +0200

    Recreated SVG version of the "Made Near Me" favicon and added auto-generated favicons from http://realfavicongenerator.net/
    Fixes openfoodfacts/openfoodfacts-server#357

--- cgi/madenearyou.pl
+++ cgi/madenearyou.pl
@@ -90,6 +90,14 @@ $initjs
 <meta property="og:description" content="Carte interactive des lieux de production des produits alimentaires"/>
 <meta name="description" content="Carte interactive des lieux de production des produits alimentaires" />
 
+<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon-madenearme/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="/images/favicon-madenearme/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/images/favicon-madenearme/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/images/favicon-madenearme/manifest.json">
+<link rel="mask-icon" href="/images/favicon-madenearme/safari-pinned-tab.svg" color="#003380">
+<link rel="shortcut icon" href="/images/favicon-madenearme/favicon.ico">
+<meta name="msapplication-config" content="/images/favicon-madenearme/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
 
 <style type="text/css" media="all">
 
