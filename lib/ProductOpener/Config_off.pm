d072634e2cd7fdac0a1e06ab6be408eaf0c96cab
Stephane Gigandet <stephane@openfoodfacts.org>
2017-07-11 13:27:23 +0200

    add warnings below the Nutri-score when fruits/vegetables are not known or estimated

--- lib/ProductOpener/Config_off.pm
+++ lib/ProductOpener/Config_off.pm
@@ -250,7 +250,7 @@ XML
 
 # fields for which we will load taxonomies
 
-@taxonomy_fields = qw(states countries languages labels categories additives additives_classes allergens traces nutrient_levels );
+@taxonomy_fields = qw(states countries languages labels categories additives additives_classes allergens traces nutrient_levels misc);
 
 
 # fields in product edit form
