1a615d4d06a010eb0ed7daf41c4be99ac5db2358
hangy <hangy@hangy.de>
2017-09-27 11:39:46 +0200

    Fix compilation errors.

--- cgi/product_multilingual.pl
+++ cgi/product_multilingual.pl
@@ -1892,7 +1892,7 @@ elsif ($action eq 'process') {
 	
 	if (defined $product_ref->{server}) {
 		# product that was moved to OBF from OFF etc.
-		my $product_url = "https://" . $subdomain . "." . $options{other_servers}{$product_ref->{server}}{domain} . $product_url;
+		my $product_url = "https://" . $subdomain . "." . $options{other_servers}{$product_ref->{server}}{domain} . product_url($product_ref);;
 		$html .= "<p>" . lang("product_changes_saved") . "</p><p>&rarr; <a href=\"" . $product_url . "\">"
 			. lang("see_product_page") . "</a></p>";
 	}
