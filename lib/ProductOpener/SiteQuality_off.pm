fb61d57c24f6d7f51bacb84520f07e37ac8cf5be
Stephane Gigandet <stephane@openfoodfacts.org>
2017-11-13 14:45:22 +0100

    update header info

--- lib/ProductOpener/SiteQuality_off.pm
+++ lib/ProductOpener/SiteQuality_off.pm
@@ -1,13 +1,24 @@
-﻿package ProductOpener::SiteQuality;
+﻿# This file is part of Product Opener.
+# 
+# Product Opener
+# Copyright (C) 2011-2017 Association Open Food Facts
+# Contact: contact@openfoodfacts.org
+# Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
+# 
+# Product Opener is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-######################################################################
-#
-#	Package	SiteQuality
-#
-#	Author:	Stephane Gigandet
-#	Date:	23/01/2015
-#
-######################################################################
+package ProductOpener::SiteQuality;
 
 use utf8;
 use Modern::Perl '2012';
