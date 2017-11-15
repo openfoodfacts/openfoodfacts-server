33e7a3af426c20d447b60e9764e47aa96fb736d6
hangy <hangy@hangy.de>
2017-11-15 17:55:39 +0100

    Add additional symlink for taxonomies Add tmpfs mount for $data_root/tmp Try to enforce LF for po-foreground.sh

--- docker/backend-git/conf/po-foreground.sh
+++ docker/backend-git/conf/po-foreground.sh
@@ -5,6 +5,7 @@ mkdir -p /mnt/podata/users
 ln -s /opt/product-opener/lang /mnt/podata/lang
 ln -s /opt/product-opener/po /mnt/podata/po
 ln -s /opt/product-opener/po/openfoodfacts /opt/product-opener/po/site-specific
+ln -s /opt/product-opener/taxonomies /mnt/podata/taxonomies
 perl -I/opt/product-opener/lib /opt/product-opener/scripts/build_lang.pl
 chown -R daemon:daemon /mnt/podata
 /usr/local/bin/httpd-foreground
