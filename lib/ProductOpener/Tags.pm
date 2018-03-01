d06ef587fa94046739548207d08db6ad843ecbf5
hangy <hangy@hangy.de>
2018-03-01 10:52:54 +0100

    Enable canonicalization for wikidata keys.
    For example, if the user enters wikidata:en:Q470974, we'll convert it to the known tag fr:fitou.

--- lib/ProductOpener/Tags.pm
+++ lib/ProductOpener/Tags.pm
@@ -2007,6 +2007,27 @@ sub canonicalize_taxonomy_tag($$$)
 	$tag =~ s/^ //g;
 	$tag =~ s/ $//g;		
 	
+	if (($tag =~ /^(\w+:\w\w):(.+)/) and (defined $properties{$tagtype})) {
+		# Test for linked data, ie. wikidata:en:Q1234
+		my $property_key = $1;
+		my $property_value = $2;
+		my $matched_tagid;
+		foreach my $canon_tagid (keys %{$properties{$tagtype}}) {
+			if ((defined $properties{$tagtype}{$canon_tagid}{$property_key}) and ($properties{$tagtype}{$canon_tagid}{$property_key} eq $property_value)) {
+				if (defined $matched_tagid) {
+					# Bail out on multiple matches for a single tag.
+					undef $matched_tagid;
+					last;
+				}
+				
+				$matched_tagid = $canon_tagid;
+			}
+		}
+
+		if (defined $matched_tagid) {
+			return $matched_tagid;
+		}
+	}
 		
 	if ($tag =~ /^(\w\w):/) {
 		$tag_lc = $1;
--- t/tags.t
+++ t/tags.t
@@ -26,4 +26,7 @@ ok( !has_tag($product_ref, 'test', 'de:mein-tag'), 'has_tag should be false afte
 add_tag($product_ref, 'nexist', 'en:test');
 ok( has_tag($product_ref, 'nexist', 'en:test'), 'has_tag should be true after add' );
 
+# verify known Wikidata ID is converted to the taxonomy tag
+is( canonicalize_taxonomy_tag('en', 'categories', 'wikidata:en:Q470974'), 'fr:fitou', '"wikidata:en:Q470974" should be canonicalized to "fr:fitou"' );
+
 done_testing();
