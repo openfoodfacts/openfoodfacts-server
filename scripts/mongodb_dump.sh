29b154ae7e94e41f5bba96e7af9cac586f611dac
hangy <hangy@hangy.de>
2018-03-15 20:58:43 +0100

    Update mongodb_dump.sh
    Add delta export for the products collection.

--- scripts/mongodb_dump.sh
+++ scripts/mongodb_dump.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
 cd /home/off/html
+
 mongodump --collection products --db off
 tar cvfz data/openfoodfacts-mongodbdump.tar.gz dump
 pushd data/ > /dev/null
 sha256sum openfoodfacts-mongodbdump.tar.gz > sha256sum
 md5sum openfoodfacts-mongodbdump.tar.gz > md5sum
+
+# Export delta of products modified in the last 24h or since last run of the script
+TSFILE='last_delta_export.txt'
+LASTTS=-1
+if [ -f $TSFILE ]; then
+  typeset -i LASTTS=$(cat $TSFILE)
+else
+  LASTTS=$(($(date +%s)-24*60*60))
+fi
+
+NEWTS=$(date +%s)
+mongoexport --collection products --db off --query "{ last_modified_t: { \$gt: $LASTTS, \$lte: $NEWTS } }" | gzip -9 > products_delta.json.gz
+echo $NEWTS > $TSFILE
+
 popd > /dev/null
