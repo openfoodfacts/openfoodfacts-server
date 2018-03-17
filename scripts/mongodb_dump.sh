ffdbb5bd648ffd789719156942709383afac55da
hangy <hangy@hangy.de>
2018-03-17 10:11:23 +0100

    Update mongodb_dump.sh
    - Output delta files in a separate directory (`data/delta`),
    - Add timestamp range to delta files, so that consumers know - from the filename - which data is contained,
    - Add `index.txt` files, which contains all `product_*.json.gz` files in the `data/delta` directory - for discoverability,
    - Automatically delete all but the last 14 delta files.

--- scripts/mongodb_dump.sh
+++ scripts/mongodb_dump.sh
@@ -9,6 +9,9 @@ sha256sum openfoodfacts-mongodbdump.tar.gz > sha256sum
 md5sum openfoodfacts-mongodbdump.tar.gz > md5sum
 
 # Export delta of products modified in the last 24h or since last run of the script
+mkdir -p delta
+pushd delta/ > /dev/null
+
 TSFILE='last_delta_export.txt'
 LASTTS=-1
 if [ -f $TSFILE ]; then
@@ -16,9 +19,14 @@ if [ -f $TSFILE ]; then
 else
   LASTTS=$(($(date +%s)-24*60*60))
 fi
-
 NEWTS=$(date +%s)
-mongoexport --collection products --db off --query "{ last_modified_t: { \$gt: $LASTTS, \$lte: $NEWTS } }" | gzip -9 > products_delta.json.gz
-echo $NEWTS > $TSFILE
+mongoexport --collection products --db off --query "{ last_modified_t: { \$gt: $LASTTS, \$lte: $NEWTS } }" | gzip -9 > products_${LASTTS}_${NEWTS}.json.gz
 
-popd > /dev/null
+# Delete all but the last 14 delta files - https://stackoverflow.com/a/34862475/11963
+ls -tp products_*.json.gz | grep -v '/$' | tail -n +14 | xargs -I {} rm -- {}
+
+echo $NEWTS > $TSFILE
+ls -tp products_*.json.gz > index.txt
+
+popd > /dev/null # data/delta
+popd > /dev/null # data
