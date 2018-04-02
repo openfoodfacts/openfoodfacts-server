4135cae27ee727b32a12ddf320940298d0f7b369
Stephane Gigandet <stephane@openfoodfacts.org>
2018-04-02 13:28:16 +0200

    fix for bug #1177 - language unset when ip country is gb, causing uploaded images to be rejected

--- cgi/product_multilingual.pl
+++ cgi/product_multilingual.pl
@@ -56,6 +56,11 @@ ProductOpener::Display::init();
 
 $debug = 1;
 
+if ($User_id eq 'unwanted-user-french') { 
+	display_error("<b>Il y a des problèmes avec les modifications de produits que vous avez effectuées. Ce compte est temporairement bloqué, merci de nous contacter.</b>", 403);
+}
+
+
 
 sub normalize_nutriment_value_and_modifier($$) {
 		
