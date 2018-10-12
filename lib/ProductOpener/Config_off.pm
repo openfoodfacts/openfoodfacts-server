8acf211f52036280bb1d47c7773f6dda134f7187
Stéphane Gigandet <stephane@openfoodfacts.org>
2018-10-12 22:22:29 +0200

    update paths

--- lib/ProductOpener/Config_off.pm
+++ lib/ProductOpener/Config_off.pm
@@ -711,24 +711,24 @@ $options{other_servers} = {
 obf =>
 {
 	name => "Open Beauty Facts",
-	data_root => "/home/obf",
-	www_root => "/home/obf/html",
+	data_root => "/srv/obf",
+	www_root => "/srv/obf/html",
 	mongodb => "obf",
 	domain => "openbeautyfacts.org",
 },
 off =>
 {
 	name => "Open Food Facts",
-	data_root => "/home/off",
-	www_root => "/home/off/html",
+	data_root => "/srv/off",
+	www_root => "/srv/off/html",
 	mongodb => "off",
 	domain => "openfoodfacts.org",
 },
 opf =>
 {
 	name => "Open Products Facts",
-	data_root => "/home/opf",
-	www_root => "/home/opf/html",
+	data_root => "/srv/opf",
+	www_root => "/srv/opf/html",
 	mongodb => "opf",
 	domain => "openproductsfacts.org",
 },
@@ -736,8 +736,8 @@ opff =>
 {
 	prefix => "opff",
 	name => "Open Pet Food Facts",
-	data_root => "/home/opff",
-	www_root => "/home/opff/html",
+	data_root => "/srv/opff",
+	www_root => "/srv/opff/html",
 	mongodb => "opff",
 	domain => "openpetfoodfacts.org",
 }
