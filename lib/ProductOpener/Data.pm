6ab98bedc4ba2a1efb96b99f9e8de589e8a212f6
syl10100 <syl2019@laposte.net>
2019-01-09 19:33:33 +0100

    issue-1610-improve_aggregations_perf

--- lib/ProductOpener/Data.pm
+++ lib/ProductOpener/Data.pm
@@ -31,6 +31,7 @@ BEGIN
 	@EXPORT_OK = qw(
 					&execute_query
 					&get_products_collection
+					&get_products_tags_collection
 					&get_emb_codes_collection
 					&get_recent_changes_collection
 
@@ -66,6 +67,10 @@ sub execute_query {
 
 sub get_products_collection {
 	return get_collection($mongodb, 'products');
+}
+
+sub get_products_tags_collection {
+	return get_collection($mongodb, 'products_tags');
 }
 
 sub get_emb_codes_collection {
