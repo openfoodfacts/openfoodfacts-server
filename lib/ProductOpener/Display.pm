3631b2a76e83ec621aa5c6cec2af022466c25cd4
hangy <hangy@hangy.de>
2019-02-22 22:04:37 +0100

    Remove unnecessary script tag

--- lib/ProductOpener/Display.pm
+++ lib/ProductOpener/Display.pm
@@ -5712,7 +5712,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 </script>
 
 $scripts
-<script>
 
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
