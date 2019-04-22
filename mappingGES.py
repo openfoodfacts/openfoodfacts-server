024b1ec69b136166a02b48abf74a9c9ada084a9c
Ronan <syronanm@gmail.com>
2019-04-22 18:47:15 +0200

    some refacotr of the python script

--- mappingGES.py
+++ mappingGES.py
@@ -1,13 +1,33 @@
 import csv
 import os
 
+
+PATH_TO_TEMPORARY = "ingredients_tmp.txt"
+PATH_TO_INGREDIENTS = "taxonomies/ingredients.txt"
+STRING_FOODGES_VALUE = "carbon_footprint_fr_foodges_value:fr:"
+STRING_FOODGES_INGREDIENT = "carbon_footprint_fr_foodges_ingredient:fr:"
+
+unused_mappings = []
+dict = {}
+
+
+temporary_exists = os.path.isfile(PATH_TO_TEMPORARY)
+if temporary_exists:
+	print "The temporary file already exists"
+	exit()
+
+ingredients_exists = os.path.isfile(PATH_TO_INGREDIENTS)
+if not ingredients_exists:
+        print "The ingredient file does not exist, check the path :" + PATH_TO_INGREDIENTS
+        exit()
+
 def check_next_lines(ingredients):
 	next_line_is_not_foodges = True
 	keep_lines = []
 	while next_line_is_not_foodges:
 		next_line = ingredients.readline()
 		keep_lines.append(next_line)	  
-		if "carbon_footprint_fr_foodges_value:fr:" not in next_line or "carbon_footprint_fr_foodges_ingredient:fr:" not in next_line:
+		if STRING_FOODGES_VALUE not in next_line or STRING_FOODGES_INGREDIENT not in next_line:
 			next_line_is_not_foodges = False
 	return keep_lines
 
@@ -15,40 +35,42 @@ def write_next_lines(next_lines, temporary_file):
 	size = len(next_lines)
 	for i in range(0, size-1):
 		line = next_lines[i]
-		if "carbon_footprint_fr_foodges_ingredient:fr:" in line:
+		if STRING_FOODGES_INGREDIENT in line:
 			temporary_file.write(line)
-			temporary_file.write("carbon_footprint_fr_foodges_value:fr:" + dict.pop(line.rstrip("\n"), None) + "\n")
-	temporary_file.write(next_lines[size])
-
-dict = {}
+			temporary_file.write(STRING_FOODGES_VALUE + dict.get(line.rstrip("\n")) + "\n")
+			unused_mappings.remove(line.rstrip("\n"))
+	temporary_file.write(next_lines[size-1])
 
 with open('FoodGES.csv', 'r') as csvFile:
 	reader = csv.reader(csvFile)
 	for row in reader:
 		dict[row[2]]=row[1]
+		unused_mappings.append(row[2])
 
 csvFile.close()
 
-temporary_file = open("ingredients_tmp.txt","w+")
-ingredients = file('taxonomies/ingredients.txt')
+temporary_file = open(PATH_TO_TEMPORARY,"w+")
+ingredients = file(PATH_TO_INGREDIENTS)
 
 while True:
 	line = ingredients.readline()
 	temporary_file.write(line)
 	if not line: break
-	if "carbon_footprint_fr_foodges_ingredient:fr:" in line:
+	if STRING_FOODGES_INGREDIENT in line:
 		if line.rstrip("\n") not in dict:
 			print("this mapping is not known : " + line.rstrip("\n"))
 		else:
-			temporary_file.write("carbon_footprint_fr_foodges_value:fr:" + dict.pop(line.rstrip("\n"), None) + "\n")
+			temporary_file.write(STRING_FOODGES_VALUE + dict.get(line.rstrip("\n")) + "\n")
+			unused_mappings.remove(line.rstrip("\n"))
 			next_lines = check_next_lines(ingredients)
 			write_next_lines(next_lines, temporary_file)
 
 ingredients.close()
 temporary_file.close() 
 
-os.remove("taxonomies/ingredients.txt")
-os.rename("ingredients_tmp.txt", "taxonomies/ingredients.txt")
+os.remove(PATH_TO_INGREDIENTS)
+os.rename(PATH_TO_TEMPORARY, PATH_TO_INGREDIENTS)
 
-print "This is the dictionary of unused mapping"
-print dict
+print "This is the list of unused mapping"
+print unused_mappings
+
