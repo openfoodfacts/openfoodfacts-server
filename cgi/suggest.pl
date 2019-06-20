ab4497e2eb12749d3cf62cc0403e81d3b8b2f0d1
vaiton <eyadlorenzo@gmail.com>
2019-06-20 11:56:54 +0200

    Partial fix for issue 1734: make suggest.pl return strings that contains the word.

--- cgi/suggest.pl
+++ cgi/suggest.pl
@@ -1,22 +1,22 @@
 #!/usr/bin/perl -w
 
 # This file is part of Product Opener.
-# 
+#
 # Product Opener
 # Copyright (C) 2011-2018 Association Open Food Facts
 # Contact: contact@openfoodfacts.org
 # Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
-# 
+#
 # Product Opener is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -82,7 +82,7 @@ else {
 		next if defined $just_synonyms{$tagtype}{$canon_tagid};
 		my $tag = $translations_to{$tagtype}{$canon_tagid}{$search_lc};
 		my $tagid = get_fileid($tag);
-		next if $tagid !~ /^$stringid/;
+		next if $tagid !~ /$stringid/;
 
 		if (not ($search_lc eq $original_lc)) {
 			$tag = $search_lc . ":" . $tag;
