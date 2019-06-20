550012d465f127aabe136bc575aadb454ba3be84
Pierre Slamich <teolemon@users.noreply.github.com>
2019-06-20 23:20:55 +0200

    Update lib/ProductOpener/Ingredients.pm

--- lib/ProductOpener/Ingredients.pm
+++ lib/ProductOpener/Ingredients.pm
@@ -10,7 +10,7 @@
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useinfful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
