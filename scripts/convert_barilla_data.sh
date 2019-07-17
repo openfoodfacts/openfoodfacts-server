ccfea6326029dcef544416b5b34c1b678be43b6a
Stéphane Gigandet <stephane@openfoodfacts.org>
2019-07-17 08:35:13 +0000

    update barilla scripts

--- scripts/convert_barilla_data.sh
+++ scripts/convert_barilla_data.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./convert_barilla_data.pl /srv2/off/imports/barilla/barilla_20190509.csv > /srv2/off/imports/barilla/barilla.csv
+./convert_barilla_data.pl /srv2/off/imports/barilla/barilla_20190621.csv > /srv2/off/imports/barilla/barilla_20190621_converted.csv
