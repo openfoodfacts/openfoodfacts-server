04bb08443e9709955deaf88b4f5c27e882f8653b
hangy <hangy@hangy.de>
2019-08-26 21:59:23 +0200

    ci: Compile all scripts during CI to ensure that they still work.

--- compile_cgi.js
+++ compile_cgi.js
@@ -2,26 +2,30 @@
 /*eslint no-await-in-loop: "off"*/
 /*global process*/
 
+const process = require('process');
 const util = require('util');
 const glob = util.promisify(require('glob').glob);
 const execFile = util.promisify(require('child_process').execFile);
 
 async function main() {
-  const files = await glob('cgi/*.pl');
-  for (var i = 0; i < files.length; ++i) {
-    const path = files[i];
-    console.log(`Compiling ${path}`);
-    try {
-      const {stdout, stderr} = await execFile(`perl`, ['-c', '-CS', '-Ilib', files[i]]);
-      if (stderr) {
-        console.error(stderr);
-      }
+  // 0 = node; 1 = compile_cgi.js
+  for (let arg of process.argv.slice(2)) {
+    const files = await glob(arg);
+    for (var i = 0; i < files.length; ++i) {
+      const path = files[i];
+      console.log(`Compiling ${path}`);
+      try {
+        const { stdout, stderr } = await execFile(`perl`, ['-c', '-CS', '-Ilib', files[i]]);
+        if (stderr) {
+          console.error(stderr);
+        }
 
-      console.log(stdout);
-    } catch (e) {
-      console.error(e);
-      process.exitCode = e.code;
-      throw e;
+        console.log(stdout);
+      } catch (e) {
+        console.error(e);
+        process.exitCode = e.code;
+        throw e;
+      }
     }
   }
 
