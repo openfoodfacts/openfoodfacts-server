c1d28c1c3075c734b6ea15bcf1692c02afe3be2f
Pierre Slamich <teolemon@users.noreply.github.com>
2019-09-11 11:19:44 +0200

    Create generate-cooking-duration-taxonomy.pl

---
+++ scripts/generate-cooking-duration-taxonomy.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $entry_format = <<TXT
+en:<i> minutes, <i>minutes, <i> min, <i>M, <i>
+fr:<i> minutes, <i>min
+numeric_minutes:en:<i>
+TXT
+;
+
+for (my $i = 1; $i <= 4 * 12; $i++) {
+
+	my $entry = $entry_format;
+	$entry =~ s/<i>/$i/g;
+	print $entry . "\n";
+}
