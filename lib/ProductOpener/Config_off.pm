54ceb4a24561933eb41c69f7732e6239f49288fa
Stéphane Gigandet <stephane@openfoodfacts.org>
2019-10-20 17:09:20 +0000

    import obsolete and obsolete_since_date

--- lib/ProductOpener/Config_off.pm
+++ lib/ProductOpener/Config_off.pm
@@ -690,7 +690,7 @@ data_quality data_quality_bugs data_quality_info data_quality_warnings data_qual
 $options{import_export_fields_groups} = [
 	["identification", ["code", "producer_product_id", "producer_version_id", "lc", "product_name", "generic_name",
 		"quantity_value_unit", "net_weight_value_unit", "drained_weight_value_unit", "volume_value_unit", "packaging",
-		"brands", "categories", "categories_specific", "labels", "labels_specific", "countries", "stores"]
+		"brands", "categories", "categories_specific", "labels", "labels_specific", "countries", "stores", "obsolete", "obsolete_since_date"]
 	],
 	["origins", ["origins", "origin", "manufacturing_places", "producer", "emb_codes"]
 	],
