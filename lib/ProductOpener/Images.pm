d0c1a8b7a2b2774bdfdc0b61164f336244d51638
Stéphane Gigandet <stephane@openfoodfacts.org>
2019-12-15 15:50:34 +0000

    sort images by upload time instead of imgid, fixes bug #2786

--- lib/ProductOpener/Images.pm
+++ lib/ProductOpener/Images.pm
@@ -161,17 +161,27 @@ sub display_select_crop_init($) {
 
 	defined $object_ref->{images} or $object_ref->{images} = {};
 
+	# Construct an array of images that we can sort by upload time
+	# The imgid number is incremented by 1 for each new image, but when we move images
+	# from one product to another, they might not be sorted by upload time.
+
+	my @images = ();
+
 	for (my $imgid = 1; $imgid <= ($object_ref->{max_imgid} + 5); $imgid++) {
 		if (defined $object_ref->{images}{$imgid}) {
-			my $admin_fields = '';
-			if ($User{moderator}) {
-				$admin_fields = ", uploader: '" . $object_ref->{images}{$imgid}{uploader} . "', uploaded: '" . display_date($object_ref->{images}{$imgid}{uploaded_t}) . "'";
-			}
-			$images .= <<JS
+			push @images, $imgid;
+		}
+	}
+
+	foreach my $imgid (sort { $object_ref->{images}{$a}{uploaded_t} <=> $object_ref->{images}{$b}{uploaded_t} } @images) {
+		my $admin_fields = '';
+		if ($User{moderator}) {
+			$admin_fields = ", uploader: '" . $object_ref->{images}{$imgid}{uploader} . "', uploaded: '" . display_date($object_ref->{images}{$imgid}{uploaded_t}) . "'";
+		}
+		$images .= <<JS
 {imgid: "$imgid", thumb_url: "$imgid.$thumb_size.jpg", crop_url: "$imgid.$crop_size.jpg", display_url: "$imgid.$display_size.jpg" $admin_fields},
 JS
 ;
-		}
 	}
 
 	$images =~ s/,\n?$//;
