d0aa713f49f1ff85a60dbbf8fcd7420209992ff9
hangy <hangy@hangy.de>
2020-01-05 13:32:56 +0100

    perf: Initialize %emb_codes_* on demand

--- scripts/infobot_dk_irma.pl
+++ scripts/infobot_dk_irma.pl
@@ -1,22 +1,22 @@
 #!/usr/bin/perl -w
 
 # This file is part of Product Opener.
-# 
+#
 # Product Opener
-# Copyright (C) 2011-2019 Association Open Food Facts
+# Copyright (C) 2011-2020 Association Open Food Facts
 # Contact: contact@openfoodfacts.org
 # Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
-# 
+#
 # Product Opener is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -39,12 +39,13 @@ use ProductOpener::Food qw/:all/;
 use ProductOpener::Ingredients qw/:all/;
 use ProductOpener::Images qw/:all/;
 
-
 use CGI qw/:cgi :form escapeHTML/;
 use URI::Escape::XS;
 use Storable qw/dclone/;
 use Encode;
 use JSON::PP;
+
+init_emb_codes();
 
 my %codes = ();
 
@@ -79,10 +80,10 @@ while(<$IN>) {
 	chomp;
 	@fields = split(/;/, $_);
 	my $code = $fields[$fields{Stregkode}];
-	
-	
-	#next if ($code ne '3017760002073');	
-	
+
+
+	#next if ($code ne '3017760002073');
+
 	print STDERR "product code: $code\n";
 
 
@@ -90,13 +91,13 @@ while(<$IN>) {
 
 
 	my $bot = "infobot_dk_irma - user: $botid - ";
-	
+
 	$i++;
 
 	print "$i\t$code\t$codes{$code}{n}\t" ;
-	
+
 	$bot .= "product code $code - ";
-	
+
 	foreach my $cc (sort { $countries{$b} <=> $countries{$a} } keys %countries) {
 		print "$cc:$countries{$cc} ";
 		$bot .= "$cc:$countries{$cc} ";
@@ -105,30 +106,30 @@ while(<$IN>) {
 
 	$bot .= " -- ";
 
-	
+
 	my $path = product_path($code);
-	
+
 	print  "$i - checking product $code \n";
-	
+
 	my $product_ref = retrieve_product($code);
-		
+
 	if ((defined $product_ref) and ($code ne '')) {
-		
+
 		$lc = $product_ref->{lc};
-		
-		
+
+
 		# Update
 		my $field = 'countries';
-		
+
 		my $current_countries = $product_ref->{$field};
 
 		my %existing = ();
 		foreach my $countryid (@{$product_ref->{countries_tags}}) {
 			$existing{$countryid} = 1;
 		}
-		
+
 		$bot .= " current countries: $current_countries -- adding ";
-		
+
 		foreach my $cc (sort { $countries{$b} <=> $countries{$a} } keys %countries) {
 			print "$cc:$countries{$cc} ";
 			if ($countries{$cc} ) {
@@ -139,31 +140,30 @@ while(<$IN>) {
 					$bot .= "+$country ";
 					$added_countries++;
 					$changed++;
-					
+
 					my $field = 'countries';
 					$product_ref->{$field . "_hierarchy" } = [ gen_tags_hierarchy_taxonomy($lc, $field, $product_ref->{$field}) ];
 					$product_ref->{$field . "_tags" } = [];
 					foreach my $tag (@{$product_ref->{$field . "_hierarchy" }}) {
 						push @{$product_ref->{$field . "_tags" }}, get_taxonomyid($tag);
-					}					
+					}
 				}
 			}
 		}
-		
 
-	
-		
+
+
+
 		if ($product_ref->{countries} =~ /^, /) {
 			$product_ref->{countries} = $';
 		}
-		
-		
+
 		foreach my $field (keys %add_info_to_tags) {
 			if ($product_ref->{$field} !~ /$add_info_to_tags{$field}/i) {
 				$product_ref->{$field} .= ", " . $add_info_to_tags{$field};
 				$product_ref->{$field} =~ s/^, //;
 				$changed++;
-				
+
 				if (defined $tags_fields{$field}) {
 
 					$product_ref->{$field . "_tags" } = [];
@@ -180,9 +180,9 @@ while(<$IN>) {
 								}
 							}
 						}
-					}			
-				}				
-			
+					}
+				}
+
 				if (defined $taxonomy_fields{$field}) {
 					$product_ref->{$field . "_hierarchy" } = [ gen_tags_hierarchy_taxonomy($lc, $field, $product_ref->{$field}) ];
 					$product_ref->{$field . "_tags" } = [];
@@ -190,8 +190,8 @@ while(<$IN>) {
 						push @{$product_ref->{$field . "_tags" }}, get_taxonomyid($tag);
 					}
 				}
-				
-				if (defined $hierarchy_fields{$field}) {		
+
+				if (defined $hierarchy_fields{$field}) {
 					$product_ref->{$field . "_hierarchy" } = [ gen_tags_hierarchy($field, $product_ref->{$field}) ];
 					$product_ref->{$field . "_tags" } = [];
 					foreach my $tag (@{$product_ref->{$field . "_hierarchy" }}) {
@@ -200,42 +200,42 @@ while(<$IN>) {
 						}
 					}
 				}
-			
+
 			}
-			
+
 		}
 
 
-			
+
 		my $comment = $bot . " - $bug";
-			
+
 		#if ($code eq '3033710073511') {
 		if ($changed) {
 			# notify slack
 			#  payload={"text": "A very important thing has occurred! <https://alert-system.com/alerts/1234|Click here> for details!"}
-			
+
 			# curl -X POST --data-urlencode 'payload={"channel": "#general", "username": "webhookbot", "text": "This is posted to #general and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' https://openfoodfacts.slack.com/services/hooks/incoming-webhook?token=jMDE8Fzkz9qD7uC9Lq04fbZH
-			
+
 			# my $data =  encode_json(\%response);
-			
+
 			$bot .= " -- <https://world.openfoodfacts.org/product/$code>";
-			
+
 			print "notifying slack: $bot\n";
-			
+
 			use LWP::UserAgent;
- 
+
 			my $ua = LWP::UserAgent->new;
-			 
+
 			my $server_endpoint = "https://hooks.slack.com/services/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";
-			 
+
 			# set custom HTTP request header fields
 			my $req = HTTP::Request->new(POST => $server_endpoint);
 			$req->header('content-type' => 'application/json');
-			 
+
 			# add POST data to HTTP request body
 			my $post_data = '{"channel": "#bots", "username": "infobot", "text": "' . $bot . '", "icon_emoji": ":rabbit:" }';
 			$req->content($post_data);
-			 
+
 			my $resp = $ua->request($req);
 			if ($resp->is_success) {
 				my $message = $resp->decoded_content;
@@ -245,19 +245,19 @@ while(<$IN>) {
 				print "HTTP POST error code: ", $resp->code, "\n";
 				print "HTTP POST error message: ", $resp->message, "\n";
 			}
-			
-			
-		
-		
+
+
+
+
 			$User_id = $botid;
 			store_product($product_ref, $comment);
-			
+
 			$changed_products ++;
-		}			
-		
+		}
+
 	}
-		
-	
+
+
 }
 
 
@@ -267,21 +267,21 @@ if (($changed_products > 0) and ($added_countries > 0)) {
 
 	my $msg = "\nI added $added_countries countries to $changed_products products\n\n";
 	print $msg;
-	
+
 use LWP::UserAgent;
- 
+
 			my $ua = LWP::UserAgent->new;
-			 
+
 			my $server_endpoint = "https://hooks.slack.com/services/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";
-			 
+
 			# set custom HTTP request header fields
 			my $req = HTTP::Request->new(POST => $server_endpoint);
 			$req->header('content-type' => 'application/json');
-			 
+
 			# add POST data to HTTP request body
 			my $post_data = '{"channel": "#bots", "username": "infobot", "text": "' . $msg . '", "icon_emoji": ":rabbit:" }';
 			$req->content($post_data);
-			 
+
 			my $resp = $ua->request($req);
 			if ($resp->is_success) {
 				my $message = $resp->decoded_content;
@@ -290,8 +290,8 @@ use LWP::UserAgent;
 			else {
 				print "HTTP POST error code: ", $resp->code, "\n";
 				print "HTTP POST error message: ", $resp->message, "\n";
-			}	
-	
+			}
+
 }
- 
+
 print "i :$i\n";
