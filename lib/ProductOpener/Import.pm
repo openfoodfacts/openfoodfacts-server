5723343d42ec0ba8d855e7890bd3f1f73e3e6772
Stéphane Gigandet <stephane@openfoodfacts.org>
2020-02-03 21:51:06 +0000

    select other image as front if no front image exists

--- lib/ProductOpener/Import.pm
+++ lib/ProductOpener/Import.pm
@@ -1338,12 +1338,12 @@ sub import_csv_file($) {
 
 					my $imagefield_with_lc = $imagefield;
 
-					if ($imagefield !~ /_\w\w/) {
-						$imagefield_with_lc .= "_" . $product_ref->{lc};
-					}
-
 					# image_other_url.2 -> remove the number
 					$imagefield_with_lc =~ s/\.(\d+)$//;
+
+					if ($imagefield_with_lc !~ /_\w\w/) {
+						$imagefield_with_lc .= "_" . $product_ref->{lc};
+					}
 
 					# upload the image
 					my $file = $images_ref->{$imagefield};
@@ -1386,6 +1386,12 @@ sub import_csv_file($) {
 								}
 
 							}
+						}
+						# If the image type is "other" and we don't have a front image, assign it
+						# This is in particular for producers that send us many images without specifying their type: assume the first one is the front
+						elsif (($imagefield_with_lc =~ /^other/) and (not defined $product_ref->{images}{"front_" . $product_ref->{lc}})) {
+							$log->debug("selecting front image as we don't have one", { imgid => $imgid, imagefield => $imagefield, front_imagefield => "front_" . $product_ref->{lc}}) if $log->is_debug();
+							process_image_crop($product_id, "front_" . $product_ref->{lc}, $imgid, 0, undef, undef, -1, -1, -1, -1);
 						}
 					}
 					else {
