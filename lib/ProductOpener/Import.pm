46dd8b77e3a0e695a0f4e5c55443b9fdcc883144
Stéphane Gigandet <stephane@openfoodfacts.org>
2020-02-04 09:02:14 +0000

    select first other image as front if we don't have one already

--- lib/ProductOpener/Import.pm
+++ lib/ProductOpener/Import.pm
@@ -1320,6 +1320,9 @@ sub import_csv_file($) {
 
 				my $images_ref = $images_ref->{$code};
 
+				# Keep track of the images we select so that we don't select multiple images for the same field
+				my %selected_images = ();
+
 				foreach my $imagefield (sort keys %{$images_ref}) {
 
 					$log->debug("uploading image for product", { imagefield => $imagefield, code => $code }) if $log->is_debug();
@@ -1368,6 +1371,7 @@ sub import_csv_file($) {
 							if (($imgid > 0) and ($imgid > $current_max_imgid)) {
 
 								$log->debug("assigning image imgid to imagefield_with_lc", { code => $code, imgid => $imgid, imagefield_with_lc => $imagefield_with_lc }) if $log->is_debug();
+								$selected_images{$imagefield_with_lc} = 1;
 								eval { process_image_crop($product_id, $imagefield_with_lc, $imgid, 0, undef, undef, -1, -1, -1, -1); };
 								# $modified++;
 
@@ -1381,6 +1385,7 @@ sub import_csv_file($) {
 									and (exists $product_ref->{images}{$imagefield_with_lc})
 									and ($product_ref->{images}{$imagefield_with_lc}{imgid} != $imgid)) {
 									$log->debug("re-assigning image imgid to imagefield_with_lc", { code => $code, imgid => $imgid, imagefield_with_lc => $imagefield_with_lc }) if $log->is_debug();
+									$selected_images{$imagefield_with_lc} = 1;
 									eval { process_image_crop($product_id, $imagefield_with_lc, $imgid, 0, undef, undef, -1, -1, -1, -1); };
 									# $modified++;
 								}
@@ -1389,9 +1394,12 @@ sub import_csv_file($) {
 						}
 						# If the image type is "other" and we don't have a front image, assign it
 						# This is in particular for producers that send us many images without specifying their type: assume the first one is the front
-						elsif (($imagefield_with_lc =~ /^other/) and (not defined $product_ref->{images}{"front_" . $product_ref->{lc}})) {
+						elsif (($imgid > 0) and ($imagefield_with_lc =~ /^other/) and (not defined $product_ref->{images}{"front_" . $product_ref->{lc}}) and (not defined $selected_images{"front_" . $product_ref->{lc}})) {
 							$log->debug("selecting front image as we don't have one", { imgid => $imgid, imagefield => $imagefield, front_imagefield => "front_" . $product_ref->{lc}}) if $log->is_debug();
-							process_image_crop($product_id, "front_" . $product_ref->{lc}, $imgid, 0, undef, undef, -1, -1, -1, -1);
+							# Keep track that we have selected an image, so that we don't select another one after,
+							# as we don't reload the product_ref after calling process_image_crop()
+							$selected_images{"front_" . $product_ref->{lc}} = 1;
+							eval { process_image_crop($product_id, "front_" . $product_ref->{lc}, $imgid, 0, undef, undef, -1, -1, -1, -1); };
 						}
 					}
 					else {
