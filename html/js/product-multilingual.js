0955c56a5e2c6bb5330f36e112f7c33df0f94938
Stéphane Gigandet <stephane@openfoodfacts.org>
2020-03-15 15:39:20 +0000

    updated cropper, bug #3031

--- html/js/product-multilingual.js
+++ html/js/product-multilingual.js
@@ -228,15 +228,7 @@ function upload_image (imagefield) {
 }
 
 
-function init_image_area_select(imagefield) {
 
-	$('img#crop_' + imagefield).cropper({
-		"strict": false, "guides": false, "autoCrop": false, "zoomable": false, "mouseWheelZoom": false, "touchDragZoom": false, "toggleDragModeOnDblclick": false, built: function () {
-		$('img#crop_' + imagefield ).cropper('setDragMode', "crop");
-		}
-	});
-
-}
 
 function update_image(imagefield) {
 
@@ -309,18 +301,29 @@ function change_image(imagefield, imgid) {
 	angles[imagefield] = 0;
 	imagefield_imgid[imagefield] = imgid;
 
-	var html = '<div class="command">' + lang().product_js_image_rotate_and_crop + '</div>';
+	var html = '';
+
+  html += '<div class="command">' + lang().product_js_image_rotate_and_crop + '</div>';
+
+  html += '<div class="row"><div class="small-6 medium-7 large-8 columns">';
 	html += '<div class="command"><a id="rotate_left_' + imagefield + '" class="small button" type="button">' + lang().product_js_image_rotate_left + '</a> &nbsp;';
 	html += '<a id="rotate_right_' + imagefield + '" class="small button" type="button">' + lang().product_js_image_rotate_right + '</a>';
 	html += '</div>';
-	html += '<div id="cropimgdiv_' + imagefield + '" style="width:100%;height:400px"><img src="' + img_path + image.crop_url +'" id="' + 'crop_' + imagefield + '"/></div>';
-	html += '<a href="' + img_path + image.imgid + '.jpg" target="_blank">' + lang().product_js_image_open_full_size_image + '</a><br/>';
-	html += '<input type="checkbox" id="normalize_' + imagefield + '" onchange="update_image(\'' + imagefield + '\');blur();" /><label for="normalize_' + imagefield + '">' + lang().product_js_image_normalize + '</label></div><br/>';
+  html += '</div><div class="small-6 medium-5 large-4 columns" style="float:right">';
+
+	html += '<div class="cropbutton_' + imagefield + '"></div>';
+	html += '<div class="cropbuttonmsg_' + imagefield + '" class="ui-state-highlight ui-corner-all" style="padding:2px;margin-top:10px;margin-bottom:10px;display:none" ></div>';
+  html += '</div></div>';
+	html += '<div id="cropimgdiv_' + imagefield + '" style="width:100%;height:800px"><img src="' + img_path + image.imgid +'.jpg" id="' + 'crop_' + imagefield + '"/></div>';
+
+  html += '<div class="row"><div class="small-6 medium-7 large-8 columns">';
+	html += '<input type="checkbox" id="normalize_' + imagefield + '" onchange="update_image(\'' + imagefield + '\');blur();" /><label for="normalize_' + imagefield + '">' + lang().product_js_image_normalize + '</label><br/>';
 	html +=	'<input type="checkbox" id="white_magic_' + imagefield + '" style="display:inline" /><label for="white_magic_' + imagefield
 		+ '" style="display:inline">' + lang().product_js_image_white_magic + '</label>';
-	html += '<div id="cropbutton_' + imagefield + '"></div>';
-	html += '<div id="cropbuttonmsg_' + imagefield + '" class="ui-state-highlight ui-corner-all" style="padding:2px;margin-top:10px;margin-bottom:10px;display:none" ></div>';
-
+  html += '</div><div class="small-6 medium-5 large-4 columns" style="float:right;padding-top:1rem">';
+	html += '<div class="cropbutton_' + imagefield + '"></div>';
+	html += '<div class="cropbuttonmsg_' + imagefield + '" class="ui-state-highlight ui-corner-all" style="padding:2px;margin-top:10px;margin-bottom:10px;display:none" ></div>';
+  html += '</div></div>';
 
 	if (current_cropbox) {
 		$('div[id="' + current_cropbox + '"]').html('');
@@ -330,46 +333,54 @@ function change_image(imagefield, imgid) {
 	$('div[id="cropimgdiv_' + imagefield +'"]').height($('div[id="cropimgdiv_' + imagefield +'"]').width());
 
 	$("#white_magic_" + imagefield).change(function() {
-			$('div[id="cropbuttonmsg_' + imagefield +'"]').hide();
+			$('.cropbuttonmsg_' + imagefield).hide();
 	} );
 
-			var id = 'crop_' + imagefield + '_button';
-			$('div[id="cropbutton_' + imagefield +'"]').html('<button id="' + id + '" class="small button" type="button">' + lang().product_js_image_save + '</button>');
-			$("#" + id).click({imagefield:imagefield},function(event) {
-				event.stopPropagation();
-				event.preventDefault();
-				var imgid = imagefield_imgid[imagefield];
+  var crop_button = 'crop_' + imagefield + '_button';
+  $('.cropbutton_' + imagefield).html('<button class="' + crop_button + ' small button" type="button">' + lang().product_js_image_save + '</button>');
+  $("." + crop_button).click({imagefield:imagefield},function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    var imgid = imagefield_imgid[imagefield];
 
-				var selection = $('img#crop_' + imagefield ).cropper('getData');
+    var selection = $('img#crop_' + imagefield ).cropper('getData');
 
-				if (! selection) {
-					selection = {'x1':-1,'y1':-1,'x2':-1,'y2':-1};
-				}
-				// alert(event.data.imagefield);
-				$("#" + id).blur();
-				$('div[id="cropbutton_' + imagefield +'"]').hide();
-				$('div[id="cropbuttonmsg_' + imagefield +'"]').html('<img src="/images/misc/loading2.gif" /> ' + lang().product_js_image_saving);
-				$('div[id="cropbuttonmsg_' + imagefield +'"]').show();
-				$.post('/cgi/product_image_crop.pl',
-			{
-				code: code, id: imagefield, imgid: imgid,
-						x1:selection.x, y1:selection.y, x2:selection.x + selection.width, y2:selection.y + selection.height,
-						angle:angles[imagefield], normalize:$("#normalize_" + imagefield).prop('checked'),
-				white_magic: $("#white_magic_" + imagefield).prop('checked')
-			}, function (data) {
+    if (! selection) {
+      selection = {'x1':-1,'y1':-1,'x2':-1,'y2':-1};
+    }
+    // alert(event.data.imagefield);
+    $("." + crop_button).blur();
+    $('.cropbutton_' + imagefield).hide();
+    $('.cropbuttonmsg_' + imagefield).html('<img src="/images/misc/loading2.gif" /> ' + lang().product_js_image_saving);
+    $('.cropbuttonmsg_' + imagefield).show();
+    $.post('/cgi/product_image_crop.pl',
+  {
+    code: code, id: imagefield, imgid: imgid,
+        x1:selection.x, y1:selection.y, x2:selection.x + selection.width, y2:selection.y + selection.height,
+        angle:angles[imagefield], normalize:$("#normalize_" + imagefield).prop('checked'),
+    white_magic: $("#white_magic_" + imagefield).prop('checked')
+  }, function (data) {
 
-					imagefield_url[imagefield] = data.image.display_url;
-					update_display(imagefield, false);
-					$('div[id="cropbutton_' + imagefield +'"]').show();
-					$('div[id="cropbuttonmsg_' + imagefield +'"]').html(lang().product_js_image_saved);
-					$(document).foundation('equalizer', 'reflow');
-				}, 'json');
-			});
+      imagefield_url[imagefield] = data.image.display_url;
+      update_display(imagefield, false);
+      $('.cropbutton_' + imagefield).show();
+      $('.cropbuttonmsg_' + imagefield).html(lang().product_js_image_saved);
+      $(document).foundation('equalizer', 'reflow');
+    }, 'json');
+  });
 
 	$("#rotate_left_" + imagefield).click({imagefield:imagefield, angle:-90}, rotate_image);
 	$("#rotate_right_" + imagefield).click({imagefield:imagefield, angle:90}, rotate_image);
 
-	init_image_area_select(imagefield);
+  $('img#crop_' + imagefield).click(function() {
+    $('img#crop_' + imagefield ).cropper('clear');
+  });
+
+	$('img#crop_' + imagefield).cropper({
+		"viewMode" : 2, "guides": false, "autoCrop": false, "zoomable": true, "zoomOnWheel": true, "zoomOnTouch": true, "toggleDragModeOnDblclick": false, built: function () {
+		$('img#crop_' + imagefield ).cropper('setDragMode', "crop");
+		}
+	});
 
 	$(document).foundation('equalizer', 'reflow');
 }
@@ -411,6 +422,12 @@ function update_display(imagefield, first_display) {
 				$('#nutrition_image_copy').html('<img src="' + img_path + display_url + '" />').css("left", $('#nutrition_data_table').width() + 10);
 			}
 		}
+	}
+
+	if (stringStartsWith(imagefield, 'ingredients')) {
+
+    var full_url = display_url.replace(/\.400\./, ".full.");
+    $('#ingredients_image_full').html('<img src="' + img_path + full_url + '" />');
 	}
 
 	$('div[id="display_' + imagefield +'"]').html(html);
@@ -705,13 +722,15 @@ function get_recents(tagfield) {
 + '</div>';
 
 
-			html += '<div class="cropbox" id="cropbox_' + id +'"></div>';
-			html += '<div class="display" id="display_' + id +'"></div>';
-
-
+      html += '<div class="row">';
+			html += '<div class="columns small-12 medium-12 large-6 xlarge-8"><div class="cropbox" id="cropbox_' + id +'"></div></div>';
+			html += '<div class="columns small-12 medium-12 large-6 xlarge-4"><div class="display" id="display_' + id +'"></div></div>';
+      html += '</div>';
 			}
 
 			$this.html(html);
+
+      $(document).foundation('equalizer', 'reflow');
 
 			if (! stringStartsWith(id, 'manage')) {
 
