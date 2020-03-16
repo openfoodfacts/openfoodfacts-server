eaad40905e37e953e6d515bd5be93c2df11258bb
areeshatariq <areeshatariq02@gmail.com>
2020-03-16 20:23:14 +0500

    Document the ProductOpener::URL.pm module
     Documentation in perlpod format

--- lib/ProductOpener/URL.pm
+++ lib/ProductOpener/URL.pm
@@ -18,6 +18,30 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+=head1 NAME
+
+ProductOpener::URL - generates the URL of the product
+
+=head1 SYNOPSIS
+
+C<ProductOpener::URL> is used to generate a URL of the product according to the subdomain .
+
+	use ProductOpener::URL qw/:all/;
+
+	my $image = "$www_root/images/products/$path/$filename.full.jpg";
+	my $image_url = format_subdomain('static') . "/images/products/$path/$filename.full.jpg";
+	
+	# subdomain format:
+	# [2 letters country code or "world" or "static"]
+
+=head1 DESCRIPTION
+
+The module implements the URL generation 
+on the basis of subdomain format which can be country code, world or static and on the basis of subdomain's support for https or http.
+
+=cut
+
 package ProductOpener::URL;
 
 use utf8;
@@ -42,6 +66,22 @@ use experimental 'smartmatch';
 
 use ProductOpener::Config qw/:all/;
 
+=head1 FUNCTIONS
+
+=head2 format_subdomain( SUBDOMAIN )
+
+C<format_subdomain()> returns URL on the basis of subdomain and scheme (http/https)
+
+=head3 Arguments
+
+A scalar variable needs to be passed as an argument. 
+
+=head3 Return Values
+
+The function returns a URL by concatenating scheme, subdomain and server-domain.
+
+=cut
+
 sub format_subdomain {
 	
 	my ($sd) = @_;
@@ -58,6 +98,18 @@ sub format_subdomain {
 	return $scheme . '://' . $sd . '.' . $server_domain;
 	
 }
+
+=head2 subdomain_supports_https( SUBDOMAIN )
+
+C<subdomain_supports_https()> determines if the subdomain supports https.
+
+=head3 Arguments
+
+A scalar variable needs to be passed as an argument. 
+
+=head3 Return Values
+
+The function returns true after evaluating the true value for the regular expression using grep or subdomain, ssl_subdomains
 
 sub subdomain_supports_https {
 
