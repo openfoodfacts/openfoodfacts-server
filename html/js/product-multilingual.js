201a58c467dd7488a07172f92a433d2cf69e7309
hangy <hangy@hangy.de>
2020-03-22 11:25:29 +0100

    fix: Disable undefined warning
    https://eslint.org/docs/rules/no-undefined

--- html/js/product-multilingual.js
+++ html/js/product-multilingual.js
@@ -1,7 +1,7 @@
 // This file is part of Product Opener.
 //
 // Product Opener
-// Copyright (C) 2011-2019 Association Open Food Facts
+// Copyright (C) 2011-2020 Association Open Food Facts
 // Contact: contact@openfoodfacts.org
 // Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
 //
@@ -620,8 +620,9 @@ function get_recents(tagfield) {
 	}
 
 	if (
-		obj !== null &&
-		obj[tagfield] !== undefined &&
+	obj !== null &&
+	// eslint-disable-next-line no-undefined
+	obj[tagfield] !== undefined &&
 		obj[tagfield] !== null
 	) {
 		return obj[tagfield];
