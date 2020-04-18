440b9fd82d9e36a302adc247606cbbd8b95c078f
Pierre Slamich <pierre@openfoodfacts.org>
2020-04-18 13:19:02 +0200

    improve cutting
    https://static.openfoodfacts.org/images/products/351/269/000/4987/ingredients_fr.10.full.jpg

--- lib/ProductOpener/Ingredients.pm
+++ lib/ProductOpener/Ingredients.pm
@@ -2327,6 +2327,7 @@ fr => [
 'conservation:',
 '(a|à) protéger de ', # humidité, chaleur, lumière etc.
 'conditionn(e|é) sous atmosph(e|è)re protectrice',
+'produit conditionn(e|é) sous atmosph(e|è)re protectrice',
 'la pr(e|é)sence de vide',	# La présence de vide au fond du pot est due au procédé de fabrication.
 '(a|à) consommer (cuit|rapidement|dans|jusqu)',
 '(a|à) conserver (dans|de|a|à)',
