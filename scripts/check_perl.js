dd7f30b2b22806f650236a159bfef8294d7e1470
hangy <hangy@hangy.de>
2020-05-05 22:56:29 +0200

    fix: Run perl compilers in sequence instead of allowing parallel exeuction, since this requires too much memory and cpu in most CI scenarios

--- scripts/check_perl.js
+++ scripts/check_perl.js
@@ -6,26 +6,33 @@ const util = require("util");
 const glob = util.promisify(require("glob").glob);
 const { spawn } = require("child_process");
 
+function checkFile(path, doneCallback) {
+  console.log(`Checking ${path}`);
+  try {
+    const child = spawn(`perl`, ["-c", "-CS", "-Ilib", path]);
+    child.stdout.on("data", (data) => console.log("[" + path + "] " + data));
+    child.stderr.on("data", (data) => console.error("[" + path + "] " + data));
+    child.on("close", (code) => {
+      console.log("[" + path + "] result: " + code);
+      process.exitCode = code;
+      doneCallback();
+    });
+  } catch (e) {
+    console.error("[" + path + "] " + e);
+    process.exitCode = e.code;
+    throw e;
+  }
+}
+
+const check = util.promisify(checkFile);
+
 async function main() {
   // 0 = node; 1 = check_perl.js
   for (const arg of process.argv.slice(2)) {
     const files = await glob(arg);
     for (var i = 0; i < files.length; ++i) {
       const path = files[i];
-      console.log(`Checking ${path}`);
-      try {
-        const child = spawn(`perl`, ["-c", "-CS", "-Ilib", path]);
-        child.stdout.on("data", (data) => console.log("[" + path + "] " + data));
-        child.stderr.on("data", (data) => console.error("[" + path + "] " + data));
-        child.on("close", (code) => {
-          console.log("[" + path + "] result: " + code);
-          process.exitCode = code;
-        });
-      } catch (e) {
-        console.error("[" + path + "] " + e);
-        process.exitCode = e.code;
-        throw e;
-      }
+      await check(path);
     }
   }
 
