c6180b8fe5d2e3ab9554fae50887df4a9f44e3b9
Alexandre Marty <alexandre.marty@datapred.com>
2020-05-27 20:16:49 +0200

    Added quick and dirty main

--- scripts/packager-codes/non-eu/pdf_extraction.py
+++ scripts/packager-codes/non-eu/pdf_extraction.py
@@ -7,15 +7,20 @@ def get_bbox(pdf_obj):
 
 
 def extract_page(page: Page):
+    if len(page.curves) < 2:
+        raise ValueError(
+            "PDF page contains less than 2 curves, formatting may have changed"
+        )
+
     # Find table bounding box
     min_x0 = min(c["x0"] for c in page.curves)
     max_x1 = max(c["x1"] for c in page.curves)
     min_top = min(c["top"] for c in page.curves)
     max_bottom = max(c["bottom"] for c in page.curves)
     table_bbox = (min_x0, min_top, max_x1, max_bottom)
+    table_page = page.crop(table_bbox)
 
     # Get table lines
-    table_page = page.crop(table_bbox)
     vertical_lines_x = sorted(set(p[0] for p in table_page.curves[0]["points"]))
     vertical_lines_x = [table_page.curves[1]["x0"]] + vertical_lines_x + [
         table_page.curves[1]["x1"]]
@@ -32,3 +37,34 @@ def extract_page(page: Page):
         }
     )
 
+
+table_header = [
+    "Approval number",
+    "Name",
+    "City",
+    "Regions",
+    "Activities",
+    "Remark",
+    "Date of request",
+]
+
+
+def extract_doc(pdf):
+    doc_table = [table_header]
+    for page in pdf.pages:
+        table = extract_page(page)
+        if table[0] != table_header:
+            raise ValueError("invalid table header")
+        doc_table.extend(table[1:])
+    return doc_table
+
+
+def main():
+    import sys
+    with pdfplumber.open(sys.argv[1]) as pdf:
+        table = extract_doc(pdf)
+        print("\n".join(["\t".join(l) for l in table]))
+
+
+if __name__ == "__main__":
+    main()
