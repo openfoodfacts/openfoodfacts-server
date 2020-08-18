0c58b88f5bd0e866d1d2f4d51d5e4ca2bc2328fe
areeshatariq <areeshatariq02@gmail.com>
2020-08-18 15:23:56 +0500

    Bug fix

--- lib/ProductOpener/Display.pm
+++ lib/ProductOpener/Display.pm
@@ -7461,7 +7461,7 @@ CSS
 	if ($code =~ /^2000/) { # internal code
 	}
 	else {
-		$template_data_ref->{upc} = 'defined';
+		$template_data_ref->{upc_code} = 'defined';
 		# Also display UPC code if the EAN starts with 0
 		my $upc = "";
 		if (length($code) == 13) {
