8c26424eca7afb9679e3257a3424e09aa0f093bd
hangy <hangy@hangy.de>
2020-08-20 10:06:46 +0200

    chore: Remove unused sub

--- lib/ProductOpener/Cache.pm
+++ lib/ProductOpener/Cache.pm
@@ -28,8 +28,6 @@ BEGIN
 {
 	use vars       qw(@ISA @EXPORT_OK %EXPORT_TAGS);
 	@EXPORT_OK = qw(
-		&get_multi_objects
-
 		$memd
 		);    # symbols to export on request
 	%EXPORT_TAGS = (all => [@EXPORT_OK]);
@@ -51,56 +49,5 @@ $memd = Cache::Memcached::Fast->new(
 		'utf8'    => 1,
 	}
 );
-
-# Initialize internal variables
-# - using my $variable; is causing problems with mod_perl, it looks
-# like inside subroutines below, they retain the first value they were
-# called with. (but no "$variable will not stay shared" warning).
-# Converting them to global variables.
-# - better solution: create a class?
-
-use vars qw();
-
-sub get_multi_objects($)
-{
-	my $keys_ref = shift;
-	my $values_ref = $memd->get_multi(keys %{$keys_ref});
-
-	foreach my $key (keys %{$keys_ref}) {
-		if (not defined $values_ref->{$key}) {
-			if ($key =~ /\/blogs\/(.*)$/) {
-				my $blog_ref = retrieve("$data_root/index/blogs/$1/blog.sto");
-				if (defined $blog_ref) {
-					$values_ref->{$key} = {
-						title => $blog_ref->{title},
-						color => $blog_ref->{color2},
-						url => $blog_ref->{url}
-					};
-					$log->debug("Cache::get_multi_objects - retrieved from disk", { key => $key }) if $log->is_debug();
-					$memd->set($key, $values_ref->{$key});
-				}
-			}
-			elsif ($key =~ /\/tags\/(.*)$/) {
-				my $tag_ref = retrieve("$data_root/index/tags/$1/tag.sto");
-				if ( defined $tag_ref ) {
-					$values_ref->{$key} = {
-						canon_tag => $tag_ref->{canon_tag},
-						color     => $tag_ref->{color2},
-					};
-					$log->debug(
-						"Cache::get_multi_objects - retrieved from disk",
-						{ key => $key } )
-						if $log->is_debug();
-					$memd->set( $key, $values_ref->{$key} );
-				}
-			}
-		}
-		else {
-			$log->debug("Cache::get_multi_objects - in cache", { key => $key }) if $log->is_debug();
-		}
-	}
-	
-	return $values_ref;
-}
 
 1;
