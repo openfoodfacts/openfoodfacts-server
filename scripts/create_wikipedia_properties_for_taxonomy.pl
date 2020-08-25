ee0d8fa09cc6155c3cdee44b22c2e0a7e849e307
hangy <hangy@hangy.de>
2020-08-25 17:50:40 +0200

    perlcritic: Close filehandles as soon as possible after opening them
    See page 209 of PBP.  (Severity: 4)
    
    Also resolved some issues at the 'brutal' level.

--- scripts/create_wikipedia_properties_for_taxonomy.pl
+++ scripts/create_wikipedia_properties_for_taxonomy.pl
@@ -23,110 +23,105 @@
 use Modern::Perl '2017';
 use utf8;
 
+use Carp ();
+
 use ProductOpener::Store qw/:all/;
 use ProductOpener::Config qw/:all/;
 
-# tmx files contain translations from the EU laws
+sub _parse_wikidata {
+	my ($in) = @_;
 
+	my %properties = ();
+
+	#wikidata:en:Q1640437
+	#description:bg:
+	#description:es:
+	#description:cs:
+	#description:da:
+	#de:Hydroxypropylstärke
+	#wikipedia_url:de:https://de.wikipedia.org/wiki/Hydroxypropylstärke
+	#description:de:Hydroxypropylstärke (HPS) ist eine modifizierte Stärke, die als Lebensmittelzusatzstoff mit der Bezeichnung E 1440 verwendet wird.
+	#Sie wird aus Wachsmaisstärke oder Kartoffelstärke durch chemische Reaktion mit Propylenoxid hergestellt. Damit besteht sie fast ausschließlich aus Amylopektin, also aus verzweigten Ketten von Glucosemolekülen. Um einen zu schnellen Abbau des Amylopektins durch das endogene Enzym Amylase zu verhindern, erfolgt eine teilweise Hydroxypropylierung der Glucoseeinheiten. Diese Hydroxypropylierung ist auch notwendig um eine Wasserlöslichkeit von Stärke zu erreichen.
+	#Stärkeether werden vor allem als Verdickungsmittel eingesetzt, um die gewünschte Konsistenz wässriger Produkte zu erreichen. Heutige moderne Hydroxypropylstärken haben eine molare Masse von etwa 200.000 Da mit einem Substitutionsgrad von 59 %.
+	#description:et:
+
+	my $wikidata;
+	my $property;
+	my $lc;
+	my $value;
+
+	while (<$in>) {
+
+		# Humectant;450(ii);Trisodium diphosphate
+		my $line = $_;
+		chomp $line;
+		$line =~ s/[\r\n]//gmsx;
+
+		next if ($line =~ /:\w\w:$/msx);
+
+		if ($line =~ /^wikidata:en:(.*)$/msx) {
+			$wikidata = $1;
+			$property = undef;
+			$lc = undef;
+			$value = undef;
+		}
+		elsif ($line =~ /^(wikipedia_.*):(\w\w):(.+)$/msx) {
+			$property = $1;
+			$lc = $2;
+			$value = $3;
+			defined $properties{$wikidata} or $properties{$wikidata} = {};
+			defined $properties{$wikidata}{$lc} or $properties{$wikidata}{$lc} = {};
+			$properties{$wikidata}{$lc}{$property} = $value;
+		}
+		elsif ((defined $property) and (defined $lc)) {
+			$properties{$wikidata}{$lc}{$property} .= " $line";
+		}
+	}
+
+	return %properties;
+}
+
+# tmx files contain translations from the EU laws
 
 my $wikipedia_file = "$data_root/taxonomies/off/additives/additives.wikipedia.txt";
 
-open (my $IN, "<:encoding(UTF-8)", $wikipedia_file) or die("Could not open $wikipedia_file: $!");
+open my $IN, '<:encoding(UTF-8)', $wikipedia_file or Carp::croak("Could not open $wikipedia_file");
+my %properties = _parse_wikidata();
+close $IN or Carp::croak("Could not open $wikipedia_file");
 
-my %properties = ();
-
-#wikidata:en:Q1640437
-#description:bg:
-#description:es:
-#description:cs:
-#description:da:
-#de:Hydroxypropylstärke
-#wikipedia_url:de:https://de.wikipedia.org/wiki/Hydroxypropylstärke
-#description:de:Hydroxypropylstärke (HPS) ist eine modifizierte Stärke, die als Lebensmittelzusatzstoff mit der Bezeichnung E 1440 verwendet wird.
-#Sie wird aus Wachsmaisstärke oder Kartoffelstärke durch chemische Reaktion mit Propylenoxid hergestellt. Damit besteht sie fast ausschließlich aus Amylopektin, also aus verzweigten Ketten von Glucosemolekülen. Um einen zu schnellen Abbau des Amylopektins durch das endogene Enzym Amylase zu verhindern, erfolgt eine teilweise Hydroxypropylierung der Glucoseeinheiten. Diese Hydroxypropylierung ist auch notwendig um eine Wasserlöslichkeit von Stärke zu erreichen.
-#Stärkeether werden vor allem als Verdickungsmittel eingesetzt, um die gewünschte Konsistenz wässriger Produkte zu erreichen. Heutige moderne Hydroxypropylstärken haben eine molare Masse von etwa 200.000 Da mit einem Substitutionsgrad von 59 %.
-#description:et:
-
-my $wikidata;
-
-my $property;
-my $lc;
-my $value;
-
-while(<$IN>) {
-
-	# Humectant;450(ii);Trisodium diphosphate
-	my $line = $_;
-	chomp($line);
-	$line =~ s/\r|\n//g;
-	
-	next if ($line =~ /:\w\w:$/);
-	
-	if ($line =~ /^wikidata:en:(.*)$/) {
-		$wikidata = $1;
-		$property = undef;
-		$lc = undef;
-		$value = undef;
-	}
-	elsif ($line =~ /^(wikipedia_.*):(\w\w):(.+)$/) {
-		$property = $1;
-		$lc = $2;
-		$value = $3;
-		defined $properties{$wikidata} or $properties{$wikidata} = {};
-		defined $properties{$wikidata}{$lc} or $properties{$wikidata}{$lc} = {};
-		$properties{$wikidata}{$lc}{$property} = $value;
-	}
-	elsif ((defined $property) and (defined $lc)) {
-		$properties{$wikidata}{$lc}{$property} .= " " . $line;
-	}
-}
-
-#exit;
-
-
-binmode(STDIN, ":encoding(UTF-8)");
-binmode(STDOUT, ":encoding(UTF-8)");
-binmode(STDERR, ":encoding(UTF-8)");
-
-
-
+binmode STDIN, ':encoding(UTF-8)';
+binmode STDOUT, ':encoding(UTF-8)';
+binmode STDERR, ':encoding(UTF-8)';
 
 require Encode;
 
-
-my $j = 0;
-
 my $id;
-$wikidata = undef;
+my $wikidata;
 
 while (<>) {
 
 	my $line = $_;
-	
-	if ($line =~ /^en:(.*?)(,|$)/i) {
+
+	if ($line =~ /^en:(.*?)(,|$)/imsx) {
 		$id = $1;
 		$wikidata = undef;
 	}
-	
-	if ($line =~ /wikidata:en:(.*)$/) {
+
+	if ($line =~ /wikidata:en:(.*)$/msx) {
 		$wikidata = $1;
 		if (defined $properties{$wikidata}) {
-		
-			print "\nen:$id\n";
+			print "\nen:$id\n" or Carp::croak('Unable to write to *STDOUT');
 			foreach my $lc (sort keys %{$properties{$wikidata}}) {
 				foreach my $property (sort keys %{$properties{$wikidata}{$lc}}) {
-					print "$property:$lc: " . $properties{$wikidata}{$lc}{$property} . "\n";
+					print "$property:$lc: " . $properties{$wikidata}{$lc}{$property} . "\n" or Carp::croak('Unable to write to *STDOUT');
 				}
 			}
 		}
 	}
 
-	if (($line =~ /^\s*$/)) {
-
+	if (($line =~ /^\s*$/msx)) {
 		$id = undef;
 		$wikidata = undef;
 	}
-	
+
 }
-
-
