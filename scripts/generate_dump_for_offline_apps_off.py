2e5bcbea0b6adbc947e62c31726734c081500d14
Pierre Slamich <pierre@openfoodfacts.org>
2020-11-08 16:54:44 +0100

    Add support for the Eco-Score for Offline-Scan
    Add support for the Eco-Score for Offline-Scan

--- scripts/generate_dump_for_offline_apps_off.py
+++ scripts/generate_dump_for_offline_apps_off.py
@@ -8,7 +8,7 @@ from operator import itemgetter
 def main():
     import pandas
     df = pandas.read_csv('/srv/off/html/data/en.openfoodfacts.org.products.csv', sep='\t', low_memory=False)
-    colnames = ['code','product_name','quantity','brands','nutrition_grade_fr','nova_group']
+    colnames = ['code','product_name','quantity','brands','nutrition_grade_fr','nova_group','ecoscore_grade']
     df.to_csv('/srv/off/html/data/offline/en.openfoodfacts.org.products.small.csv', columns = colnames,sep='\t',index=False)
 
 if __name__ == '__main__':
