3b6bd7950d643b4a45f8a9701139e7d4210d4823
rosheen <rosheennaeem4@gmail.com>
2021-04-01 21:41:15 +0200

    Document the ProductOpener::GeoIP.pm

--- lib/ProductOpener/GeoIP.pm
+++ lib/ProductOpener/GeoIP.pm
@@ -18,6 +18,28 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+=head1 NAME
+
+ProductOpener::GeoIP - finds the country name and iso code from the given IP address.
+
+=head1 SYNOPSIS
+
+C<ProductOpener::GeoIP> is used to find the country name and iso code from the IP address using GeoIP2 API.
+
+    use ProductOpener::GeoIP qw/:all/;
+
+    $geolite2_path = '/usr/local/share/GeoLite2-Country/GeoLite2-Country.mmdb';
+    $geolite2_path is a path to geo IP location max mind database file.
+
+    Max Mind DataBase file(.mmdb) is a database format that maps the given IP addresses to their geolocation.
+
+=head1 DESCRIPTION
+
+The module implements the functionality to find the country name, and iso code by using the GeoIP2 API.
+The functions used in this module take the IP address and return the geolocation.
+
+=cut
+
 package ProductOpener::GeoIP;
 
 use utf8;
@@ -49,6 +71,22 @@ if (-e $geolite2_path) {
 	$gi = GeoIP2::Database::Reader->new(file => $geolite2_path);
 }
 
+=head1 FUNCTIONS
+
+=head2 get_country_for_ip()
+
+C<get_country_for_ip()> takes the IP address as an input parameter and returns the country name where the IP address is located.
+
+=head3 Arguments
+
+One scalar variable ip is passed as an argument.
+
+=head3 Return values
+
+If the function executes successfully it returns the country name. On the other hand, if it throws an exception, it simply returns undefined.
+
+=cut
+
 sub get_country_for_ip {
 	return unless $gi;
 	my $ip = shift;
@@ -67,6 +105,23 @@ sub get_country_for_ip {
 
 	return $country;
 }
+
+=head1 FUNCTIONS
+
+=head2 get_country_code_for_ip()
+
+C<get_country_code_for_ip()> takes the IP address as input paramter and returns the country iso code where IP address is located.
+
+=head3 Arguments
+
+One scalar variable ip is passed as an argument.
+
+=head3 Return values
+
+If the function executes successfully it returns the iso code for the country where the IP address is located.
+On the other hand, if it throws an exception, it simply returns undefined.
+
+=cut
 
 sub get_country_code_for_ip {
 	return unless $gi;
