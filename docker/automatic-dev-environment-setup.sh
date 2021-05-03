1d548ec728c92d1b5d06bb2f6eec346e7d92e914
Pierre Slamich <pierre@openfoodfacts.org>
2021-05-03 18:20:32 +0200

    Update automatic-dev-environment-setup.sh

--- docker/automatic-dev-environment-setup.sh
+++ docker/automatic-dev-environment-setup.sh
@@ -1,14 +1,23 @@
 #if mac:
 # check if git is installed on mac
+echo "Checking git is installed…"
 git --version
+echo "Cloning Open Food Facts web server - product-opener…"
 git clone https://github.com/openfoodfacts/openfoodfacts-server.git
+echo "Installing Docker… you may have to open it manually to grant permissions"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew cask install docker
 # opening docker for the first time in GUI mode to grant it priviledges
 open docker
 cd ./openfoodfacts-server/docker/
+echo "Building visual assets…"
 ./build_npm.sh
+echo "Building Product Opener…"
+echo "Your ventilator is probably going to start"
 ./build_dev.sh
+echo "TODO: describe me…"
+echo "This phase is going to be very long, up to an hour. Look for the increasing number snapshot min"
 ./start_dev.sh
+echo "Populating database…"
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml exec backend bash /opt/scripts/import_sample_data.sh
   
