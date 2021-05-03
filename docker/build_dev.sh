8abd19700b053d848d7b6f6bc2a67ce3f577ef03
Pierre Slamich <pierre@openfoodfacts.org>
2021-05-03 17:15:10 +0200

    Update build_dev.sh

--- docker/build_dev.sh
+++ docker/build_dev.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+#if mac:
+  brew cask install docker
+#else:
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh get-docker.sh
 docker-compose -f ./docker-compose.yml -f ./docker-compose.dev.yml build backend
