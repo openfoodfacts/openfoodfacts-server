34dac0bfbf3a0e94916ae38bc0544a90cb68cf33
Pierre Slamich <pierre@openfoodfacts.org>
2021-05-03 17:29:57 +0200

    Update build_dev.sh

--- docker/build_dev.sh
+++ docker/build_dev.sh
@@ -2,6 +2,8 @@
 #if mac:
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   brew cask install docker
+  # opening docker for the first time in GUI mode to grant it priviledges
+  open docker
 #else:
   curl -fsSL https://get.docker.com -o get-docker.sh
   sh get-docker.sh
