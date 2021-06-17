c26b43a149b0da482a08dd4d90e310220225f92c
Stéphane Gigandet <stephane@openfoodfacts.org>
2021-06-17 09:21:49 +0000

    remove prints

--- scripts/usda-import/keep_most_recent_row_for_each_product.py
+++ scripts/usda-import/keep_most_recent_row_for_each_product.py
@@ -2,22 +2,18 @@ import pandas as pd
 import sys
 
 if  (len(sys.argv) != 2):
-    print("Usage: python keep_most_recent_row_for_ech_product.pl [input CSV file] > [output CSV file]")
+    sys.stderr.write("Usage: python keep_most_recent_row_for_ech_product.pl [input CSV file] > [output CSV file]\n")
     sys.exit()
 
 input_csv_file = sys.argv[1]
 
-print("Reading CSV file", input_csv_file)
+sys.stderr.write("Reading CSV file " + input_csv_file + "\n")
 
 # Make sure numbers are kept as strings and not converted
 df = pd.read_csv(input_csv_file, sep='\t', dtype=str)
 
-print(df)
-
 # This assumes that the input file is sorted by date
 # In practice, it is sorted by fdc_id, which seems to be a sequence ordered by date
 df.drop_duplicates(subset=['code'], keep='last', inplace=True)
-
-print(df)
 
 df.to_csv(sys.stdout, sep='\t', quoting=None, index=False)
