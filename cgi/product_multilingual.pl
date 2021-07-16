3a95e4d8295360b94cb999c986fbb90cf8131be6
roshnaeem <rosheennaeem4@gmail.com>
2021-07-16 10:43:14 +0200

    checking js

--- cgi/product_multilingual.pl
+++ cgi/product_multilingual.pl
@@ -70,6 +70,8 @@ my $comment = 'Modification : ';
 
 my @errors = ();
 
+$admin = 1;
+
 my $html = '';
 my $code = normalize_code(param('code'));
 my $product_id;
@@ -954,129 +956,13 @@ HTML
 		$lang_value = $product_ref->{lc};
 	}
 
-	$scripts .= <<JS
-<script type="text/javascript">
-function toggle_manage_images_buttons() {
-		\$("#delete_images").addClass("disabled");
-		\$("#move_images").addClass("disabled");
-		\$( "#manage .ui-selected"  ).first().each(function() {
-			\$("#delete_images").removeClass("disabled");
-			\$("#move_images").removeClass("disabled");
-		});
-}
-</script>
-JS
-;
-
-
-
-	$initjs .= <<JS
-\$('#manage_images_accordion').on('toggled', function (event, accordion) {
-	toggle_manage_images_buttons();
-});
-\$("#delete_images").click({},function(event) {
-event.stopPropagation();
-event.preventDefault();
-if (! \$("#delete_images").hasClass("disabled")) {
-	\$("#delete_images").addClass("disabled");
-	\$("#move_images").addClass("disabled");
- \$('div[id="moveimagesmsg"]').html('<img src="/images/misc/loading2.gif" /> ' + lang().product_js_deleting_images);
- \$('div[id="moveimagesmsg"]').show();
-	var imgids = '';
-	var i = 0;
-	\$( "#manage .ui-selected"  ).each(function() {
-		var imgid = \$( this ).attr('id');
-		imgid = imgid.replace("manage_","");
-		imgids += imgid + ',';
-		i++;
-});
-	if (i) {
-		// remove trailing comma
-		imgids = imgids.substring(0, imgids.length - 1);
-	}
- \$("#product_form").ajaxSubmit({
-  url: "/cgi/product_image_move.pl",
-  data: { code: code, move_to_override: "trash", imgids : imgids },
-  dataType: 'json',
-  beforeSubmit: function(a,f,o) {
-  },
-  success: function(data) {
-	if (data.error) {
-		\$('div[id="moveimagesmsg"]').html(lang().product_js_images_delete_error + ' - ' + data.error);
-	}
-	else {
-		\$('div[id="moveimagesmsg"]').html(lang().product_js_images_deleted);
-	}
-	\$([]).selectcrop('init_images',data.images);
-	\$(".select_crop").selectcrop('show');
-  },
-  error : function(jqXHR, textStatus, errorThrown) {
-	\$('div[id="moveimagesmsg"]').html(lang().product_js_images_delete_error + ' - ' + textStatus);
-  },
-  complete: function(XMLHttpRequest, textStatus) {
-	}
- });
-}
-});
-\$("#move_images").click({},function(event) {
-event.stopPropagation();
-event.preventDefault();
-if (! \$("#move_images").hasClass("disabled")) {
-	\$("#delete_images").addClass("disabled");
-	\$("#move_images").addClass("disabled");
- \$('div[id="moveimagesmsg"]').html('<img src="/images/misc/loading2.gif" /> ' + lang().product_js_moving_images);
- \$('div[id="moveimagesmsg"]').show();
-	var imgids = '';
-	var i = 0;
-	\$( "#manage .ui-selected"  ).each(function() {
-		var imgid = \$( this ).attr('id');
-		imgid = imgid.replace("manage_","");
-		imgids += imgid + ',';
-		i++;
-});
-	if (i) {
-		// remove trailing comma
-		imgids = imgids.substring(0, imgids.length - 1);
-	}
- \$("#product_form").ajaxSubmit({
-  url: "/cgi/product_image_move.pl",
-  data: { code: code, move_to_override: \$("#move_to").val(), copy_data_override: \$("#copy_data").prop( "checked" ), imgids : imgids },
-  dataType: 'json',
-  beforeSubmit: function(a,f,o) {
-  },
-  success: function(data) {
-	if (data.error) {
-		\$('div[id="moveimagesmsg"]').html(lang().product_js_images_move_error + ' - ' + data.error);
-	}
-	else {
-		\$('div[id="moveimagesmsg"]').html(lang().product_js_images_moved + ' &rarr; ' + data.link);
-	}
-	\$([]).selectcrop('init_images',data.images);
-	\$(".select_crop").selectcrop('show');
-  },
-  error : function(jqXHR, textStatus, errorThrown) {
-	\$('div[id="moveimagesmsg"]').html(lang().product_js_images_move_error + ' - ' + textStatus);
-  },
-  complete: function(XMLHttpRequest, textStatus) {
-		\$("#move_images").addClass("disabled");
-		\$("#move_images").addClass("disabled");
-		\$( "#manage .ui-selected"  ).first().each(function() {
-			\$("#move_images").removeClass("disabled");
-			\$("#move_images").removeClass("disabled");
-		});
-	}
- });
-}
-});
-JS
-;
 
 	$template_data_ref_display->{lang_value} = $lang_value;
 	$template_data_ref_display->{lang_options} = \@lang_options;
 	$template_data_ref_display->{lang} = $Lang{lang}{$lang};
 	$template_data_ref_display->{display_select_manage} = display_select_manage($product_ref);
 
-	if ($User{moderator}) {
+
 
 		$template_data_ref_display->{copy_data} = $Lang{copy_data}{$lc};
 		$template_data_ref_display->{manage_images_info} = $Lang{manage_images_info}{$lc};
@@ -1084,7 +970,7 @@ JS
 		$template_data_ref_display->{move_images_to_another_product} = $Lang{move_images_to_another_product}{$lc};
 		$template_data_ref_display->{barcode} = $Lang{barcode}{$lc};
 		$template_data_ref_display->{manage_images} = $Lang{manage_images}{$lc};
-	}
+	
 
 	$product_ref->{langs_order} = { fr => 0, nl => 1, en => 1, new => 2 };
 
