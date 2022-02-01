79b046c1c21cb898af56bd4a122169de0551586c
Stéphane Gigandet <stephane@openfoodfacts.org>
2022-02-01 18:01:41 +0100

    new misc facet values for Eco-Score missing packaging warning (#6373)
    * new misc facet values for Eco-Score missing packaging warning
    
    * more ecoscore missing data facets
    
    * update tests

--- lib/ProductOpener/Ecoscore.pm
+++ lib/ProductOpener/Ecoscore.pm
@@ -77,7 +77,7 @@ use ProductOpener::Packaging qw/:all/;
 use Storable qw(dclone freeze);
 use Text::CSV();
 use Math::Round;
-use Data::DeepAccess qw(deep_get);
+use Data::DeepAccess qw(deep_get deep_exists);
 
 my %agribalyse = ();
 
@@ -592,6 +592,15 @@ sub compute_ecoscore($) {
 		adjustments => {},
 	};
 
+	remove_tag($product_ref,"misc","en:ecoscore-computed");
+	remove_tag($product_ref,"misc","en:ecoscore-missing-data-warning");
+	remove_tag($product_ref,"misc","en:ecoscore-missing-data-no-packagings");
+	foreach my $missing (qw(labels origins packagings)) {
+		remove_tag($product_ref,"misc","en:ecoscore-missing-data-" . $missing);
+	}
+	remove_tag($product_ref,"misc","en:ecoscore-no-missing-data");
+	remove_tag($product_ref,"misc","en:ecoscore-not-applicable");
+
 	# Check if we have extended ecoscore_data from the impact estimator
 	# Remove any misc "en:ecoscore-extended-data-version-[..]" tags
 	if (defined $product_ref->{misc_tags}) {
@@ -631,13 +640,8 @@ sub compute_ecoscore($) {
 		
 		add_tag($product_ref,"misc","en:ecoscore-not-applicable");
 		add_tag($product_ref,"misc","en:ecoscore-not-computed");
-		remove_tag($product_ref,"misc","en:ecoscore-computed");
-		remove_tag($product_ref,"misc","en:ecoscore-missing-data-warning");
-		remove_tag($product_ref,"misc","en:ecoscore-no-missing-data");
 	}
 	else {
-		remove_tag($product_ref,"misc","en:ecoscore-not-applicable");
-		
 		# Compute the LCA Eco-Score based on AgriBalyse
 		
 		compute_ecoscore_agribalyse($product_ref);
@@ -750,15 +754,25 @@ sub compute_ecoscore($) {
 			if ($missing_data_warning) {
 				$product_ref->{ecoscore_data}{missing_data_warning} = 1;
 				add_tag($product_ref,"misc","en:ecoscore-missing-data-warning");
-				remove_tag($product_ref,"misc","en:ecoscore-no-missing-data");
-			}
-			else {
-				remove_tag($product_ref,"misc","en:ecoscore-missing-data-warning");
-				add_tag($product_ref,"misc","en:ecoscore-no-missing-data");
+
+				# add facets for missing data
+				foreach my $missing (qw(labels origins packagings)) {
+					if (deep_exists($product_ref, "ecoscore_data", "missing", $missing)) {
+						add_tag($product_ref,"misc","en:ecoscore-missing-data-" . $missing);
+					}
+				}
+				
+				# ecoscore-missing-data-packagings will also be triggered when we have some packaging data that is not complete
+				# e.g. we have a shape like "bottle" but no associated material
+				# also add a facet when we have no packaging information at all
+				my $packaging_warning = deep_get($product_ref, qw(ecoscore_data adjustments packaging warning));
+				if ((defined $packaging_warning) and ($packaging_warning eq "packaging_data_missing")) {
+					add_tag($product_ref,"misc","en:ecoscore-missing-data-no-packagings");
+				}
+
 			}
 			
 			add_tag($product_ref,"misc","en:ecoscore-computed");
-			remove_tag($product_ref,"misc","en:ecoscore-not-computed");		
 		}
 		else {
 			# No AgriBalyse category match
@@ -768,9 +782,6 @@ sub compute_ecoscore($) {
 			$product_ref->{ecoscore_grade} = "unknown";
 			
 			add_tag($product_ref,"misc","en:ecoscore-not-computed");
-			remove_tag($product_ref,"misc","en:ecoscore-computed");
-			remove_tag($product_ref,"misc","en:ecoscore-missing-data-warning");
-			remove_tag($product_ref,"misc","en:ecoscore-no-missing-data");
 		}
 	}
 }
