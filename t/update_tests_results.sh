99f694a841f830ef386316d34386fa372b81b6ac
Stéphane Gigandet <stephane@openfoodfacts.org>
2022-03-02 17:51:25 +0100

    feat: add missing_key_data flag to ecoscore

---
+++ t/update_tests_results.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Update the expected results with the content of the actual results
+# git diff can then be used to review the differences
+
+perl attributes.t --results expected_test_results/attributes/
+perl ecoscore.t --results expected_test_results/ecoscore/
+perl forest_footprint.t --results expected_test_results/forest_footprint/
+perl import_gs1.t --results expected_test_results/import_gs1/
+perl ingredients.t --results expected_test_results/ingredients/
+perl nutriscore.t --results expected_test_results/nutriscore/
+perl packaging.t --results expected_test_results/packaging/
+perl recipes.t --results expected_test_results/recipes/
