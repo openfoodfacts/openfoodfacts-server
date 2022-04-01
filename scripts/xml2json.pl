25c10b2e1e6383195a2da1e54415ba6a6ce12084
Stéphane Gigandet <stephane@openfoodfacts.org>
2022-04-01 17:46:47 +0200

    try to use Perl XML::XML2JSON instead of nodejs xml2json

---
+++ scripts/xml2json.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl -w
+
+# This file is part of Product Opener.
+# 
+# Product Opener
+# Copyright (C) 2011-2019 Association Open Food Facts
+# Contact: contact@openfoodfacts.org
+# Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
+# 
+# Product Opener is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use strict;
+use utf8;
+use warnings;
+
+use JSON;
+use XML::XML2JSON;
+
+my $dir = $ARGV[0];
+
+if (not defined $dir) {
+    die("Missing directory parameter.");
+}
+
+opendir (my $dh, $dir) or die("Could not open the $dir directory: $!\n");
+
+my $xml2json = XML::XML2JSON->new( module => 'JSON', pretty => 1, force_array => 0, attribute_prefix => "");
+
+foreach my $file (sort(readdir($dh))) {
+	
+	next if $file !~ /\.xml$/;
+
+    open (my $in, "<:encoding(UTF-8)", "$dir/$file") or die("Could not read $dir/$file: $!");
+    my $xml = join('', (<$in>));
+    close ($in);
+
+    my $json = $xml2json->convert($xml);
+
+    # XML2JSON changes the namespace concatenation character from : to $
+    # e.g. "allergen_information$allergenInformationModule":
+    # it is unwanted, turn it back to : so that we can match the expected input of ProductOpener::GS1
+    $json =~ s/([a-z])\$([a-z])/$1:$2/ig;
+
+    $file =~ s/\.xml$/.json/;
+
+    open (my $out, ">:encoding(UTF-8)", "$dir/$file") or die("Could not read $dir/$file: $!");
+    print $out $json;
+    close ($out);
+}
