74691151f648c7e87247674a90f6cc53d68cdfaa
Aadarsh A <aadarsh.ram@gmail.com>
2022-04-11 21:42:41 +0530

    fix: Email address with space (#6578)
    * Added modified email
    
    * Added else clause

--- lib/ProductOpener/Users.pm
+++ lib/ProductOpener/Users.pm
@@ -348,6 +348,10 @@ sub check_user_form($$$) {
 	if (not $address) {
 		push @{$errors_ref}, $Lang{error_invalid_email}{$lang};
 	}
+	else {
+		# If all checks have passed, reinitialize with modified email
+		$user_ref->{email} = $address;
+	}
 
 	if ($type eq 'add') {
 
