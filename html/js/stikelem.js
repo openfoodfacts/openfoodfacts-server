3aad16a087bbe822d9fe2e2ff70b45970b074288
fred <>
2022-04-27 10:22:34 +0200

    [CHG] topbar end

--- html/js/stikelem.js
+++ html/js/stikelem.js
@@ -4,9 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
         stickTo: '.main-container'
     });
     var StickyNavProd = new hcSticky('.prod-nav', {
-        stickTo: '.main-product'
+        stickTo: '#main_column',
+        followScroll: false,
+        responsive: {
+            1023: {
+              top: -20},
+            731: {
+            top: 40},
+        }
     });
     //StickyNav.update({
     //top: 20
     //});
+    console.log ($('#main_column').offset.top)
 });
