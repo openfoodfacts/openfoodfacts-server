d5fbdf5a61cfa19b4ca213b480f5bfeab8ad4458
yuktea <73362292+yuktea@users.noreply.github.com>
2022-06-20 11:25:49 +0530

    Add template path

--- cgi/export_products.pl
+++ cgi/export_products.pl
@@ -185,8 +185,9 @@ elsif (($action eq "process") and $allow_submit) {
 	$html .= "<p>" . lang("export_job_import") .  " - <span id=\"result2\"></span></p>";
 	$html .= "<p>" . lang("export_job_status_update") .  " - <span id=\"result3\"></span></p>";
 
-	$initjs .= $js;
+	process_template('web/pages/import_file_process/export_products.tt.js', $template_data_ref, \$js);
 
+	$initjs .= $js;
 }
 else {
 
