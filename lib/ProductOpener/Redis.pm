ef153864b882b01f405bf492cf6cd94af07d9f26
Simon Jenkins <simonj2@gmail.com>
2022-10-04 20:14:39 -0700

    Working

--- lib/ProductOpener/Redis.pm
+++ lib/ProductOpener/Redis.pm
@@ -21,18 +21,24 @@ use ProductOpener::Config2;
 use Redis::Client;
 
 sub init_redis() {
+
+	$log->debug("init_redis", { redis_url_env => $ENV{REDIS_URL}, redis_url => $ProductOpener::Config2::redis_url } ) if $log->is_debug();
+	
+	$log->warn("REDIS_URL env", {redis_url => $ENV{REDIS_URL} }) if $log->is_warn();
 	if ($ProductOpener::Config2::redis_url eq "")
 	{
 		$log->warn("Redis URL not provided for search indexing", { error => $@ }) if $log->is_warn();
 		return undef;
 	}
+	my $redis_client;
 	eval {
-		my $redis_client = Redis::Client->new(host => $ProductOpener::Config2::redis_url);
-		return $redis_client;
-	
+		$redis_client = Redis::Client->new(host => $ProductOpener::Config2::redis_url);
 	};
 	if ($@) {
 		$log->warn("Error connecting to Redis", { error => $@ }) if $log->is_warn();
+	}
+	else {
+		return $redis_client;
 	}
 	return undef;
 }
@@ -41,7 +47,9 @@ my $redis_client = init_redis();
 
 sub push_to_search_service($product_ref) {
 	if (defined($redis_client)) {
-                $redis_client->rpush('search_import_queue', $product_ref->{code});
+		eval {
+                	$redis_client->rpush('search_import_queue', $product_ref->{code});
+		};
 	}
 
         return;
