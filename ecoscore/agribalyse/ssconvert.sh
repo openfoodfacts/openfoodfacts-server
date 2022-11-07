99e41c35abdb35c509970cddc58e9a7676ea4462
John Gomersall <thegoms@gmail.com>
2022-11-07 18:33:08 +0000

    Rename files to not contain version component

--- ecoscore/agribalyse/ssconvert.sh
+++ ecoscore/agribalyse/ssconvert.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ssconvert AGRIBALYSE3.0.1_vf.xlsm AGRIBALYSE3.0.1_vf.csv -S
+ssconvert AGRIBALYSE_vf.xlsm AGRIBALYSE_vf.csv -S
+tail -n +9 AGRIBALYSE_vf.csv.2 | sort --numeric-sort --field-separator "," > AGRIBALYSE_details_by_step.csv
