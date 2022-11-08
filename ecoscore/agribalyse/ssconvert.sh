54a2a02cb4ea87f85c52ad32c1c10386a177a721
John Gomersall <thegoms@gmail.com>
2022-11-08 10:09:00 +0000

    Parse summary csv on export

--- ecoscore/agribalyse/ssconvert.sh
+++ ecoscore/agribalyse/ssconvert.sh
@@ -3,12 +3,5 @@ ssconvert AGRIBALYSE_vf.xlsm AGRIBALYSE_vf.csv -S
 tail -n +9 AGRIBALYSE_vf.csv.2 | sort --numeric-sort --field-separator "," | perl -MText::CSV -le '
     binmode(STDOUT, ":utf8"); $csv = Text::CSV->new({binary=>1}); 
     while ($row_ref = $csv->getline(STDIN)){
-        my ($code,              # Agribalyse code 
-        $ciqual_code,           # Ciqual code (equal to above)
-        $group,                 # Groupe d aliment
-        $sub_group,             # Sous-groupe d aliment
-        $name_fr,               # Nom du Produit en Français
-        $name_en                # LCI Name
-        ) = @$row_ref;
-        print "$code,$ciqual_code,$group,$sub_group,$name_fr,$name_en";
+        $csv->print(*STDOUT, [@$row_ref[0..5]]);
     }' > AGRIBALYSE_summary.csv
