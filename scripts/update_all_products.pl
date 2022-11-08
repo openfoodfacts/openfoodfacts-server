7a34e95a604aab81f14ddcc802e385f08d2740a3
Stéphane Gigandet <stephane@openfoodfacts.org>
2022-11-08 14:07:35 +0100

    suggestions from code review

--- scripts/update_all_products.pl
+++ scripts/update_all_products.pl
@@ -416,6 +416,11 @@ if ($prefix_packaging_tags_with_language) {
 
 while (my $product_ref = $cursor->next) {
 
+	# Response structure to keep track of warnings and errors
+	# Note: currently some warnings and errors are added,
+	# but we do not yet do anything with them
+	my $response_ref = get_initialized_response();
+
 	my $productid = $product_ref->{_id};
 	my $code = $product_ref->{code};
 	my $path = product_path($product_ref);
@@ -1207,11 +1212,7 @@ while (my $product_ref = $cursor->next) {
 		}
 
 		if ($process_packagings) {
-			# Until we provide an interface to directly change the packaging data structure
-			# erase it before reconstructing it
-			# (otherwise there is no way to remove incorrect entries)
-			$product_ref->{packagings} = [];
-			analyze_and_combine_packaging_data($product_ref);
+			analyze_and_combine_packaging_data($product_ref, $response_ref);
 		}
 
 		if ($compute_ecoscore) {
