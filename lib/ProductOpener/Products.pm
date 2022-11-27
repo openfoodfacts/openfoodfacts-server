04d636c2d35ed958b693625ab0a76ae036d796b4
Pierre Slamich <pierre@openfoodfacts.org>
2022-11-27 12:44:35 +0100

    Update lib/ProductOpener/Products.pm

--- lib/ProductOpener/Products.pm
+++ lib/ProductOpener/Products.pm
@@ -2515,7 +2515,7 @@ sub product_action_url ($code, $action) {
 	elsif ($action eq "add_packaging_text") {
 		$url .= "#packaging";
 	}
-	# Note: 27/11/2022 - The following HTML anchors will do nothing unless a custom HTML anchor is added in the future
+	# Note: 27/11/2022 - Pierre - The following HTML anchors will do nothing unless a custom HTML anchor is added in the future
 	elsif ($action eq "add_origins") {
 		$url .= "#origins";
 	}
