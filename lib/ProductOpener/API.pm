c96a7ce15d7908377995224d93b66fd1b0e62b37
Stéphane Gigandet <stephane@openfoodfacts.org>
2022-11-30 15:11:37 +0100

    fix conflict: customized packagings and knowledge panels

--- lib/ProductOpener/API.pm
+++ lib/ProductOpener/API.pm
@@ -78,6 +78,7 @@ use Apache2::RequestIO();
 use Apache2::RequestRec();
 use JSON::PP;
 use Data::DeepAccess qw(deep_get);
+use Storable qw(dclone);
 
 sub get_initialized_response() {
 	return {
@@ -506,29 +507,41 @@ Reference to the customized product object.
 
 sub customize_packagings ($request_ref, $product_ref) {
 
+	my $customized_packagings_ref = $product_ref->{packagings};
+
 	if (defined $product_ref->{packagings}) {
 
 		my $tags_lc = request_param($request_ref, 'tags_lc');
 
+		# We need to make a copy of $product_ref->{packagings}, it cannot be updated directly
+		# as the internal format of "packagings" is used in other functions
+		# (e.g. to generate knowledge panels)
+
+		$customized_packagings_ref = [];
+
 		foreach my $packaging_ref (@{$product_ref->{packagings}}) {
+
+			my $customized_packaging_ref = dclone($packaging_ref);
 
 			if ($request_ref->{api_version} >= 3) {
 				# Shape, material and recycling are localized
 				foreach my $property ("shape", "material", "recycling") {
 					if (defined $packaging_ref->{$property}) {
 						my $property_value_id = $packaging_ref->{$property};
-						$packaging_ref->{$property} = {"id" => $property_value_id};
+						$customized_packaging_ref->{$property} = {"id" => $property_value_id};
 						if (defined $tags_lc) {
-							$packaging_ref->{$property}{lc_name}
+							$customized_packaging_ref->{$property}{lc_name}
 								= display_taxonomy_tag($tags_lc, $packaging_taxonomies{$property}, $property_value_id);
 						}
 					}
 				}
 			}
+
+			push @$customized_packagings_ref, $customized_packaging_ref;
 		}
 	}
 
-	return $product_ref->{packagings};
+	return $customized_packagings_ref;
 }
 
 =head2 customize_response_for_product ( $request_ref, $product_ref, $fields )
