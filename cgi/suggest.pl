bfe45fc1acda9d172e1bbac70f9d2f1e8cdbcfc8
Stéphane Gigandet <stephane@openfoodfacts.org>
2023-01-17 17:40:52 +0100

    moved code from suggest.pl to APITaxonomySuggestions

--- cgi/suggest.pl
+++ cgi/suggest.pl
@@ -26,15 +26,9 @@ use CGI::Carp qw(fatalsToBrowser);
 use CGI qw/:cgi :form escapeHTML/;
 
 use ProductOpener::Config qw/:all/;
-use ProductOpener::Store qw/:all/;
-use ProductOpener::Index qw/:all/;
 use ProductOpener::Display qw/:all/;
-use ProductOpener::Users qw/:all/;
-use ProductOpener::Products qw/:all/;
-use ProductOpener::Food qw/:all/;
-use ProductOpener::Tags qw/:all/;
+use ProductOpener::APITaxonomySuggestions qw/:all/;
 use ProductOpener::Lang qw/:all/;
-use ProductOpener::PackagerCodes qw/:all/;
 use ProductOpener::HTTP qw/:all/;
 
 use CGI qw/:cgi :form escapeHTML/;
@@ -42,7 +36,6 @@ use URI::Escape::XS;
 use Storable qw/dclone/;
 use Encode;
 use JSON::PP;
-use List::Util qw/min/;
 
 my $request_ref = ProductOpener::Display::init_request();
 
@@ -53,155 +46,17 @@ my $tagtype = single_param('tagtype');
 
 my $string = decode utf8 => (single_param('string') || single_param('term'));
 
-# my @suggestions = get_taxonomy_suggestions_matching_string($tagtype, $string);
+my @suggestions = get_taxonomy_suggestions_matching_string($request_ref, $tagtype, $string);
 
-=head1 CGI script to auto-complete entries for tags
-
-=head2 Request parameters
-
-=head3 tagtype - the type of tag
-
-=head3 string - string to search
-
-=head3 string - string to search
-
-If string and string are passed together, they are concatenated together as separate words
-
-=head3 limit - max number of suggestions
-
-Warning, we are currently doing a brute force search, so avoid setting it too high
-
-=cut
-
-# search language code
-my $search_lc = $lc;
-# superseed by request parameter
-if (defined single_param('lc')) {
-	$search_lc = single_param('lc');
-}
-
-my $original_lc = $search_lc;
-
-# if search string begins with a language code, use it for search
-if ($string =~ /^(\w\w):/) {
-	$search_lc = $1;
-	$string = $';
-}
-
-# max results
-my $limit = 25;
-# superseed by request parameter
-if (defined single_param('limit')) {
-	# we put a hard limit however
-	$limit = min(int(single_param('limit')), 400);
-}
-
-my @suggestions = ();    # Suggestions starting with the string
-my @suggestions_c = ();    # Suggestions containing the string
-my @suggestions_f = ();    # fuzzy suggestions
-
-my $cache_max_age = 0;
-my $suggestions_count = 0;
-
-# search for emb codes
-if ($tagtype eq 'emb_codes') {
-	my $stringid = get_string_id_for_lang("no_language", normalize_packager_codes($string));
-	my @tags = sort keys %packager_codes;
-	foreach my $canon_tagid (@tags) {
-		next if $canon_tagid !~ /^$stringid/;
-		push @suggestions, normalize_packager_codes($canon_tagid);
-		last if ++$suggestions_count >= $limit;
-	}
-	# add cache to request
-	$cache_max_age = 3600;
-}
-else {
-	# search for string in a taxonomy
-
-	# normalize string
-	my $stringid = get_string_id_for_lang($search_lc, $string);
-	# remove eventual leading or ending "-"
-	$stringid =~ s/^-//;
-	$stringid =~ s/^-$//;
-	# fuzzy match whole words with eventual inter-words
-	my $fuzzystringid = join(".*", split("-", $stringid));
-	# all tags can be retrieve from the $translations_to hash
-	my @tags = sort keys %{$translations_to{$tagtype}};
-	foreach my $canon_tagid (@tags) {
-		# just_synonyms are not real entries
-		next if defined $just_synonyms{$tagtype}{$canon_tagid};
-
-		my $tag;    # this is the content string
-		my $tagid;    # this is the tag
-
-		# search if the tag exists in target language
-		if (defined $translations_to{$tagtype}{$canon_tagid}{$search_lc}) {
-
-			$tag = $translations_to{$tagtype}{$canon_tagid}{$search_lc};
-			# TODO: explain why $tagid can be different from $canon_tagid
-			$tagid = get_string_id_for_lang($search_lc, $tag);
-
-			# add language prefix if we are not searching current interface language
-			if (not($search_lc eq $original_lc)) {
-				$tag = $search_lc . ":" . $tag;
-			}
-		}
-		# also search for special language code "xx" which is universal
-		elsif (defined $translations_to{$tagtype}{$canon_tagid}{xx}) {
-			$tag = $translations_to{$tagtype}{$canon_tagid}{xx};
-			$tagid = get_string_id_for_lang("xx", $tag);
-		}
-
-		if (defined $tag) {
-			# matching at start, best matches
-			if ($tagid =~ /^$stringid/) {
-				push @suggestions, $tag;
-				# only matches at start are considered
-				$suggestions_count++;
-			}
-			# matching inside
-			elsif ($tagid =~ /$stringid/) {
-				push @suggestions_c, $tag;
-			}
-			# fuzzy match
-			elsif ($tagid =~ /$fuzzystringid/) {
-				push @suggestions_f, $tag;
-			}
-			# end as soon as we got enough
-			last if $suggestions_count >= $limit;
-		}
-	}
-	# add cache to request
-	$cache_max_age = 3600;
-}
-# sort best suggestions
-@suggestions = sort @suggestions;
-# suggestions containing string
-my $contains_to_add = min($limit - (scalar @suggestions), scalar @suggestions_c) - 1;
-if ($contains_to_add >= 0) {
-	push @suggestions, @suggestions_c[0 .. $contains_to_add];
-}
-# Suggestions as fuzzy match
-my $fuzzy_to_add = min($limit - (scalar @suggestions), scalar @suggestions_f) - 1;
-if ($fuzzy_to_add >= 0) {
-	push @suggestions, @suggestions_f[0 .. $fuzzy_to_add];
-}
 my $data = encode_json(\@suggestions);
 
 # send response
 write_cors_headers();
 
-if ($cache_max_age) {
-	print header(
-		-type => 'application/json',
-		-charset => 'utf-8',
-		-cache_control => 'public, max-age=' . $cache_max_age,
-	);
-}
-else {
-	print header(
-		-type => 'application/json',
-		-charset => 'utf-8',
-	);
-}
+print header(
+	-type => 'application/json',
+	-charset => 'utf-8',
+	-cache_control => 'public, max-age=' . 60,	# 1 minute cache
+);
+
 print $data;
--- lib/ProductOpener/APITaxonomySuggestions.pm
+++ lib/ProductOpener/APITaxonomySuggestions.pm
@@ -37,7 +37,7 @@ BEGIN {
 	use vars qw(@ISA @EXPORT_OK %EXPORT_TAGS);
 	@EXPORT_OK = qw(
 		&taxonomy_suggestions_api
-		&generate_
+		&get_taxonomy_suggestions_matching_string
 	);    # symbols to export on request
 	%EXPORT_TAGS = (all => [@EXPORT_OK]);
 }
@@ -45,10 +45,14 @@ BEGIN {
 use vars @EXPORT_OK;
 
 use ProductOpener::Config qw/:all/;
+use ProductOpener::Store qw/:all/;
 use ProductOpener::Display qw/:all/;
 use ProductOpener::Lang qw/:all/;
 use ProductOpener::Tags qw/:all/;
 use ProductOpener::API qw/:all/;
+use ProductOpener::PackagerCodes qw/:all/;
+
+use List::Util qw/min/;
 
 =head2 taxonomy_suggestions_api ( $request_ref )
 
@@ -102,6 +106,146 @@ sub taxonomy_suggestions_api ($request_ref) {
 	$log->debug("taxonomy_suggestions_api - stop", {request => $request_ref}) if $log->is_debug();
 
 	return;
+}
+
+
+
+=head2 get_taxonomy_suggestions_matching_string ($request_ref, $tagtype, $string)
+
+Generate taxonomy suggestions matching a string.
+
+The generation uses a brute force approach to match the input string to taxonomies.
+
+By priority, the function returns:
+- taxonomy entries that match the input string at the beginning
+- taxonomy entries that contain the input string
+- taxonomy entries that contain words contained in the input string
+
+=head3 Parameters
+
+=head4 $request_ref (input)
+
+Reference to the request object.
+
+=head4 tagtype - the type of tag
+
+=head4 string - string to search
+
+=cut
+
+sub get_taxonomy_suggestions_matching_string($request_ref, $tagtype, $string) {
+
+	# search language code
+	my $search_lc = single_param('lc') || $request_ref->{lc};
+
+	# superseed by request parameter
+	if (defined single_param('lc')) {
+		$search_lc = single_param('lc');
+	}
+
+	my $original_lc = $search_lc;
+
+	# if search string begins with a language code, use it for search
+	if ($string =~ /^(\w\w):/) {
+		$search_lc = $1;
+		$string = $';
+	}
+
+	# max results
+	my $limit = 25;
+	# superseed by request parameter
+	if (defined single_param('limit')) {
+		# we put a hard limit however
+		$limit = min(int(single_param('limit')), 400);
+	}
+
+	my @suggestions = ();    # Suggestions starting with the string
+	my @suggestions_c = ();    # Suggestions containing the string
+	my @suggestions_f = ();    # fuzzy suggestions
+
+	my $suggestions_count = 0;
+
+	# search for emb codes
+	if ($tagtype eq 'emb_codes') {
+		my $stringid = get_string_id_for_lang("no_language", normalize_packager_codes($string));
+		my @tags = sort keys %packager_codes;
+		foreach my $canon_tagid (@tags) {
+			next if $canon_tagid !~ /^$stringid/;
+			push @suggestions, normalize_packager_codes($canon_tagid);
+			last if ++$suggestions_count >= $limit;
+		}
+	}
+	else {
+		# search for string in a taxonomy
+
+		# normalize string
+		my $stringid = get_string_id_for_lang($search_lc, $string);
+		# remove eventual leading or ending "-"
+		$stringid =~ s/^-//;
+		$stringid =~ s/^-$//;
+		# fuzzy match whole words with eventual inter-words
+		my $fuzzystringid = join(".*", split("-", $stringid));
+		# all tags can be retrieve from the $translations_to hash
+		my @tags = sort keys %{$translations_to{$tagtype}};
+		foreach my $canon_tagid (@tags) {
+			# just_synonyms are not real entries
+			next if defined $just_synonyms{$tagtype}{$canon_tagid};
+
+			my $tag;    # this is the content string
+			my $tagid;    # this is the tag
+
+			# search if the tag exists in target language
+			if (defined $translations_to{$tagtype}{$canon_tagid}{$search_lc}) {
+
+				$tag = $translations_to{$tagtype}{$canon_tagid}{$search_lc};
+				# TODO: explain why $tagid can be different from $canon_tagid
+				$tagid = get_string_id_for_lang($search_lc, $tag);
+
+				# add language prefix if we are not searching current interface language
+				if (not($search_lc eq $original_lc)) {
+					$tag = $search_lc . ":" . $tag;
+				}
+			}
+			# also search for special language code "xx" which is universal
+			elsif (defined $translations_to{$tagtype}{$canon_tagid}{xx}) {
+				$tag = $translations_to{$tagtype}{$canon_tagid}{xx};
+				$tagid = get_string_id_for_lang("xx", $tag);
+			}
+
+			if (defined $tag) {
+				# matching at start, best matches
+				if ($tagid =~ /^$stringid/) {
+					push @suggestions, $tag;
+					# only matches at start are considered
+					$suggestions_count++;
+				}
+				# matching inside
+				elsif ($tagid =~ /$stringid/) {
+					push @suggestions_c, $tag;
+				}
+				# fuzzy match
+				elsif ($tagid =~ /$fuzzystringid/) {
+					push @suggestions_f, $tag;
+				}
+				# end as soon as we got enough
+				last if $suggestions_count >= $limit;
+			}
+		}
+	}
+	# sort best suggestions
+	@suggestions = sort @suggestions;
+	# suggestions containing string
+	my $contains_to_add = min($limit - (scalar @suggestions), scalar @suggestions_c) - 1;
+	if ($contains_to_add >= 0) {
+		push @suggestions, @suggestions_c[0 .. $contains_to_add];
+	}
+	# Suggestions as fuzzy match
+	my $fuzzy_to_add = min($limit - (scalar @suggestions), scalar @suggestions_f) - 1;
+	if ($fuzzy_to_add >= 0) {
+		push @suggestions, @suggestions_f[0 .. $fuzzy_to_add];
+	}
+
+	return @suggestions;
 }
 
 1;
