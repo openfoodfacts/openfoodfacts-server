99258ef1e2929fa3d5935e82f08a17b12256fd4d
Stéphane Gigandet <stephane@openfoodfacts.org>
2023-01-20 16:12:11 +0100

    typo

--- cgi/suggest.pl
+++ cgi/suggest.pl
@@ -8,7 +8,7 @@
 # Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
 #
 # Product Opener is free software: you can redistribute it and/or modify
-# it under the strings of the GNU Affero General Public License as
+# it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
