87f5fb7ede0a3bafc34b98447b62b1c26420e7db
John Gomersall <thegoms@gmail.com>
2023-01-30 10:15:59 +0000

    Move pattern matching into script

--- scripts/build_tags_taxonomy.pl
+++ scripts/build_tags_taxonomy.pl
@@ -28,110 +28,124 @@ use ProductOpener::Tags qw/:all/;
 use ProductOpener::Food qw/:all/;
 use Digest::SHA1;
 use File::Copy;
- 
+use File::Basename;
+
 my $tagtype = $ARGV[0];
 my $publish = $ARGV[1];
 
 (defined $tagtype) or die '$tagtype not defined, exited';
 (defined $publish) or die '$publish not defined, exited';
 
-print "building taxonomy for $tagtype - publish: $publish\n";
-
-binmode STDERR, ":encoding(UTF-8)";
-binmode STDIN, ":encoding(UTF-8)";
-binmode STDOUT, ":encoding(UTF-8)";
-
-my $file = $tagtype . ".txt";
-
-# The nutrients_taxonomy.txt source file is created from values in the .po files
-if ($tagtype eq "nutrient_levels") {
-	create_nutrients_level_taxonomy();
+if ($tagtype eq 'all') {
+	$tagtype = '*';
 }
-
-my @files = ();
-
-# For the origins taxonomy, include the countries taxonomy
-
-if ($tagtype eq "origins") {
-
-	@files = ("countries", "origins");
-}
-
-# For the Open Food Facts ingredients taxonomy, concatenate additives, minerals, vitamins, nucleotides and other nutritional substances taxonomies
-
-elsif (($tagtype eq "ingredients") and (defined $options{product_type}) and ($options{product_type} eq "food")) {
-
-	@files = (
-		"additives_classes", "additives", "minerals", "vitamins",
-		"nucleotides", "other_nutritional_substances", "ingredients"
-	);
-}
-
-# Packaging
-
-elsif (($tagtype eq "packaging")) {
-
-	@files = ("packaging_materials", "packaging_shapes", "packaging_recycling", "preservation");
-}
-
-# Concatenate taxonomy files if needed
-
-if ((scalar @files) > 0) {
-
-	$file = "$tagtype.all.txt";
-
-	open(my $OUT, ">:encoding(UTF-8)", "$data_root/taxonomies/$file")
-		or die("Cannot write $data_root/taxonomies/$file : $!\n");
-
-	foreach my $taxonomy (@files) {
-
-		if (open(my $IN, "<:encoding(UTF-8)", "$data_root/taxonomies/$taxonomy.txt")) {
-
-			print $OUT "# $taxonomy.txt\n\n";
-
-			while (<$IN>) {
-				print $OUT $_;
-			}
-
-			print $OUT "\n\n";
-			close($IN);
-		}
-		else {
-			print STDERR "Missing $data_root/taxonomies/$taxonomy.txt\n";
-		}
-	}
-
-	close($OUT);
-}
-
-my $sha1 = Digest::SHA1->new;
-if (open(my $IN, "<", "$data_root/taxonomies/$file")) {
-	binmode($IN);
-	$sha1->addfile($IN);
-	close($IN);
-
-	my $hash = $sha1->hexdigest;
-	(-e "$data_root/cache") or mkdir("$data_root/cache", 0755);
-
-	if (-e "$data_root/cache/$tagtype.result.$hash.sto") {
-		copy("$data_root/cache/$tagtype.result.$hash.txt", "$data_root/taxonomies/$tagtype.result.txt");
-		copy("$data_root/cache/$tagtype.result.$hash.sto", "$data_root/taxonomies/$tagtype.result.sto");
-		copy("$data_root/cache/$tagtype.$hash.json","$www_root/data/taxonomies/$tagtype.json");
-		print "Obtained $tagtype from cache.\n"
-	}
-	else {
-		build_tags_taxonomy($tagtype, $file, $publish);
-
-		copy("$data_root/taxonomies/$tagtype.result.txt", "$data_root/cache/$tagtype.result.$hash.txt");
-		if ($publish) {
-			copy("$data_root/taxonomies/$tagtype.result.sto", "$data_root/cache/$tagtype.result.$hash.sto");
-		}
-		copy("$www_root/data/taxonomies/$tagtype.json","$data_root/cache/$tagtype.$hash.json");
-	}
-}
-else {
-	print STDERR "Missing $data_root/taxonomies/$file\n";
+my @files = grep { !/\..*\.txt$/ } glob("$data_root/taxonomies/$tagtype.txt");
+foreach my $taxonomy (@files) {
+	my ($tag) = fileparse($taxonomy,'\..*');
+	build_taxonomy($tag,$publish);
 }
 
 exit(0);
+
+sub build_taxonomy {
+	my ($tagtype, $publish) = @_;
+
+	print "building taxonomy for $tagtype - publish: $publish\n";
+
+	binmode STDERR, ":encoding(UTF-8)";
+	binmode STDIN, ":encoding(UTF-8)";
+	binmode STDOUT, ":encoding(UTF-8)";
+
+	my $file = $tagtype . ".txt";
+
+	# The nutrients_taxonomy.txt source file is created from values in the .po files
+	if ($tagtype eq "nutrient_levels") {
+		create_nutrients_level_taxonomy();
+	}
+
+	my @files = ();
+
+	# For the origins taxonomy, include the countries taxonomy
+
+	if ($tagtype eq "origins") {
+
+		@files = ("countries", "origins");
+	}
+
+	# For the Open Food Facts ingredients taxonomy, concatenate additives, minerals, vitamins, nucleotides and other nutritional substances taxonomies
+
+	elsif (($tagtype eq "ingredients") and (defined $options{product_type}) and ($options{product_type} eq "food")) {
+
+		@files = (
+			"additives_classes", "additives", "minerals", "vitamins",
+			"nucleotides", "other_nutritional_substances", "ingredients"
+		);
+	}
+
+	# Packaging
+
+	elsif (($tagtype eq "packaging")) {
+
+		@files = ("packaging_materials", "packaging_shapes", "packaging_recycling", "preservation");
+	}
+
+	# Concatenate taxonomy files if needed
+
+	if ((scalar @files) > 0) {
+
+		$file = "$tagtype.all.txt";
+
+		open(my $OUT, ">:encoding(UTF-8)", "$data_root/taxonomies/$file")
+			or die("Cannot write $data_root/taxonomies/$file : $!\n");
+
+		foreach my $taxonomy (@files) {
+
+			if (open(my $IN, "<:encoding(UTF-8)", "$data_root/taxonomies/$taxonomy.txt")) {
+
+				print $OUT "# $taxonomy.txt\n\n";
+
+				while (<$IN>) {
+					print $OUT $_;
+				}
+
+				print $OUT "\n\n";
+				close($IN);
+			}
+			else {
+				print STDERR "Missing $data_root/taxonomies/$taxonomy.txt\n";
+			}
+		}
+
+		close($OUT);
+	}
+
+	my $sha1 = Digest::SHA1->new;
+	if (open(my $IN, "<", "$data_root/taxonomies/$file")) {
+		binmode($IN);
+		$sha1->addfile($IN);
+		close($IN);
+
+		my $hash = $sha1->hexdigest;
+		(-e "$data_root/cache") or mkdir("$data_root/cache", 0755);
+
+		if (-e "$data_root/cache/$tagtype.result.$hash.sto") {
+			copy("$data_root/cache/$tagtype.result.$hash.txt", "$data_root/taxonomies/$tagtype.result.txt");
+			copy("$data_root/cache/$tagtype.result.$hash.sto", "$data_root/taxonomies/$tagtype.result.sto");
+			copy("$data_root/cache/$tagtype.$hash.json", "$www_root/data/taxonomies/$tagtype.json");
+			print "Obtained $tagtype from cache.\n";
+		}
+		else {
+			build_tags_taxonomy($tagtype, $file, $publish);
+
+			copy("$data_root/taxonomies/$tagtype.result.txt", "$data_root/cache/$tagtype.result.$hash.txt");
+			if ($publish) {
+				copy("$data_root/taxonomies/$tagtype.result.sto", "$data_root/cache/$tagtype.result.$hash.sto");
+			}
+			copy("$www_root/data/taxonomies/$tagtype.json", "$data_root/cache/$tagtype.$hash.json");
+		}
+	}
+	else {
+		print STDERR "Missing $data_root/taxonomies/$file\n";
+	}
+}
 
