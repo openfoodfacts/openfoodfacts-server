1943dfd4faa4348ae391fd24d0ce20747333a393
Stéphane Gigandet <stephane@openfoodfacts.org>
2023-02-08 11:07:08 +0100

    lint

--- scripts/download_matomo_logs.pl
+++ scripts/download_matomo_logs.pl
@@ -14,6 +14,8 @@ for (my $month = 1; $month <= 12; $month++) {
 	for (my $day = 1; $day <= 31; $day++) {
 		my $date = sprintf("%d-%02d-%02d", $year, $month, $day);
 		print STDERR "Downloading data for $date\n";
-		system("wget -O matomo_app.log.scan.$date 'https://analytics.openfoodfacts.org/?module=API&method=Live.getLastVisitsDetails&idSite=2&period=day&date=$date&format=JSON&token_auth=$token&filter_limit=-1'");
+		system(
+			"wget -O matomo_app.log.scan.$date 'https://analytics.openfoodfacts.org/?module=API&method=Live.getLastVisitsDetails&idSite=2&period=day&date=$date&format=JSON&token_auth=$token&filter_limit=-1'"
+		);
 	}
 }
