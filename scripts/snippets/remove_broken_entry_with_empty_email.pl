3612d5be8cdb3c0d4e95f8fe655db1717a547dd2
Stéphane Gigandet <stephane@openfoodfacts.org>
2023-02-28 18:18:14 +0100

    lint

--- scripts/snippets/remove_broken_entry_with_empty_email.pl
+++ scripts/snippets/remove_broken_entry_with_empty_email.pl
@@ -7,7 +7,7 @@ use ProductOpener::Store qw/:all/;
 
 my $emails_ref = retrieve("$data_root/users/users_emails.sto");
 
-if(defined $emails_ref->{''}) {
+if (defined $emails_ref->{''}) {
 	delete $emails_ref->{''};
 	store("$data_root/users/users_emails.sto", $emails_ref);
 }
