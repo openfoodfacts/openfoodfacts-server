24bdfa2ca530eef6c29b19eee9dc53e19c096e84
Stéphane Gigandet <stephane@openfoodfacts.org>
2023-04-04 17:37:39 +0200

    Update scripts/move_obsolete_products_to_products_obsolete_collection.pl
    Co-authored-by: Alex Garel <alex@garel.org>

--- scripts/move_obsolete_products_to_products_obsolete_collection.pl
+++ scripts/move_obsolete_products_to_products_obsolete_collection.pl
@@ -20,8 +20,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use Modern::Perl '2017';
-use utf8;
+use ProductOpener::PerlStandards;
 
 use ProductOpener::Config qw/:all/;
 use ProductOpener::Store qw/:all/;
