c456e0f60e97e3a5fe211ae54f6641cde67208cc
Alex Garel <alex@garel.org>
2023-06-01 14:09:06 +0200

    fix: linting

--- scripts/ban_abusive_ip.pl
+++ scripts/ban_abusive_ip.pl
@@ -4,41 +4,39 @@ use strict;
 
 use Storable;
 
+use ProductOpener::PerlStandards;
+
 my $blocked_ips_file = "/srv/off/logs/blocked_ips.txt";
 
 my %blocked_ips = ();
 
 my $in;
 
-if (open ($in, "<", $blocked_ips_file)) {
+if (open($in, "<", $blocked_ips_file)) {
 
-	while(<$in>) {
+	while (<$in>) {
 		my $ip = $_;
 		chomp($ip);
 		print STDERR "loaded ip: $ip\n";
 		$blocked_ips{$ip} = 1;
 	}
 }
-close ($in);
-
+close($in);
 
 my %ip = ();
 
-while (<STDIN>)
-{
-        if ($_ =~ /(^\S+) /)
-        {
+while (<STDIN>) {
+	if ($_ =~ /(^\S+) /) {
 		my $ip = $1;
 		next if $ip !~ /^[0-9\.]+$/;
-                $ip{$ip}++;
-        }
+		$ip{$ip}++;
+	}
 }
 
-foreach my $ip (sort { $ip{$a} <=> $ip{$b}} keys %ip)
-{
+foreach my $ip (sort {$ip{$a} <=> $ip{$b}} keys %ip) {
 	next if exists $blocked_ips{$ip};
 	if ($ip{$ip} > 100) {
-	        print STDERR "detected abusive $ip : $ip{$ip}\n";
+		print STDERR "detected abusive $ip : $ip{$ip}\n";
 		my $command1 = "iptables -A INPUT -s $ip -p tcp -m state --state NEW -m tcp --dport 80 -j DROP";
 		my $command2 = "iptables -A INPUT -s $ip -p tcp -m state --state NEW -m tcp --dport 443 -j DROP";
 		print STDERR $command1 . "\n";
@@ -46,7 +44,7 @@ foreach my $ip (sort { $ip{$a} <=> $ip{$b}} keys %ip)
 		system($command1);
 		system($command2);
 		my $out;
-		open ($out, ">>", $blocked_ips_file) ;
+		open($out, ">>", $blocked_ips_file);
 		print $out $ip . "\n";
 		close $out;
 	}
