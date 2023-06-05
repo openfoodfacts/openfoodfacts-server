09e86b4ccce0381363b45eb16e4a83ff69d0cb27
Monalika Patnaik <99353300+MonalikaPatnaik@users.noreply.github.com>
2023-06-05 23:27:48 +0530

    feat: Hide the Classic Packaging Field in Edit Mode (#8500)

--- cgi/product_multilingual.pl
+++ cgi/product_multilingual.pl
@@ -994,7 +994,8 @@ CSS
 
 	my @display_fields_arr;
 	foreach my $field (@fields) {
-		next if $field eq "origins";    # now displayed below allergens and traces in the ingredients section
+		# hide packaging field & origins are now displayed below allergens and traces in the ingredients section
+		next if $field eq "origins" || $field eq "packaging";
 		$log->debug("display_field", {field_name => $field, field_value => $product_ref->{$field}}) if $log->is_debug();
 		my $display_field = display_input_field($product_ref, $field, undef);
 		push(@display_fields_arr, $display_field);
