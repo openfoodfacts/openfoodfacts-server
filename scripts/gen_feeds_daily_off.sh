3f4f144a6ecbbbcd33fec3607558c1a20d49efc8
OFF <off@openfoodfacts.org>
2023-10-10 10:29:01 +0000

    fix images for carrefour import

--- scripts/gen_feeds_daily_off.sh
+++ scripts/gen_feeds_daily_off.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+cd /srv/off
+
+# load paths
+. <(perl -e 'use ProductOpener::Paths qw/:all/; print base_paths_loading_script()')
 
 cd /srv/off/scripts
 export PERL5LIB="../lib:${PERL5LIB}"
@@ -7,9 +12,9 @@ export PERL5LIB="../lib:${PERL5LIB}"
 ./gen_top_tags_per_country.pl
 #./compute_missions.pl
 ./export_database.pl
-./mongodb_dump.sh /srv2/off/html openfoodfacts 10.0.0.3 off
+./mongodb_dump.sh /srv/off/html openfoodfacts 10.0.0.3 off
 
-cd /srv2/off/html/data
+cd $OFF_PUBLIC_DATA_DIR
 gzip < en.openfoodfacts.org.products.rdf > en.openfoodfacts.org.products.rdf.gz
 gzip < fr.openfoodfacts.org.products.rdf > fr.openfoodfacts.org.products.rdf.gz
 
@@ -20,8 +25,15 @@ cd /srv/off/scripts
 
 # Small products data and images export for Docker dev environments
 # for about 1/10000th of the products contained in production.
-./export_products_data_and_images.pl --sample-mod 10000,0 --products-file /srv/off/html/exports/products.random-modulo-10000.tar.gz --images-file /srv/off/html/exports/products.random-modulo-10000.images.tar.gz
+./export_products_data_and_images.pl --sample-mod 10000,0 --products-file $OFF_PUBLIC_EXPORTS_DIR/products.random-modulo-10000.tar.gz --images-file $OFF_PUBLIC_EXPORTS_DIR/products.random-modulo-10000.images.tar.gz
 
 ./generate_dump_for_offline_apps_off.py
-cd /srv2/off/html/data/offline
+cd /srv/off/html/data/offline
 zip en.openfoodfacts.org.products.small.csv.zip en.openfoodfacts.org.products.small.csv
+
+# Exports for Carrefour
+cd /srv/off
+./scripts/export_csv_file.pl --fields code,nutrition_grades_tags --query editors_tags=carrefour --separator ';' > $OFF_PUBLIC_DATA_DIR/exports/carrefour_nutriscore.csv
+
+./scripts/export_csv_file.pl --fields code,nutrition_grades_tags --separator ';' > $OFF_PUBLIC_DATA_DIR/exports/nutriscore.csv
+
--- scripts/imports/carrefour/run_carrefour_import.sh
+++ scripts/imports/carrefour/run_carrefour_import.sh
@@ -10,6 +10,7 @@ set -e
 export PERL5LIB=lib:$PERL5LIB
 
 # load paths
+echo "Load paths"
 . <(perl -e 'use ProductOpener::Paths qw/:all/; print base_paths_loading_script()')
 
 if [[ -z "$OFF_SFTP_HOME_DIR" ]]
@@ -19,6 +20,8 @@ then
 fi
 
 DATA_TMP_DIR=$OFF_CACHE_TMP_DIR/carrefour-data
+
+# Separate image directory as we want to keep images cached for later imports
 IMAGES_TMP_DIR=$OFF_CACHE_TMP_DIR/carrefour-images
 
 SUCCESS_FILE_PATH="$OFF_PRIVATE_DATA_DIR/carrefour-import-success"
@@ -27,12 +30,12 @@ IMPORT_SINCE=$(import_since $SUCCESS_FILE_PATH)
 
 echo "IMPORT_SINCE: $IMPORT_SINCE days"
 
-# copy XML files modified in the last successful run
+# copy XML files modified since the last successful run
+echo "Copy XML files modified since the last successful run"
 rm -rf $DATA_TMP_DIR
 mkdir $DATA_TMP_DIR
 mkdir $DATA_TMP_DIR/data
 find $OFF_SFTP_HOME_DIR/carrefour/data/ -mtime -$IMPORT_SINCE -type f -name "*.xml" -exec cp {} $DATA_TMP_DIR/data/ \;
-
 
 # mv files that are not human food (cosmetics and pet food)
 # TODO: we could in fact just import them in the pro platform, and dispatch them
@@ -52,20 +55,22 @@ find $DATA_TMP_DIR/data/ -name "*.xml" -type f -exec sed -i 's/<\/TabNutXMLPF><T
 # all images we got.
 # Carrefour will soon send us CSV files with images urls, so this process will
 # eventually be replaced (i.e. not worth improving it now)
-mkdir $DATA_TMP_DIR/images
-unzip -o "$OFF_SFTP_HOME_DIR/carrefour/data/*.zip" -d "$DATA_TMP_DIR/images/"
+# create the images dir if it does not exist yet
+mkdir -p $IMAGES_TMP_DIR
+# unzip -j: create all files in destination folder, without any path
+unzip -j -o "$OFF_SFTP_HOME_DIR/carrefour/data/*.zip" -d "$IMAGES_TMP_DIR/"
+# copy images.rules used to determine the image type from the image file name
+cp ./scripts/imports/carrefour/images.rules $IMAGES_TMP_DIR
 
 # Convert Carrefour XML files to one OFF csv file
+echo "Convert Carrefour XML files to OFF csv file"
 ./scripts/imports/carrefour/convert_carrefour_data.pl $DATA_TMP_DIR/data ./scripts/imports/carrefour/Nomenclature_OpenFoodFacts.csv > $DATA_TMP_DIR/carrefour-data.tsv || exit 101;
 
 # Note: for testing, we can import products under the carrefour-test-off2 org
 
 # import data
-./scripts/import_csv_file.pl --csv_file $DATA_TMP_DIR/carrefour-data.tsv --user_id carrefour --comment "Import Carrefour" --source_id "carrefour" --source_name "Carrefour" --source_url "https://www.carrefour.fr" --manufacturer --org_id carrefour --define lc=fr 
-
-./scripts/export_csv_file.pl --fields code,nutrition_grades_tags --query editors_tags=carrefour --separator ';' > $OFF_PUBLIC_DATA_DIR/exports/carrefour_nutriscore.csv
-
-./scripts/export_csv_file.pl --fields code,nutrition_grades_tags --separator ';' > $OFF_PUBLIC_DATA_DIR/exports/nutriscore.csv
+echo "Import data"
+./scripts/import_csv_file.pl --csv_file $DATA_TMP_DIR/carrefour-data.tsv --user_id carrefour --comment "Import Carrefour" --source_id "carrefour" --source_name "Carrefour" --source_url "https://www.carrefour.fr" --manufacturer --org_id carrefour-test-off2 --define lc=fr --images_dir $IMAGES_TMP_DIR
 
 # mark successful run
 mark_successful_run $SUCCESS_FILE_PATH
