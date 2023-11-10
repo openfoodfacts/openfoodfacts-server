fc26f6cf9ee0a39a183d001d0e438288d624bea4
Pierre Slamich <pierre@openfoodfacts.org>
2023-11-10 17:42:52 +0100

    Update update_all_packager_codes.sh

--- scripts/packager-codes/update_all_packager_codes.sh
+++ scripts/packager-codes/update_all_packager_codes.sh
@@ -1,1 +1,32 @@
-# Do whatever is needed
+#!/bin/bash
+
+# Define a function for running a script with exception handling
+run_script() {
+    echo "🚀 Starting $1..."
+    if ./$1; then
+        echo "✅ Successfully executed $1."
+    else
+        echo "❌ Error occurred in $1."
+    fi
+}
+
+# List of scripts to run
+scripts=(
+    "de-packagers-refresh.pl"
+    "ee-packagers-xml2tsv.pl"
+    "es-packagers-html2csv.pl"
+    "fi-packagers-xls2csv.pl"
+    "fr-packagers-refresh.pl"
+    "hr-packagers-refresh.pl"
+    "poland_packager_code.py"
+    "portugal-concatenate-csv-sections.py"
+    "portugal-geocode.sh"
+    "se-packagers-html2tsv.pl"
+)
+
+# Run each script
+for script in "${scripts[@]}"; do
+    run_script "$script"
+done
+
+echo "🎉 All scripts executed!"
