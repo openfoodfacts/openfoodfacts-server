0b597d126e1ac31c25a7e4e9afe05bcfbeb2bbea
off <off@openfoodfacts.org>
2023-11-22 17:58:19 +0100

    update

--- scripts/utils/logs/download_matomo_logs.pl
+++ scripts/utils/logs/download_matomo_logs.pl
@@ -8,6 +8,6 @@ for (my $month = 1; $month <= 12; $month++) {
 	for (my $day = 1; $day <= 31; $day++) {
 		my $date = sprintf("%d-%02d-%02d", $year, $month, $day);
 		print STDERR "Downloading data for $date\n";
-		system("wget -O matomo_app.log.scan.$date 'https://analytics.openfoodfacts.org/?module=API&method=Live.getLastVisitsDetails&idSite=2&period=day&date=$date&format=JSON&token_auth=5e488aef77864fd10902389eadea107b&filter_limit=-1'");
+		system("wget -O matomo_app.log.scan.$date 'https://analytics.openfoodfacts.org/?module=API&method=Live.getLastVisitsDetails&idSite=2&period=day&date=$date&format=JSON&token_auth=[replace with token]&filter_limit=-1'");
 	}
 }
