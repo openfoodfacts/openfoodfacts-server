318db2332cc1856c6efae25e72e70ceac751a53b
Alex Garel <alex@garel.org>
2024-01-17 16:50:07 +0100

    chore: fix mongodb url and add refresh_products daily refresh

--- scripts/gen_feeds_daily_obf.sh
+++ scripts/gen_feeds_daily_obf.sh
@@ -3,10 +3,13 @@
 cd /srv/obf/scripts
 export PERL5LIB="../lib:${PERL5LIB}"
 
+MONGO_URL=10.1.0.102
+
 ./remove_empty_products.pl
 ./compute_missions.pl
+/usr/bin/mongosh $MONGO_URL/obf ./refresh_products_tags.js
 ./export_database.pl
-./mongodb_dump.sh /srv/obf/html openbeautyfacts 10.0.0.2 obf
+./mongodb_dump.sh /srv/obf/html openbeautyfacts $MONGO_URL obf
 
 cd /srv/obf/html/data
 gzip < en.openbeautyfacts.org.products.rdf > en.openbeautyfacts.org.products.rdf.gz
