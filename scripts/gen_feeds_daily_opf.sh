03db2ecebe8e906824bed034b4d7733378204831
Alex Garel <alex@openfoodfacts.org>
2024-01-18 12:23:08 +0100

    chore: fix mongodb url (#9667)
    and add refresh_products daily refresh

--- scripts/gen_feeds_daily_opf.sh
+++ scripts/gen_feeds_daily_opf.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+MONGO_URL=10.1.0.102
+
 cd /srv/opf/scripts
 ./remove_empty_products.pl
 ./compute_missions.pl
+/usr/bin/mongosh $MONGO_URL/opf ./refresh_products_tags.js
 ./export_database.pl
-./mongodb_dump.sh /srv/opf/html openproductsfacts 10.0.0.2 opf
+./mongodb_dump.sh /srv/opf/html openproductsfacts $MONGO_URL opf
 
 cd /srv/opf/html/data
 gzip < en.openproductsfacts.org.products.rdf > en.openproductsfacts.org.products.rdf.gz
