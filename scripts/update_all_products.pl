2474ae4c53c4c388010ddf0a8e8a714252cb36cd
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-02-07 11:40:08 +0100

    fix: code stored as string (#9767)
    $product_ref->{code} .= ''; worked if it was just before the store() call, but not inside the make_sure_numbers_as_stored_as_numbers() function.

--- scripts/update_all_products.pl
+++ scripts/update_all_products.pl
@@ -1419,9 +1419,6 @@ while (my $product_ref = $cursor->next) {
 				# In all cases (even if the product data did not change),
 				# we store the product with the new update_key in the .sto file and the mongodb collection
 
-				# make sure nutrient values are numbers
-				ProductOpener::Products::make_sure_numbers_are_stored_as_numbers($product_ref);
-
 				# Set last modified time if something was changed
 				if ($any_change) {
 					$product_ref->{last_updated_t} = time() + 0;
@@ -1432,6 +1429,9 @@ while (my $product_ref = $cursor->next) {
 						$product_ref->{last_updated_t} = $product_ref->{last_modified_t};
 					}
 				}
+
+				# make sure nutrient values are numbers
+				ProductOpener::Products::make_sure_numbers_are_stored_as_numbers($product_ref);
 
 				if (!$mongodb_to_mongodb) {
 					# Store data to .sto file
