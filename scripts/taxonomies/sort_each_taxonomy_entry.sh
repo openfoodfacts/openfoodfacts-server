71a5571f7ca1ebb8d8357a54ea4f6f1bd5930e40
Alex Garel <alex@garel.org>
2024-02-26 17:51:51 +0100

    fix: sort_each_taxonomy_entry.sh

--- scripts/taxonomies/sort_each_taxonomy_entry.sh
+++ scripts/taxonomies/sort_each_taxonomy_entry.sh
@@ -24,16 +24,18 @@ script=$(dirname $0 )"/sort_each_taxonomy_entry.pl"
 FINAL_EXIT=0;
 for taxonomy in "${FILES[@]}"
 do
-    echo "$ACTION $taxonomy ===============";
+    echo "$ACTION $taxonomy ==============="
+    rm -f $taxonomy.tmp
     # redirect output only if we're not checking
     ( \
-        [[ -z "$IS_CHECK" ]] && exec >$taxonomy; \
-        $script "${ARGS[@]}" <$taxonomy \
+        [[ -z "$IS_CHECK" ]] && exec >$taxonomy.tmp; \
+        $script "${ARGS[@]}" <$taxonomy; \
     )
-    EXIT=$?;
+    [[ -s $taxonomy.tmp ]] && mv $taxonomy.tmp $taxonomy
+    EXIT=$?
     if [[ $EXIT -ne 0 ]]
     then
-        echo "=> Error in $taxonomy";
+        echo "=> Error in $taxonomy"
         FINAL_EXIT=$EXIT;
     fi
 done
