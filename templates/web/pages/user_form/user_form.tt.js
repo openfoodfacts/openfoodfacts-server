a1a4e51d3bcfe59f3fa90bf7480f5d08161aac24
Alex Garel <alex@garel.org>
2024-03-06 13:21:31 +0100

    fix: quotes in user_form

--- templates/web/pages/user_form/user_form.tt.js
+++ templates/web/pages/user_form/user_form.tt.js
@@ -6,7 +6,7 @@
 		\$('.pro_org_display').show();
 		\$('#teams_section').hide();
 		if ( ! \$('#pro-email-warning').length ) {
-			let pro_email_warning = '<div style="color: red; font-weight: 600;" id="pro-email-warning">🚨 [% lang("email_warning") %]</div>';
+			let pro_email_warning = '<div style="color: red; font-weight: 600;" id="pro-email-warning">🚨 [% esq(lang("email_warning")) %]</div>';
 			\$('.pro_org_display').first().prepend(pro_email_warning);
 		}
 	} else {
