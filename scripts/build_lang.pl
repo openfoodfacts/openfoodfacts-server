7d7160c2fe2ac0377cdf1a01287b9b63209e49f2
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-03-07 19:01:10 +0100

    fix: generate and build nutrient_levels.txt (without build_taxonomies in pull_request.yml) (#9886)
    fix of #9883

--- scripts/build_lang.pl
+++ scripts/build_lang.pl
@@ -55,8 +55,9 @@ ProductOpener::Lang::build_json();
 
 # Nutrients level taxonomy file is built using languages
 create_nutrients_level_taxonomy();
+
 # and generate corresponding taxonomy
-build_tags_taxonomy('nutrients_levels', 1);
+build_tags_taxonomy('nutrient_levels', 1);
 
 exit(0);
 
