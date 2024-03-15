ae2dd05d449ecd7a66b98714d87840203d70b2db
anaritadauane <anarita.dauane@gmail.com>
2024-03-15 16:52:36 +0200

    fixed the formatting

--- cgi/generate_sample_import_file.pl
+++ cgi/generate_sample_import_file.pl
@@ -56,9 +56,30 @@ my $workbook = Excel::Writer::XLSX->new(\*STDOUT);
 my $worksheet = $workbook->add_worksheet();
 my %formats = (
 	normal => $workbook->add_format(border => 1, bold => 1),
-	mandatory => $workbook->add_format(border => 1, bold => 1, bg_color => '#0f55cc', color => 'white', valign => 'vcenter', align=>'center'),
-	recommended => $workbook->add_format(border => 1, bold => 1, bg_color => '#1f6ced',color => 'white', valign => 'vcenter', align=>'center'),
-	optional => $workbook->add_format(border => 1, bold => 1, bg_color => '#4d89f1', color => 'white', valign => 'vcenter', align=>'center'),
+	mandatory => $workbook->add_format(
+		border => 1,
+		bold => 1,
+		bg_color => '#0f55cc',
+		color => 'white',
+		valign => 'vcenter',
+		align => 'center'
+	),
+	recommended => $workbook->add_format(
+		border => 1,
+		bold => 1,
+		bg_color => '#1f6ced',
+		color => 'white',
+		valign => 'vcenter',
+		align => 'center'
+	),
+	optional => $workbook->add_format(
+		border => 1,
+		bold => 1,
+		bg_color => '#4d89f1',
+		color => 'white',
+		valign => 'vcenter',
+		align => 'center'
+	),
 	description => $workbook->add_format(italic => 1, text_wrap => 1, valign => 'vcenter'),
 );
 
@@ -76,7 +97,7 @@ my $comment;
 $worksheet->set_row(0, 70);
 $worksheet->set_column('A:ZZ', 30);
 $worksheet->set_column('A:A', 30);
-$worksheet->write( $description_row, 0,  $description , $formats{'description'});
+$worksheet->write($description_row, 0, $description, $formats{'description'});
 
 foreach my $group_ref (@$select2_options_ref) {
 	my $group_start_col = $col;
