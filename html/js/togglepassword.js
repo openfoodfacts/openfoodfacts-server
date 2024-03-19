f8f8b0d84e088f4579eb7e23ae2e3ff54d6cf006
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-03-19 16:59:07 +0000

    converted-togglepassword-and-user_form-into-raw-js

--- html/js/togglepassword.js
+++ html/js/togglepassword.js
@@ -1,18 +1,19 @@
+/* eslint-disable no-unused-vars */
 
 function togglePasswordVisibility(FieldID) {
     const passwordInput = document.getElementById(FieldID);
     const toggleIcon = passwordInput.nextElementSibling.querySelector(".material-icons");
-
+    
     if (passwordInput.type === "password") {
-        passwordInput.type = "text";
+        passwordInput.setAttribute("type", "text");
         if (toggleIcon) {
             toggleIcon.textContent = "visibility";
         }
-       
     } else {
-        passwordInput.type = "password";
+        passwordInput.setAttribute("type", "password");
         if (toggleIcon) {
             toggleIcon.textContent = "visibility_off";
         }
     }
 }
+
