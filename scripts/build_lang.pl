23b6bfe00cddec471645505b400cc6436fa776b7
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-03-19 19:56:05 +0100

    do not create nutrient_levels.txt automatically

--- scripts/build_lang.pl
+++ scripts/build_lang.pl
@@ -53,11 +53,5 @@ store("$data_root/data/Lang_tags.${server_domain}.sto", $tags_ref);
 # Generate JSON files for JavaScript I18N
 ProductOpener::Lang::build_json();
 
-# Nutrients level taxonomy file is built using languages
-create_nutrients_level_taxonomy();
-
-# and generate corresponding taxonomy
-build_tags_taxonomy('nutrient_levels', 1);
-
 exit(0);
 
