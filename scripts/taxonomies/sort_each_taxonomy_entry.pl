ba6fafdc32d9986c23a1b42d69388bf86dabb6ea
Alex Garel <alex@garel.org>
2024-03-28 00:13:32 +0100

    refactor: modular taxonomy linter

--- scripts/taxonomies/sort_each_taxonomy_entry.pl
+++ scripts/taxonomies/sort_each_taxonomy_entry.pl
@@ -20,23 +20,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use Modern::Perl '2017';
-use utf8;
+use ProductOpener::PerlStandards;
 
-use ProductOpener::Store qw/:all/;
-use ProductOpener::Config qw/:all/;
 
-my $is_check = grep {$_ eq "--check"} @ARGV;
-my $is_verbose = grep {$_ eq "-v"} @ARGV;
-my $has_changes = 0;
-
-binmode(STDIN, ":encoding(UTF-8)");
-binmode(STDOUT, ":encoding(UTF-8)");
-binmode(STDERR, ":encoding(UTF-8)");
-
-sub cmp_on_language ($$) {
-	my $a = shift;
-	my $b = shift;
+sub cmp_on_language :prototype($$) ($a, $b) {
 	if ((!defined $a) || (!defined $b)) {
 		return $a cmp $b;
 	}
@@ -64,28 +51,114 @@ sub cmp_on_language ($$) {
 	return $a cmp $b;
 }
 
-# read all in memory to take care of last line in a simple way
-my @lines = (<STDIN>);
+sub iter_taxonomy_lines($fd) {
+	my $last_line;
+	# iterator
+	return sub {
+		while (my $line = <$fd>) {
+			$last_line = $line;
+			return $line;
+		}
+		# be sure to end with a blank line
+		unless ($last_line =~ /^\s*$/) {
+			$last_line = "\n";  # make next call terminate
+			return "\n";
+		}
+		# end of iterator
+		return;
+	}
+}
 
-# be sure to end with a blank line
-push @lines, "\n" unless $lines[-1] =~ /^\s*$/;
 
-# structures for one entry
-my @parents = ();    # lines defining parents
-my $entry_id_line = undef;    # line defining entry id, we don't want to change it's position
-my %entries = ();    # lines defining synonyms
-my %props = ();    # lines defining properties
-my @original_lines = ();
-# non meaningful lines above a meaningful line (entry, parent or porperty)
-my @previous_lines = ();
-my $line_num = 0;
-my $entry_start_line = 1;    # tracking line number of the first line of an entry
-foreach my $line (@lines) {
-	$line_num += 1;
-	push @original_lines, $line;    # collect lines for comparison
+sub iter_taxonomy_entries ($lines_iter) {
+	my $line_num = 0;  # this is global
+	return sub {
+		# re-init at start and after returning each entry
+		my @parents = ();    # lines defining parents
+		my $entry_id_line = undef;    # line defining entry id, we don't want to change it's position
+		my %entries = ();    # lines defining synonyms
+		my %props = ();    # lines defining properties
+		my @original_lines = ();
+		# non meaningful lines above a meaningful line (entry, parent or porperty)
+		my @previous_lines = ();
+		my $entry_start_line = $line_num + 1;
+		while (my $line = $lines_iter->()) {
+			$line_num += 1;
+			push @original_lines, $line;    # collect lines for comparison
 
-	# blank line means we are changing entry, so let's print collected lines
-	if ($line =~ /^\s*$/) {
+			# blank line means we are changing entry, so let's return collected data
+			if ($line =~ /^\s*$/) {
+				my $entry = {
+					parents => \@parents,
+					entry_id_line => $entry_id_line,
+					entries => \%entries,
+					props => \%props,
+					original_lines => \@original_lines,
+					tail_lines => \@previous_lines,
+					start_line => $entry_start_line,
+					end_line => $line_num,
+				};
+				# return $entry
+				return $entry;
+			}
+			# parents
+			elsif ($line =~ /^</) {
+				push @parents, {line => $line, previous => [@previous_lines]};
+				@previous_lines = ();
+			}
+			# synonym
+			elsif ($line =~ /^(\w+):[^:]*(,.*)*$/) {
+				if (!defined $entry_id_line) {
+					$entry_id_line = {line => $line, previous => [@previous_lines], lc => $1};
+				}
+				else {
+					my $lc = $1;
+					if ((defined $entries{$lc}) || ($entry_id_line->{lc} eq $lc)) {
+						# emit a warning as this seems like a strange case
+						print STDERR "Warning: duplicate synonym for $lc, on entry line $line_num\n";
+						print STDERR "- " . ($entries{$lc}{line} // $entry_id_line->{line});
+						print STDERR "- " . $line;
+					}
+					# but try to do our best and continue
+					if (defined $entries{$lc}) {
+						$entries{$lc}{line} = $entries{$lc}{line} . $line;
+						push @{$entries{$lc}{previous}}, @previous_lines;
+					}
+					else {
+						$entries{$lc} = {line => $line, previous => [@previous_lines]};
+					}
+				}
+				@previous_lines = ();
+			}
+			# property
+			elsif ($line =~ /^(\w+):(\w+):(.*)$/) {
+				my $prop = $1;
+				my $lc = $2;
+				$props{"$prop:$lc"} = {line => $line, previous => [@previous_lines]};
+				@previous_lines = ();
+			}
+			# comments or undefined
+			else {
+				push @previous_lines, $line;
+			}
+		}
+		# end of iterator
+		return;
+	}
+}
+
+sub lint_taxonomy($entries_iterator, $is_check, $is_verbose) {
+	my $has_changes = 0;
+	while (my $entry_ref = $entries_iterator->()) {
+		my @parents = @{$entry_ref->{parents}};
+		my $entry_id_line = $entry_ref->{entry_id_line};
+		my %entries = %{$entry_ref->{entries}};
+		my %props = %{$entry_ref->{props}};
+		my @original_lines = @{$entry_ref->{original_lines}};
+		my @tail_lines = @{$entry_ref->{tail_lines}};
+		my $entry_start_line = $entry_ref->{start_line};
+		my $entry_end_line = $entry_ref->{end_line};
+		# eventual result
 		my @output_lines = ();
 		# sort items
 		@parents = sort {$a->{line} cmp $b->{line}} @parents;
@@ -108,10 +181,9 @@ foreach my $line (@lines) {
 			push @output_lines, @{$props{$key}->{previous}};
 			push @output_lines, $props{$key}->{line};
 		}
-		# print remaining previous_lines (if any)
-		push @output_lines, @previous_lines;
-		# print this blank line
-		push @output_lines, $line;
+		push @output_lines, @tail_lines;
+		# print a blank line
+		push @output_lines, "\n";
 		my $original = join("", @original_lines);
 		my $output = join("", @output_lines);
 		if ($is_check) {
@@ -119,7 +191,7 @@ foreach my $line (@lines) {
 			if (not $original eq $output) {
 				$has_changes = 1;
 				if ($is_verbose) {
-					print "Error: output is not the same as original, line $entry_start_line..$line_num\n";
+					print "Error: output is not the same as original, line $entry_start_line..$entry_end_line\n";
 					print "Original --------------------\n";
 					print "$original\n";
 					print "Sorted --------------------\n";
@@ -130,55 +202,20 @@ foreach my $line (@lines) {
 		else {
 			print "$output";
 		}
-		# re-init
-		$entry_id_line = undef;
-		@parents = ();
-		%entries = ();
-		%props = ();
-		@previous_lines = ();
-		@original_lines = ();
-		$entry_start_line = $line_num;
 	}
-	# parents
-	elsif ($line =~ /^</) {
-		push @parents, {line => $line, previous => [@previous_lines]};
-		@previous_lines = ();
-	}
-	# synonym
-	elsif ($line =~ /^(\w+):[^:]*(,.*)*$/) {
-		if (!defined $entry_id_line) {
-			$entry_id_line = {line => $line, previous => [@previous_lines], lc => $1};
-		}
-		else {
-			my $lc = $1;
-			if ((defined $entries{$lc}) || ($entry_id_line->{lc} eq $lc)) {
-				# emit a warning as this seems like a strange case
-				print STDERR "Warning: duplicate synonym for $lc, on entry line $line_num\n";
-				print STDERR "- " . ($entries{$lc}{line} // $entry_id_line->{line});
-				print STDERR "- " . $line;
-			}
-			# but try to do our best and continue
-			if (defined $entries{$lc}) {
-				$entries{$lc}{line} = $entries{$lc}{line} . $line;
-				push @{$entries{$lc}{previous}}, @previous_lines;
-			}
-			else {
-				$entries{$lc} = {line => $line, previous => [@previous_lines]};
-			}
-		}
-		@previous_lines = ();
-	}
-	# property
-	elsif ($line =~ /^(\w+):(\w+):(.*)$/) {
-		my $prop = $1;
-		my $lc = $2;
-		$props{"$prop:$lc"} = {line => $line, previous => [@previous_lines]};
-		@previous_lines = ();
-	}
-	# comments or undefined
-	else {
-		push @previous_lines, $line;
-	}
+	return $has_changes;
 }
 
-exit($is_check and $has_changes);
+unless (caller) {
+	# main
+	my $is_check = grep {$_ eq "--check"} @ARGV;
+	my $is_verbose = grep {$_ eq "-v"} @ARGV;
+
+	binmode(STDIN, ":encoding(UTF-8)");
+	binmode(STDOUT, ":encoding(UTF-8)");
+	binmode(STDERR, ":encoding(UTF-8)");
+
+	my $entries_iterator = iter_taxonomy_entries(iter_taxonomy_lines(*STDIN));
+	my $has_changes = lint_taxonomy($entries_iterator, $is_check, $is_verbose);
+	exit($is_check and $has_changes);
+}
