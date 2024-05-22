5a26d429babd3d049df6ba459e13ab48e006ec2c
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-05-22 18:47:39 +0200

    fix checkperl

--- lib/ProductOpener/Products.pm
+++ lib/ProductOpener/Products.pm
@@ -3527,7 +3527,7 @@ sub remove_fields ($product_ref, $fields_ref) {
 	return;
 }
 
-=head2 add_images_urls_to_product ($product_ref, $target_lc, $imagetype = undef)
+=head2 add_images_urls_to_product ($product_ref, $target_lc, $specific_imagetype = undef)
 
 Add fields like image_[front|ingredients|nutrition|packaging]_[url|small_url|thumb_url] to a product object.
 
@@ -3544,13 +3544,13 @@ Reference to a complete product a subfield.
 
 2 language code of the preferred language for the product images.
 
-=head4 $imagetype
+=head4 $specific_imagetype
 
 Optional parameter to specify the type of image to add. Default is to add all types.
 
 =cut
 
-sub add_images_urls_to_product ($product_ref, $target_lc, $imagetype = undef) {
+sub add_images_urls_to_product ($product_ref, $target_lc, $specific_imagetype = undef) {
 
 	my $images_subdomain = format_subdomain('images');
 
@@ -3558,14 +3558,14 @@ sub add_images_urls_to_product ($product_ref, $target_lc, $imagetype = undef) {
 
 	# If $imagetype is specified (e.g. "front" when we display a list of products), only compute the image for this type
 	my @imagetypes;
-	if (defined $imagetype) {
-		@imagetypes = ($imagetype);
+	if (defined $specific_imagetype) {
+		@imagetypes = ($specific_imagetype);
 	}
 	else {
 		@imagetypes = ('front', 'ingredients', 'nutrition', 'packaging');
 	}
 
-	foreach $imagetype (@imagetypes) {
+	foreach my $imagetype (@imagetypes) {
 
 		my $size = $display_size;
 
