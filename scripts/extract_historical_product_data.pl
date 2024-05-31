1a8cca4950c0d49d8a4dcbe769d39644d0ab63f7
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-05-31 10:18:55 +0200

    it works

--- scripts/extract_historical_product_data.pl
+++ scripts/extract_historical_product_data.pl
@@ -94,61 +94,12 @@ use Log::Any::Adapter 'TAP';
 
 use Getopt::Long;
 
-my @fields_to_update = ();
-my $key;
-my $index = '';
-my $count = '';
-my $just_print_codes = '';
-my $pretend = '';
-my $analyze_and_enrich_product_data = '';
-my $process_ingredients = '';
-my $process_packagings = '';
-my $clean_ingredients = '';
-my $compute_nutriscore = '';
-my $compute_serving_size = '';
-my $compute_data_sources = '';
-my $compute_nova = '';
-my $check_quality = '';
-my $compute_codes = '';
-my $compute_carbon = '';
-my $compute_history = '';
-my $compute_sort_key = '';
-my $comment = '';
-my $fix_serving_size_mg_to_ml = '';
-my $fix_missing_lc = '';
-my $fix_zulu_lang = '';
-my $fix_rev_not_incremented = '';
-my $fix_yuka_salt = '';
-my $run_ocr = '';
-my $autorotate = '';
-my $remove_team = '';
-my $remove_label = '';
-my $remove_category = '';
-my $remove_nutrient = '';
-my $remove_old_carbon_footprint = '';
-my $fix_spanish_ingredientes = '';
-my $team = '';
-my $assign_categories_properties = '';
-my $restore_values_deleted_by_user = '';
-my $delete_debug_tags = '';
-my $all_owners = '';
-my $mark_as_obsolete_since_date = '';
-my $reassign_energy_kcal = '';
-my $delete_old_fields = '';
-my $mongodb_to_mongodb = '';
-my $compute_ecoscore = '';
-my $compute_forest_footprint = '';
-my $fix_nutrition_data_per = '';
-my $fix_nutrition_data = '';
-my $compute_main_countries = '';
-my $prefix_packaging_tags_with_language = '';
-my $fix_non_string_ids = '';
-my $fix_non_string_codes = '';
-my $fix_string_last_modified_t = '';
-my $assign_ciqual_codes = '';
-my $obsolete = 0;
-my $fix_obsolete;
-my $fix_last_modified_t;    # Will set the update key and ensure last_updated_t is initialised
+my $analyze_and_enrich_product_data;
+my $codes_file;
+my $field_to_extract;
+my $min_year;
+my $max_year;
+my $recompute_taxonomies;
 
 my $query_ref = {};    # filters for mongodb query
 
@@ -156,9 +107,12 @@ GetOptions(
 	# We can extract data for products matching a specific query, or for a list of product codes
 	"query=s%" => $query_ref,    # filters for mongodb query
 	"codes-file=s" => \$codes_file,    # file with product codes to extract data for
-	"field=s" => \@field,
-	"index" => \$index,
+	"field=s" => \$field_to_extract,
+	"recompute-taxonomies" =>
+		\$recompute_taxonomies,    # Recompute tag fields like categories and labels with the current taxonomies
 	"analyze-and-enrich-product-data" => \$analyze_and_enrich_product_data,
+	"min-year=i" => \$min_year,
+	"max-year=i" => \$max_year,
 ) or die("Error in command line arguments:\n\n$usage");
 
 my @codes = ();
@@ -171,137 +125,85 @@ if (defined $codes_file) {
 		push @codes, $row;
 	}
 	close $fh;
-}
+	my $products_count = scalar @codes;
+	print STDERR "$products_count documents retrieved from MongoDB\n";
 
-# Use query filters entered using --query categories_tags=en:plant-milks
-
-use boolean;
-
-foreach my $field (sort keys %{$query_ref}) {
-
-	my $not = 0;
-
-	if ($query_ref->{$field} =~ /^-/) {
-		$query_ref->{$field} = $';
-		$not = 1;
-	}
-
-	if ($query_ref->{$field} eq 'null') {
-		# $query_ref->{$field} = { '$exists' => false };
-		$query_ref->{$field} = undef;
-	}
-	elsif ($query_ref->{$field} eq 'exists') {
-		$query_ref->{$field} = {'$exists' => true};
-	}
-	elsif ($field =~ /_t$/) {    # created_t, last_modified_t etc.
-		$query_ref->{$field} += 0;
-	}
-	# Multiple values separated by commas
-	elsif ($query_ref->{$field} =~ /,/) {
-		my @tagids = split(/,/, $query_ref->{$field});
-
-		if ($not) {
-			$query_ref->{$field} = {'$nin' => \@tagids};
-		}
-		else {
-			$query_ref->{$field} = {'$in' => \@tagids};
-		}
-	}
-	elsif ($not) {
-		$query_ref->{$field} = {'$ne' => $query_ref->{$field}};
-	}
-}
-
-# Query products that have the _id field stored as a number
-if ($fix_non_string_ids) {
-	$query_ref->{_id} = {'$type' => "long"};
-}
-
-# Query products that have the _id field stored as a number
-if ($fix_non_string_codes) {
-	$query_ref->{code} = {'$type' => "long"};
-}
-
-# Query products that have the last_modified_t field stored as a number
-if ($fix_string_last_modified_t) {
-	$query_ref->{last_modified_t} = {'$type' => "string"};
-}
-
-# On the producers platform, require --query owners_tags to be set, or the --all-owners field to be set.
-
-if ((defined $server_options{private_products}) and ($server_options{private_products})) {
-	if ((not $all_owners) and (not defined $query_ref->{owners_tags})) {
-		print STDERR "On producers platform, --query owners_tags=... or --all-owners must be set.\n";
-		exit();
-	}
-}
-
-if ((defined $remove_team) and ($remove_team ne "")) {
-	$query_ref->{teams_tags} = $remove_team;
-}
-
-if ((defined $remove_label) and ($remove_label ne "")) {
-	$query_ref->{labels_tags} = $remove_label;
-}
-
-if ((defined $remove_category) and ($remove_category ne "")) {
-	$query_ref->{categories_tags} = $remove_category;
-}
-
-if (defined $key) {
-	$query_ref->{update_key} = {'$ne' => "$key"};
 }
 else {
-	$key = "key_" . time();
+	# Use query filters entered using --query categories_tags=en:plant-milks
+
+	use boolean;
+
+	foreach my $field (sort keys %{$query_ref}) {
+
+		my $not = 0;
+
+		if ($query_ref->{$field} =~ /^-/) {
+			$query_ref->{$field} = $';
+			$not = 1;
+		}
+
+		if ($query_ref->{$field} eq 'null') {
+			# $query_ref->{$field} = { '$exists' => false };
+			$query_ref->{$field} = undef;
+		}
+		elsif ($query_ref->{$field} eq 'exists') {
+			$query_ref->{$field} = {'$exists' => true};
+		}
+		elsif ($field =~ /_t$/) {    # created_t, last_modified_t etc.
+			$query_ref->{$field} += 0;
+		}
+		# Multiple values separated by commas
+		elsif ($query_ref->{$field} =~ /,/) {
+			my @tagids = split(/,/, $query_ref->{$field});
+
+			if ($not) {
+				$query_ref->{$field} = {'$nin' => \@tagids};
+			}
+			else {
+				$query_ref->{$field} = {'$in' => \@tagids};
+			}
+		}
+		elsif ($not) {
+			$query_ref->{$field} = {'$ne' => $query_ref->{$field}};
+		}
+	}
+
+	use Data::Dumper;
+	print STDERR "MongoDB query:\n" . Dumper($query_ref);
+
+	my $socket_timeout_ms = 2 * 60000;    # 2 mins, instead of 30s default, to not die as easily if mongodb is busy.
+
+	# Collection that will be used to iterate products
+	# TODO: we should query both the products and obsolete products collections
+	my $products_collection = get_products_collection({obsolete => 0, timeout => $socket_timeout_ms});
+
+	my $products_count = "";
+
+	eval {
+		$products_count = $products_collection->count_documents($query_ref);
+
+		print STDERR "$products_count documents retrieved from MongoDB\n";
+	};
+
+	my $cursor = $products_collection->query($query_ref)->fields({_id => 1, code => 1, owner => 1});
+	$cursor->immortal(1);
+
+	while (my $product_ref = $cursor->next) {
+		push @codes, $product_ref->{code};
+	}
 }
-
-# $query_ref->{unknown_nutrients_tags} = { '$exists' => true,  '$ne' => [] };
-
-print STDERR "Update key: $key\n\n";
-
-use Data::Dumper;
-print STDERR "MongoDB query:\n" . Dumper($query_ref);
-
-my $socket_timeout_ms = 2 * 60000;    # 2 mins, instead of 30s default, to not die as easily if mongodb is busy.
-
-# Collection that will be used to iterate products
-my $products_collection = get_products_collection({obsolete => $obsolete, timeout => $socket_timeout_ms});
-
-# Collections for saving current / obsolete products
-my %products_collections = (
-	current => get_products_collection({timeout => $socket_timeout_ms}),
-	obsolete => get_products_collection({obsolete => $obsolete, timeout => $socket_timeout_ms}),
-);
-
-my $products_count = "";
-
-eval {
-	$products_count = $products_collection->count_documents($query_ref);
-
-	print STDERR "$products_count documents to update.\n";
-};
-
-if ($count) {exit(0);}
-
-my $cursor = $products_collection->query($query_ref)->fields({_id => 1, code => 1, owner => 1});
-$cursor->immortal(1);
 
 # load data needed to analyze and enrich products
 if ($analyze_and_enrich_product_data) {
 	load_data();
 }
 
-while (my $product_ref = $cursor->next) {
-	push @codes, $product_ref->{code};
-}
-
 # Go through all products
 foreach my $code (@codes) {
 
 	my $productid = $code;
-	my $path = product_path_from_id($product_id);
-
-	$product_ref = retrieve_product($productid);
+	my $path = product_path_from_id($productid);
 
 	# Go through all product revisions
 
@@ -317,7 +219,7 @@ foreach my $code (@codes) {
 	# Go through all revisions, keep the latest value of all fields
 
 	my %deleted_values = ();
-	my $previous_rev_product_ref = {};
+	my $previous_product_ref = {};
 	my $revs = 0;
 
 	foreach my $change_ref (@{$changes_ref}) {
@@ -327,18 +229,88 @@ foreach my $code (@codes) {
 			$rev = $revs;    # was not set before June 2012
 		}
 
-		my $rev_product_ref = retrieve("$BASE_DIRS{PRODUCTS}/$path/$rev.sto");
+		my $product_ref = retrieve("$BASE_DIRS{PRODUCTS}/$path/$rev.sto");
 
-		if (defined $rev_product_ref) {
+		if (defined $product_ref) {
+
+			my $value;
+
+			# Make sure we have a lc field
+			if ((not defined $product_ref->{lc}) and (defined $product_ref->{lang})) {
+				$product_ref->{lc} = $product_ref->{lang};
+			}
+
+			# Determine the year of the revision, using the UNIX timestamp last_modified_t
+			my $year = (localtime($product_ref->{last_modified_t}))[5] + 1900;
 
 			# This option runs all the data enrichment functions
 			if ($analyze_and_enrich_product_data) {
 				analyze_and_enrich_product_data($product_ref);
 			}
 
-			$previous_rev_product_ref = $rev_product_ref;
+			# Value of a tag field that has a specific prefix
+			# e.g. labels_tags=en:nutriscore-
+			# We will take the value of the first tag that has the prefix
+
+			if ($field_to_extract =~ /^(.+)_tags=(.+)$/) {
+				my $tagtype = $1;
+				my $prefix = $2;
+				# *_tags fields may contain old canonical tags, we can recompute tag fields with the newest taxonomy
+				if (($recompute_taxonomies) and (defined $taxonomy_fields{$tagtype})) {
+					# if the field was previously not taxonomized, the $field_hierarchy field does not exist
+					# assume the $field value is in the main language of the product
+					if (
+
+						defined $product_ref->{$tagtype . "_hierarchy"}
+						)
+					{
+						# we do not know the language of the current value of $product_ref->{$tagtype}
+						# so regenerate it in the main language of the product
+
+						$product_ref->{$tagtype}
+							= list_taxonomy_tags_in_language($lc, $tagtype, $product_ref->{$tagtype . "_hierarchy"});
+					}
+
+					compute_field_tags($product_ref, $lc, $tagtype);
+				}
+				foreach my $tag (@{$product_ref->{$tagtype . "_tags"}}) {
+					if ($tag =~ /^$prefix/) {
+						$value = $tag;
+						last;
+					}
+				}
+			}
+			else {
+				# Access any field in the product
+				# e.g. nutriments.energy_100g
+				$value = deep_get($product_ref, split(/\./, $field_to_extract));
+			}
+
+			# We consider the value at the beginning of the year
+			# So the last value of the previous year is the value for the current year
+			if ($value) {
+				$value_per_year{$year + 1} = $value;
+			}
+			else {
+				delete($value_per_year{$year + 1});
+			}
+
+			# Keep a reference to the previous rev, if we want to compute changes between revisions
+			$previous_product_ref = $product_ref;
 		}
 	}
+
+	# Assign values for missing years and output values between min_year and max_year
+	my $values = "";
+	for (my $year = 2000; $year <= $max_year; $year++) {
+		if (not defined $value_per_year{$year}) {
+			$value_per_year{$year} = $value_per_year{$year - 1};
+		}
+		if ($year >= $min_year) {
+			$values .= "\t" . ($value_per_year{$year} || '');
+		}
+	}
+	print $code . $values . "\n";
 }
 
 exit(0);
