732aa5e88bd5237b2b93c4bc9152a50a191f5c57
TheSussex <successologunsua@gmail.com>
2024-06-11 23:21:21 +0100

    initial commit

---
+++ cgi/display_org_table.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl -w
+
+# This file is part of Product Opener.
+#
+# Product Opener
+# Copyright (C) 2011-2023 Association Open Food Facts
+# Contact: contact@openfoodfacts.org
+# Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
+#
+# Product Opener is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use ProductOpener::PerlStandards;
+
+use CGI::Carp qw(fatalsToBrowser);
+use ProductOpener::Config qw/:all/;
+use ProductOpener::Display qw/:all/;
+use ProductOpener::Lang qw/:all/;
+use ProductOpener::Data qw/:all/;
+use Log::Any qw($log);
+
+my $request_ref = ProductOpener::Display::init_request();
+
+my $orgs_collection = get_orgs_collection();
+my @orgs = $orgs_collection->find->all;
+
+my $template_data_ref = { lang => \&lang, orgs => \@orgs };
+
+my $html;
+process_template('web/pages/dashboard/display_orgs_table.tt.html', $template_data_ref, \$html) or $html = '';
+if ($tt->error()) {
+    $html .= '<p>' . $tt->error() . '</p>';
+}
+
+$request_ref->{title} = "Organization List";
+$request_ref->{content_ref} = \$html;
+display_page($request_ref);
