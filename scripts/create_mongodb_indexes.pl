3f9be95f43ec3972c0d14f5723e5ef65343928fb
John Gomersall <thegoms@gmail.com>
2024-06-20 17:29:29 +0100

    Drop indexes that aren't needed anymore
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- scripts/create_mongodb_indexes.pl
+++ scripts/create_mongodb_indexes.pl
@@ -22,77 +22,130 @@
 
 use ProductOpener::PerlStandards;
 use ProductOpener::Data qw/get_products_collection/;
-use Tie::IxHash qw/tie/;
 
 my @index_list = ();
 
 sub add_index {
-	my %index_def;
-	my $t = tie %index_def, 'Tie::IxHash';
-
-	while (@_) {
-		my $field = shift;
-		my $direction = shift;
-		$index_def{$field} = $direction;
-	}
-	push(@index_list, {keys => $t, options => {background => 1}});
+	push(@index_list, \@_);
 	return 1;
 }
 
-my $indexes = get_products_collection()->indexes;
-
-foreach my $tag (
-	'_keywords',
-	'additives_tags', 'allergens_tags', 'amino_acids_tags',
-	'brands_tags', 'categories_properties_tags',
-	'categories_tags', 'checkers_tags',
-	'cities_tags', 'codes_tags',
-	'correctors_tags', 'countries_tags',
-	'creator',    # Not in production 'creator_tags',
-	'data_quality_bugs_tags', 'data_quality_errors_tags',
-	'data_quality_info_tags', 'data_quality_tags',
-	'data_quality_warnings_tags', 'data_sources_tags',
-	'debug_tags',
-	'ecoscore_tags', 'editors_tags',
-	'emb_codes_tags', 'entry_dates_tags',
-	'food_groups_tags', 'informers_tags',
-	'ingredients_analysis_tags', 'ingredients_from_palm_oil_tags',
-	# not in production 'ingredients_n_tags', 
-	'ingredients_tags',
-	# not in production 'ingredients_that_may_be_from_palm_oil_tags',
-	'labels_tags', 'languages_tags',
-	'last_edit_dates_tags', # not in production 'last_image_dates_tags',
-	'manufacturing_places_tags', 'minerals_tags',
-	'misc_tags', 'nova_groups_tags',
-	'nucleotides_tags', # not in production 'nutrient_levels_tags',
-	'nutrition_grades_tags', 'origins_tags',
-	'other_nutritional_substances_tags', 'owners_tags', 'packaging_tags',
-	'photographers_tags', # not in production 'pnns_groups_1_tags',
-	# not in production 'pnns_groups_2_tags',
-	'popularity_tags',
-	'purchase_places_tags', 'states_tags',
-	'stores_tags', 'teams_tags',
-	'traces_tags', # not in production 'unknown_nutrients_tags',
-	'users_tags', 'vitamins_tags'
-	)
-{
-	add_index($tag, 1, 'last_modified_t', -1);
-}
-
+add_index('_keywords', 1, 'last_modified_t', -1);
 add_index('_keywords', 1, 'unique_scans_n', -1);
+add_index('additives_tags', 1, 'last_modified_t', -1);
+add_index('allergens_tags', 1, 'last_modified_t', -1);
+add_index('amino_acids_tags', 1, 'last_modified_t', -1);
+add_index('brands_tags', 1, 'last_modified_t', -1);
+add_index('categories_properties_tags', 1, 'last_modified_t', -1);
+add_index('categories_tags', 1, 'last_modified_t', -1);
+add_index('checkers_tags', 1, 'last_modified_t', -1);
+add_index('cities_tags', 1, 'last_modified_t', -1);
 add_index('code', 1);
+add_index('codes_tags', 1, 'last_modified_t', -1);
+add_index('correctors_tags', 1, 'last_modified_t', -1);
 add_index('countries_tags', 1, 'created_t', -1);
+add_index('countries_tags', 1, 'last_modified_t', -1);
+add_index('countries_tags', 1, 'popularity_key', -1);
+add_index('countries_tags', 1, 'sortkey', -1);
 add_index('created_t', 1);
+add_index('creator', 1, 'last_modified_t', -1);
+add_index('data_quality_bugs_tags', 1, 'last_modified_t', -1);
+add_index('data_quality_errors_tags', 1, 'last_modified_t', -1);
+add_index('data_quality_info_tags', 1, 'last_modified_t', -1);
+add_index('data_quality_tags', 1, 'last_modified_t', -1);
+add_index('data_quality_warnings_tags', 1, 'last_modified_t', -1);
+add_index('data_sources_tags', 1, 'last_modified_t', -1);
+add_index('debug_tags', 1, 'last_modified_t', -1);
 add_index('ecoscore_score', -1);
+add_index('ecoscore_tags', 1, 'last_modified_t', -1);
+add_index('editors_tags', 1, 'last_modified_t', -1);
+add_index('emb_codes_tags', 1, 'last_modified_t', -1);
+add_index('entry_dates_tags', 1, 'last_modified_t', -1);
+add_index('food_groups_tags', 1, 'last_modified_t', -1);
+add_index('informers_tags', 1, 'last_modified_t', -1);
+add_index('ingredients_analysis_tags', 1, 'last_modified_t', -1);
+add_index('ingredients_from_palm_oil_tags', 1, 'last_modified_t', -1);
+add_index('ingredients_tags', 1, 'last_modified_t', -1);
+add_index('labels_tags', 1, 'last_modified_t', -1);
+add_index('languages_tags', 1, 'last_modified_t', -1);
+add_index('last_edit_dates_tags', 1, 'last_modified_t', -1);
 add_index('last_modified_t', -1);
 add_index('lc', 1);
+add_index('manufacturing_places_tags', 1, 'last_modified_t', -1);
+add_index('minerals_tags', 1, 'last_modified_t', -1);
+add_index('misc_tags', 1, 'last_modified_t', -1);
+add_index('nova_groups_tags', 1, 'last_modified_t', -1);
+add_index('nucleotides_tags', 1, 'last_modified_t', -1);
 add_index('nutriscore_score_opposite', -1);
+add_index('nutrition_grades_tags', 1, 'last_modified_t', -1);
+add_index('origins_tags', 1, 'last_modified_t', -1);
+add_index('other_nutritional_substances_tags', 1, 'last_modified_t', -1);
+add_index('owners_tags', 1, 'last_modified_t', -1);
+add_index('packaging_tags', 1, 'last_modified_t', -1);
+add_index('photographers_tags', 1, 'last_modified_t', -1);
 add_index('popularity_key', -1);
+add_index('popularity_tags', 1, 'last_modified_t', -1);
+add_index('purchase_places_tags', 1, 'last_modified_t', -1);
+add_index('states_tags', 1, 'last_modified_t', -1);
+add_index('stores_tags', 1, 'last_modified_t', -1);
+add_index('teams_tags', 1, 'last_modified_t', -1);
+add_index('traces_tags', 1, 'last_modified_t', -1);
 add_index('unique_scans_n', -1);
+add_index('users_tags', 1, 'last_modified_t', -1);
+add_index('vitamins_tags', 1, 'last_modified_t', -1);
 
-# not in production add_index('countries_tags', 1, 'popularity_key', -1);
-# not in production add_index('owner', 1, 'countries_tags', 1, 'last_modified_t', -1);
+# The following were found in this file but are not in production
+# If any need to be added then a corresponding number of the above must be removed
 
-$indexes->create_many(@index_list);
+#add_index('creator_tags', 1, 'last_modified_t', -1);
+#add_index('ingredients_n_tags', 1, 'last_modified_t', -1);
+#add_index('ingredients_that_may_be_from_palm_oil_tags', 1, 'last_modified_t', -1);
+#add_index('last_image_dates_tags', 1, 'last_modified_t', -1);
+#add_index('nutrient_levels_tags', 1, 'last_modified_t', -1);
+#add_index('pnns_groups_1_tags', 1, 'last_modified_t', -1);
+#add_index('pnns_groups_2_tags', 1, 'last_modified_t', -1);
+#add_index('unknown_nutrients_tags', 1, 'last_modified_t', -1);
+#add_index('owner', 1, 'countries_tags', 1, 'last_modified_t', -1);
+
+die "Cannot have more than 63 indexes" if (@index_list > 63);
+
+# Note need to disable timeout below as index creation can take a long time
+my $indexes = get_products_collection({timeout => 0})->indexes;
+
+# Drop indexes not in the list
+my $result = $indexes->list;
+while (my $existing_index = $result->next) {
+	next if ($existing_index->{'name'} eq '_id_');
+	my $existing_keys = join(',', %{$existing_index->{'key'}});
+	my $match = 0;
+	foreach my $new_index (@index_list) {
+		my $new_keys = join(',', @{$new_index});
+		if ($new_keys eq $existing_keys) {
+			$match = 1;
+			# Remove from list if already exists
+			@index_list = grep {$_ != $new_index} @index_list;
+			last;
+		}
+	}
+	if (!$match) {
+		print "Dropping index: $existing_index->{'name'}\n";
+		# Note, this will fail if another index is still being built
+		$indexes->drop_one($existing_index->{'name'});
+	}
+}
+
+foreach my $new_index (@index_list) {
+	my $new_keys = join(',', @{$new_index});
+	print "Creating index: $new_keys\n";
+	eval {
+		$indexes->create_one($new_index, {background => 1});
+		1;
+	} or do {
+		# Timeouts are expected on large databases. The index build will continue in the background
+		if ($@ !~ m/MongoDB::NetworkTimeout/) {
+			print "$@\n";
+		}
+	}
+}
 
 exit(0);
