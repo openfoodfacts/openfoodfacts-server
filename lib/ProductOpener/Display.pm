a3fc2048bee797f2bc39799e9eadd7ed910f0278
Antoine B <antoine.bastos@edu.univ-eiffel.fr>
2024-06-26 11:45:31 +0200

    fix: make facets URLS /nova-group/unknown and /nova-group/not-applicable work

--- lib/ProductOpener/Display.pm
+++ lib/ProductOpener/Display.pm
@@ -4819,6 +4819,8 @@ sub add_params_to_query ($request_ref, $query_ref) {
 						if $log->is_debug();
 
 					# if the value is "unknown", we need to add a condition on the field being empty
+
+					my @tagtype_allowing_unknown_as_value = qw(nutrition_grades nova_groups ecoscore);
 					# warning: unknown is a value for pnns_groups_1 and 2
 					if (
 						(
@@ -4831,7 +4833,7 @@ sub add_params_to_query ($request_ref, $query_ref) {
 							)
 						)
 						and ($tagtype !~ /^pnns_groups_/)
-						and ($tagtype ne "creator")
+						and not(grep {$_ eq $tagtype} @tagtype_allowing_unknown_as_value)
 						)
 					{
 						if ($not) {
@@ -4844,6 +4846,10 @@ sub add_params_to_query ($request_ref, $query_ref) {
 					}
 					# Normal single value (not unknown)
 					else {
+						if ($tagtype eq 'nova_groups') {
+							# Remove language code. e.g. for nova_groups : en:not-applicable -> not-applicable
+							$tagid =~ s/^[a-z]{2}://;
+						}
 						if ($not) {
 							$query_ref->{$tagtype . $suffix} = {'$ne' => $tagid};
 						}
