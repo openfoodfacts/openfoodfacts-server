f570a00cae938c95de9b5803e32daa903ee37e0d
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-07-15 17:59:30 +0200

    fix: add product_type to redis events

--- lib/ProductOpener/Redis.pm
+++ lib/ProductOpener/Redis.pm
@@ -133,7 +133,7 @@ sub push_to_redis_stream ($user_id, $product_ref, $action, $comment, $diffs) {
 				# fields
 				'code', Encode::encode_utf8($product_ref->{code}),
 				# product_type should be used over flavor (kept for backward compatibility)
-				'product_type', $product_ref->{product_type},
+				'product_type', $options{product_type},
 				'flavor', $options{current_server},
 				'user_id', Encode::encode_utf8($user_id), 'action', Encode::encode_utf8($action),
 				'comment', Encode::encode_utf8($comment), 'diffs', encode_json($diffs)
