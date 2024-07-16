4bcda3d9532ac9cf435df8a98d2741158bddbcdf
benbenben2 <110821832+benbenben2@users.noreply.github.com>
2024-07-16 18:10:27 +0200

    feat: packaging add Ireland (#10533)
    ### What
    packaging codes adds Ireland
    
    ### Screenshot
    
    ![Screenshot_20240710_173536](https://github.com/openfoodfacts/openfoodfacts-server/assets/110821832/e0eb280e-5018-4daa-be72-cf0e48256762)
    
    ### Related issue(s) and discussion
    Part of https://github.com/openfoodfacts/openfoodfacts-server/issues/338
    
    More examples: #8921, #8958, #10264, #10318, #10351, #10388, #10485:
    - lib/ProductOpener/Display.pm
    add description (name, street, city) based on columns in the file or
    hardcoded
    - lib/ProductOpener/PackagerCodes.pm
    add country and suffix of the code
    - scripts/update_packager_codes.pl
    add code formatting ('country' 'code' 'suffix', for example if code does
    not already contain 'country' or 'suffix')
    add the column name for the $code variable
    - packager-codes/
    add the csv file (mind the naming)
    - scripts/packager-codes/
    add your script
    - update sto files
    ```
    docker exec -it po_off-backend-1 bash
    ./scripts/update_packager_codes.pl
    ```
    
    Based on the experience acquired in previous PR, I did the following
    changes:
    -> switch from geocode to nominatim (+ no need of API key, +/- exactly
    same results)
    -> reintroduced cache (introduced for Slovenija, #10124, and not used
    afterward)
    -> handled whole process without manual intervention (to fetch files,
    _etc_.), using Excel to dataframe feature from polars and using
    beautiful soup, not sure that this will be possible to do the same for
    future countries but at least for that one it was successful.
    
    Fixes: #1572

--- lib/ProductOpener/Display.pm
+++ lib/ProductOpener/Display.pm
@@ -4020,6 +4020,15 @@ HTML
 						;
 				}
 
+				if ($packager_codes{$canon_tagid}{cc} eq 'ie') {
+					$description .= <<HTML
+<p>$packager_codes{$canon_tagid}{name}<br>
+$packager_codes{$canon_tagid}{address} (Ireland)
+</p>
+HTML
+						;
+				}
+
 				if ($packager_codes{$canon_tagid}{cc} eq 'si') {
 					$description .= <<HTML
 <p>$packager_codes{$canon_tagid}{name}<br>
