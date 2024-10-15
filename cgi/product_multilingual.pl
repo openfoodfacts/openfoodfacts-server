42ecbb3e19cafd0090bef49fea57ef089248721e
hangy <hangy@hangy.de>
2024-10-16 00:22:45 +0200

    feat: Use Keycloak instead of manual login form for product edit pages
    Closes openfoodfacts/openfoodfacts-auth#104

--- cgi/product_multilingual.pl
+++ cgi/product_multilingual.pl
@@ -3,7 +3,7 @@
 # This file is part of Product Opener.
 #
 # Product Opener
-# Copyright (C) 2011-2023 Association Open Food Facts
+# Copyright (C) 2011-2024 Association Open Food Facts
 # Contact: contact@openfoodfacts.org
 # Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
 #
@@ -24,6 +24,7 @@ use ProductOpener::PerlStandards;
 
 use CGI::Carp qw(fatalsToBrowser);
 
+use ProductOpener::Auth qw/access_to_protected_resource/;
 use ProductOpener::Config qw/:all/;
 use ProductOpener::Paths qw/%BASE_DIRS/;
 use ProductOpener::Store qw/get_string_id_for_lang/;
@@ -349,7 +350,7 @@ if (($type eq 'delete') and (not $User{moderator})) {
 	display_error_and_exit($request_ref, $Lang{error_no_permission}{$lc}, 403);
 }
 
-if ($User_id eq 'unwanted-bot-id') {
+if ((defined $User_id) and ($User_id eq 'unwanted-bot-id')) {
 	my $r = Apache2::RequestUtil->request();
 	$r->status(500);
 	return 500;
@@ -359,9 +360,9 @@ if (($type eq 'add') or ($type eq 'edit') or ($type eq 'delete')) {
 
 	if (not defined $User_id) {
 
-		my $submit_label = "login_and_" . $type . "_product";
-		$action = 'login';
-		$template_data_ref->{type} = $type;
+		$request_ref->{return_url} = $request_ref->{canon_url};
+		# Note: This su will either finish without a result if a good user/token is present, or redirect to the login page and stop the script
+		access_to_protected_resource($request_ref);
 	}
 }
 
