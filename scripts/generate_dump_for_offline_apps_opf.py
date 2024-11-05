1b95d793926f7851222c53f7043f530b8bf8a1c2
Pierre Slamich <pierre@openfoodfacts.org>
2024-11-05 12:56:50 +0100

    feat: Create generate_dump_for_offline_apps_oxf.py

---
+++ scripts/generate_dump_for_offline_apps_opf.py
@@ -0,0 +1,17 @@
+generate_dump_for_offline_apps_off.py
+#!/usr/bin/python
+from __future__ import absolute_import, division, print_function
+import csv
+from itertools import imap
+from operator import itemgetter
+
+
+def main():
+    import pandas
+    df = pandas.read_csv('/srv/opf/html/data/en.openproductsfacts.org.products.csv', sep='\t', low_memory=False)
+    colnames = ['code','product_name','quantity','brands']
+    df.to_csv('/srv/opf/html/data/offline/en.openproductsfacts.org.products.small.csv', columns = colnames,sep='\t',index=False)
+
+if __name__ == '__main__':
+    main()
+
