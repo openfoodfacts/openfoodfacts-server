b648d32154801346b84dc4c0eafa7b208424d494
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-11-13 16:28:01 +0100

    script to move products dirs to off

---
+++ scripts/migrations/2024_11_move_obf_opf_opff_products_to_off_dirs.pl
@@ -0,0 +1,283 @@
+#!/usr/bin/perl -w
+
+# This file is part of Product Opener.
+#
+# Product Opener
+# Copyright (C) 2011-2019 Association Open Food Facts
+# Contact: contact@openfoodfacts.org
+# Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
+#
+# Product Opener is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use CGI::Carp qw(fatalsToBrowser);
+
+use ProductOpener::PerlStandards;
+
+use ProductOpener::Config qw/:all/;
+use ProductOpener::Store qw/:all/;
+use ProductOpener::Index qw/:all/;
+use ProductOpener::Display qw/:all/;
+use ProductOpener::Tags qw/:all/;
+use ProductOpener::Users qw/:all/;
+use ProductOpener::Images qw/:all/;
+use ProductOpener::Lang qw/:all/;
+use ProductOpener::Mail qw/:all/;
+use ProductOpener::Products qw/:all/;
+use ProductOpener::Food qw/:all/;
+use ProductOpener::Ingredients qw/:all/;
+use ProductOpener::Images qw/:all/;
+use ProductOpener::Data qw/:all/;
+use ProductOpener::Orgs qw/:all/;
+
+use CGI qw/:cgi :form escapeHTML/;
+use URI::Escape::XS;
+use Storable qw/dclone/;
+use Encode;
+use JSON::PP;
+
+use File::Copy (qw/move/);
+
+use Data::Dumper;
+
+# Get a list of all products
+
+use Getopt::Long;
+
+my $move = 0;
+
+GetOptions('move' => \$move,);
+
+sub product_dir_move($source, $target) {
+	# If the source does not contain directories, use move() to move it directly
+	# otherwise, create the target directory, go through all files in the source, and use move() to move them to the target, but do not move contained directories
+	# then remove the source directory
+
+	# First check if source contains directories
+	my $contains_directories = 0;
+	opendir my $dh, $source or die "could not open $source directory: $!\n";
+	foreach my $dir (sort readdir($dh)) {
+		chomp($dir);
+		next if $dir eq '.';
+		next if $dir eq '..';
+		next if $dir =~ /\.lock/;    # lingering lock files
+		if (-d "$source/$dir") {
+			$contains_directories = 1;
+			last;
+		}
+	}
+
+	# If source does not contain directories, use move() to move it directly
+	if (!$contains_directories) {
+		return move($source, $target);
+	}
+
+	print STDERR "source $source contains directories, moving files instead of directory\n";
+
+	# Otherwise, create the target directory
+	ensure_dir_created_or_die($target);
+
+	# Go through all files in the source
+	opendir my $dir_h, $source or die "could not open $source directory: $!\n";
+	foreach my $file (sort readdir($dir_h)) {
+		chomp($file);
+		# Move files and symbolic links
+		if (!-d "$source/$file") {
+			move("$source/$file", "$target/$file") or die "could not move $source/$file to $target/$file: $!\n";
+		}
+	}
+
+	# Remove the source directory
+	rmdir($source);
+
+	return 1;
+}
+
+my @dirs_without_product_sto = ();
+my @empty_dirs = ();
+my @products_existing_on_off = ();
+my @products_not_existing_on_off = ();
+my @products_existing_on_off_but_deleted_on_off() = ();
+my @products_existing_on_off_but_deleted_locally = ();
+my @products_existing_on_off_and_not_deleted_on_off_or_locally = ();
+
+sub move_product_dir_to_off ($dir, $dir2, $dir3, $dir4) {
+	# move .sto files
+	ensure_dir_created_or_die("/srv/off/products/$dir/$dir2/$dir3");
+	# if there is an existing off directory for this product, move it to deleted-off-products-codes-replaced-by-other-flavors
+	if (-e "/srv/off/products/$dir/$dir2/$dir3/$dir4") {
+		if (
+			move(
+				"/srv/off/products/$dir/$dir2/$dir3/$dir4",
+				"/srv/off/products/deleted-off-products-codes-replaced-by-other-flavors/$dir$dir2$dir3$dir4"
+			)
+			)
+		{
+			print STDERR
+				"moved /srv/off/products/$dir/$dir2/$dir3/$dir4 to /srv/off/products/deleted-off-products-codes-replaced-by-other-flavors/$dir$dir2$dir3$dir4\n";
+		}
+		else {
+			print STDERR
+				"could not move /srv/off/products/$dir/$dir2/$dir3/$dir4 to /srv/off/products/deleted-off-products-codes-replaced-by-other-flavors/$dir$dir2$dir3$dir4: $!\n";
+			die;
+		}
+	}
+	if (move("$data_root/products/$dir/$dir2/$dir3/$dir4", "/srv/off/products/$dir/$dir2/$dir3/$dir4")) {
+		print STDERR "moved /$data_root/products/$dir/$dir2/$dir3/$dir4 to /srv/off/products/$dir/$dir2/$dir3/$dir4\n";
+	}
+	else {
+		print STDERR
+			"could not move /$data_root/products/$dir/$dir2/$dir3/$dir4 to /srv/off/products/$dir/$dir2/$dir3/$dir4: $!\n";
+		die;
+	}
+
+	# move images if they exist
+	if (-e "$www_root/images/products/$dir/$dir2/$dir3/$dir4") {
+		ensure_dir_created_or_die("$www_root/images/products/$dir/$dir2/$dir3");
+		# if there is an existing off directory for this product, move it to deleted-off-products-codes-replaced-by-other-flavors
+		if (-e "/srv/off/html/images/products/$dir/$dir2/$dir3/$dir4") {
+			if (
+				move(
+					"/srv/off/html/images/products/$dir/$dir2/$dir3/$dir4",
+					"/srv/off/html/images/products/deleted-off-products-codes-replaced-by-other-flavors/$dir$dir2$dir3$dir4"
+				)
+				)
+			{
+				print STDERR
+					"moved /srv/off/html/images/products/$dir/$dir2/$dir3/$dir4 to /srv/off/html/images/products/deleted-off-products-codes-replaced-by-other-flavors/$dir$dir2$dir3$dir4\n";
+			}
+			else {
+				print STDERR
+					"could not move /srv/off/html/images/products/$dir/$dir2/$dir3/$dir4 to /srv/off/html/images/products/deleted-off-products-codes-replaced-by-other-flavors/$dir$dir2$dir3$dir4: $!\n";
+				die;
+			}
+		}
+		if (
+			move(
+				"$www_root/images/products/$dir/$dir2/$dir3/$dir4", "$www_root/images/products/$dir/$dir2/$dir3/$dir4"
+			)
+			)
+		{
+			print STDERR
+				"moved $www_root/images/products/$dir/$dir2/$dir3/$dir4 to $www_root/images/products/$dir/$dir2/$dir3/$dir4\n";
+		}
+		else {
+			print STDERR
+				"could not move $www_root/images/products/$dir/$dir2/$dir3/$dir4 to $www_root/images/products/$dir/$dir2/$dir3/$dir4: $!\n";
+			die;
+		}
+	}
+}
+
+sub check_if_we_can_move_product_dir_to_off ($dir, $dir2, $dir3, $dir4) {
+	# Check if the product.sto file exists locally
+	my $local_product_ref = retrieve("$data_root/products/$dir/$dir2/$dir3/$dir4/product.sto");
+	if ($local_product_ref) {
+		# Check if the product exists on OFF
+		my $off_product_ref = retrieve("/srv/off/products/$dir/$dir2/$dir3/$dir4/product.sto");
+		if ($off_product_ref) {
+			# Check if the product is deleted on OFF
+			if ($off_product_ref->{deleted}) {
+				push @products_existing_on_off_but_deleted_on_off, "$dir/$dir2/$dir3/$dir4";
+				if ($move) {
+					move_product_dir_to_off($dir, $dir2, $dir3, $dir4);
+				}
+			}
+			elsif ($local_product_ref->{deleted}) {
+				push @products_existing_on_off_but_deleted_locally, "$dir/$dir2/$dir3/$dir4";
+			}
+			else {
+				push @products_existing_on_off_and_not_deleted_on_off_or_locally, "$dir/$dir2/$dir3/$dir4";
+			}
+		}
+		else {
+			push @products_not_existing_on_off, "$dir/$dir2/$dir3/$dir4";
+			if ($move) {
+				move_product_dir_to_off($dir, $dir2, $dir3, $dir4);
+			}
+		}
+	}
+	else {
+		push @dirs_without_product_sto, "$dir/$dir2/$dir3/$dir4";
+		# Check if the dir is empty
+		opendir my $dh, "$data_root/products/$dir/$dir2/$dir3/$dir4" or die "Cannot open directory: $!";
+		my @files = grep {$_ ne '.' && $_ ne '..'} readdir($dh);
+		closedir $dh;
+		if (scalar @files == 0) {
+			push @empty_dirs, "$dir/$dir2/$dir3/$dir4";
+		}
+	}
+}
+
+my @products = ();
+
+my $dh;
+
+opendir $dh, "$data_root/products"
+	or die "could not open $data_root/products directory: $!\n";
+foreach my $dir (sort readdir($dh)) {
+	chomp($dir);
+
+	print STDERR "dir: $dir\n";
+
+	# Check it is a directory
+	next if not -d "$data_root/products/$dir";
+	next if ($dir =~ /codes/);
+
+	if ($dir =~ /^\d\d\d$/) {
+
+		opendir my $dh2, "$data_root/products/$dir"
+			or die "ERROR: could not open $data_root/products/$dir directory: $!\n";
+		foreach my $dir2 (sort readdir($dh2)) {
+			chomp($dir2);
+			if ($dir2 =~ /^\d\d\d$/) {
+				opendir my $dh3, "$data_root/products/$dir/$dir2"
+					or die "ERROR: could not open $data_root/products/$dir/$dir2 directory: $!\n";
+				foreach my $dir3 (sort readdir($dh3)) {
+					chomp($dir3);
+					if ($dir3 =~ /^\d\d\d$/) {
+						opendir my $dh4, "$data_root/products/$dir/$dir2/$dir3"
+							or die "ERROR: could not open $data_root/products/$dir/$dir2/$dir3 directory: $!\n";
+						foreach my $dir4 (sort readdir($dh4)) {
+							chomp($dir4);
+							# We should have 4 digits or more (for codes with more than 13 digits)
+							if ($dir4 =~ /^\d+$/) {
+								check_if_we_can_move_product_dir_to_off($dir, $dir2, $dir3, $dir4);
+							}
+						}
+						closedir $dh4;
+					}
+				}
+				closedir $dh3;
+			}
+		}
+		closedir $dh2;
+	}
+}
+closedir $dh;
+
+my $count = scalar @products;
+print STDERR "Found $count products\n";
+
+# Print the number of products in the different cases
+print STDERR "Products existing on OFF and deleted on OFF: "
+	. scalar @products_existing_on_off_but_deleted_on_off . "\n";
+print STDERR "Products existing on OFF and deleted locally: "
+	. scalar @products_existing_on_off_but_deleted_locally . "\n";
+print STDERR "Products existing on OFF and not deleted on OFF or locally: "
+	. scalar @products_existing_on_off_and_not_deleted_on_off_or_locally . "\n";
+print STDERR "Dirs without product.sto: " . scalar @dirs_without_product_sto . "\n";
+print STDERR "Empty dirs: " . scalar @empty_dirs . "\n";
+
+exit(0);
+
