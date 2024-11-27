449b69507d3b8b0ebb518a9dfcddbae7b48d055c
Charles Nepote <charles@nepote.org>
2024-11-27 13:00:07 +0100

    Dubugging

--- scripts/remove_nearly_empty_products.pl
+++ scripts/remove_nearly_empty_products.pl
@@ -33,55 +33,83 @@ use ProductOpener::Data qw/get_products_collection/;
 # This script is run to remove products with a data quality issue, 
 # few information & no image at all (excluding imports)
 
-my $cursor = get_products_collection()->query(
-	{ data_quality_errors_tags => { '$nin' => [undef, []] }
-	 , states_tags => 'en:photos-to-be-uploaded' 
-	}
-	)->fields({code => 1});
+my $cursor = get_products_collection()->query({
+                data_quality_errors_tags => { '$ne' => undef }
+                , data_quality_errors_tags => { '$ne' => "" }
+                , data_quality_errors_tags => { '$ne' => [] }
+                , states_tags => 'en:photos-to-be-uploaded'
+        })->fields({code => 1});
 $cursor->immortal(1);
 my $removed = 0;
+my $product_nb = 0;
+
+my $time = time();
+print "time: " . $time . "; time - + (60 * 60 * 24 * 30): " . ($time - + (60 * 60 * 24 * 30)) ."\n";
 
 while (my $product_ref = $cursor->next) {
 
-	my $code = $product_ref->{code};
+	printf ("%5s - ", $product_nb);
 
-	#print STDERR "updating product $code\n";
+	my $code = $product_ref->{code};
 
 	$product_ref = retrieve_product($code);
 
-	no warnings 'uninitialized';
+	my $owner = defined($product_ref->{owner}) ? $product_ref->{owner} : '';
+	my $creator = defined($product_ref->{creator}) ? $product_ref->{creator} : '';
+	my $last_modified_t = defined($product_ref->{last_modified_t}) ? $product_ref->{last_modified_t} : 0;
+	my $last_image_t = defined($product_ref->{last_image_t}) ? $product_ref->{last_image_t} : 0;
+	my $first_quality_error = defined($product_ref->{data_quality_errors_tags}[0]) ? $product_ref->{data_quality_errors_tags[0]} : "-";
+	my $time_ok = defined(time() - $last_modified_t + (60 * 60 * 24 * 30)) ? (time() - $last_modified_t + (60 * 60 * 24 * 30)) : 0;
+	my $completeness = defined($product_ref->{completeness}) ? $product_ref->{completeness} : "";
+
 
 	if ((defined $product_ref) and ($code ne '')) {
 	
-		if (1
-			#and (defined $product_ref->{last_image_t})
-			#and ($product_ref->{last_image_t} eq '')
-			and ($product_ref->{owner} eq '')
-			and ($product_ref->{creator} ne 'usda-ndb-import')
-			and ($product_ref->{creator} !~ /^org-.*/)
+		if (
+			($owner eq '' or  $owner eq null)
+			and ($creator ne 'usda-ndb-import')
+			and ($creator !~ /^org-.*/)
 			and ($product_ref->{completeness} < 0.4)
-			and (time() > $product_ref->{last_modified_t} + (60 * 60 * 24 * 30))
+			and ($time > $last_modified_t + (60 * 60 * 24 * 30))
 			) {
 
 			#$product_ref->{deleted} = 'on';
-			add_tag($product_ref, "misc", 'en:bad-product-to-be-deleted'); # Test before deleting
+			#add_tag($product_ref, "misc", 'en:bad-product-to-be-deleted'); # Test before deleting
 
 			my $comment = "[remove_nearly_empty_products.pl] removal of product with a data quality issue, " .
 							"few information & no image at all";
 
 			# Save the product
-			store_product("remove-bad-products-wo-photos-bot", $product_ref, $comment);
+			#store_product("remove-bad-products-wo-photos-bot", $product_ref, $comment);
 
-			print "Removed product $code, created by $product_ref->{creator}";
-			print ", completeness: $product_ref->{completeness}";
-			print ", quality issues: $product_ref->{data_quality_errors_tags}...\n";
+			print "Removed ";
 
 			$removed++;
 		}
+		else {
+			print "NOT RMVD - ";
+			print ($owner eq '' or  $owner eq null) ? "owner is empty - " : "owner is not empty - ";
+		}
+
+		# print debugging info
+		printf ("%18s", $code);
+		printf (", creator: %15s", substr($creator, 0, 15));
+		printf (", cmpltness: %3s", $completeness);
+		printf (", owner: %5s", $owner);
+		printf (", last_img: %10s", $last_image_t);
+		printf (", last_modified: %10s", $last_modified_t);
+		#print ", time ok?: $time_ok";
+		print ", time ok?: ";
+		print (($time > $last_modified_t + (60 * 60 * 24 * 30)) ? "" : "-"); print $time_ok;
+		print ", dq issues: " . substr($first_quality_error, 0, 12);
+		print "\n";
+
 	}
 	else {
-		#print "product $code : file not found\n";
+		print "product $code : file not found\n";
 	}
+
+$product_nb++;
 
 }
 
