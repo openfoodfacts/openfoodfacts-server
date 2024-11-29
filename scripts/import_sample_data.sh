9879355acf8b4a0a88014492d7e0fe86d0419e20
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-11-29 14:40:09 +0100

    smaller product sample for developers

--- scripts/import_sample_data.sh
+++ scripts/import_sample_data.sh
@@ -7,12 +7,12 @@ cd /tmp
 echo "\033[32m------------------ 1/ Retrieve products -----------------\033[0m";
 # explicitly specify the wget output file name so that wget does not append .1 if already present
 # e.g. if the tar command failed and the script was stopped
-wget -O products.tar.gz https://static.openfoodfacts.org/exports/products.random-modulo-10000.tar.gz 2>&1
+wget -O products.tar.gz https://static.openfoodfacts.org/exports/products.random-modulo-100000.tar.gz 2>&1
 tar -xzvf products.tar.gz -C /mnt/podata/products
 rm products.tar.gz
 
 echo "\033[32m------------------ 2/ Retrieve product images -------------------\033[0m";
-wget -O products.images.tar.gz https://static.openfoodfacts.org/exports/products.random-modulo-10000.images.tar.gz 2>&1
+wget -O products.images.tar.gz https://static.openfoodfacts.org/exports/products.random-modulo-100000.images.tar.gz 2>&1
 tar -xzvf products.images.tar.gz -C /opt/product-opener/html/images/products/
 rm products.images.tar.gz
 
