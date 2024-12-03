cced774ea5c15415947136caa3c0f7278893d783
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-12-03 15:07:56 +0100

    ofsf

--- lib/ProductOpener/FoodSupplementProducts.pm
+++ lib/ProductOpener/FoodSupplementProducts.pm
@@ -41,7 +41,7 @@ BEGIN {
 	use vars qw(@ISA @EXPORT_OK %EXPORT_TAGS);
 	@EXPORT_OK = qw(
 
-		&specific_processes_for_food_product
+		&specific_processes_for_food_supplement_product
 
 	);    # symbols to export on request
 	%EXPORT_TAGS = (all => [@EXPORT_OK]);
