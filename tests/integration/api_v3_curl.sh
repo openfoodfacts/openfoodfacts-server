946249bc08849d12a27b6d86436ffa4e0bcb4f69
Stéphane Gigandet <stephane@openfoodfacts.org>
2024-12-06 09:52:33 +0100

    remove debug test

--- tests/integration/api_v3_curl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl -X PATCH -H "Content-Type: application/json" -d '{
-    "fields" : "updated,ecoscore_grade,ecoscore_score,ecoscore_data",
-    "product": { 
-        "categories_tags_add": ["en:chocolate-cookies"],
-        "labels_tags_fr": ["ab bio"]
-    }
-}' https://world.openfoodfacts.org/api/v3/product/test	
