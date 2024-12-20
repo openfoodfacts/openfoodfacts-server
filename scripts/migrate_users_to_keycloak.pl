69f3071f334eff44902d0ba46aa8eb98a1e738f4
John Gomersall <thegoms@gmail.com>
2024-12-20 15:42:22 +0000

    Truncate name to 255 charst
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- scripts/migrate_users_to_keycloak.pl
+++ scripts/migrate_users_to_keycloak.pl
@@ -136,7 +136,7 @@ sub convert_to_keycloak_user ($user_file, $anonymize) {
 		username => $userid,
 		credentials => [$credential],
 		attributes => {
-			name => ($anonymize ? $userid : $user_ref->{name}),
+			name => substr(($anonymize ? $userid : $user_ref->{name}), 0, 255),
 			locale => $user_ref->{initial_lc},
 			country => $user_ref->{initial_cc},
 			registered => 'registered', # The prevents welcome emails from being sent
@@ -154,7 +154,7 @@ sub convert_to_keycloak_user ($user_file, $anonymize) {
 		$keycloak_user_ref->{emailVerified} = $JSON::PP::true;
 	}
 	elsif (not defined $email_status or $email_status->{invalid} or $email_status->{userid} ne $userid) {
-		$keycloak_user_ref->{attributes}{old_email} = $user_ref->{email};
+		$keycloak_user_ref->{attributes}{old_email} = substr($user_ref->{email}, 0, 255);
 	}
 	else {
 		$keycloak_user_ref->{email} = $email;
