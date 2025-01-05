378f2d3ff171a0c153a419fcb9c0d978aa0ec4ff
benbenben2 <110821832+benbenben2@users.noreply.github.com>
2025-01-05 13:18:06 +0100

    fix warnings in import_convert_carrefour_france

--- lib/ProductOpener/Ingredients.pm
+++ lib/ProductOpener/Ingredients.pm
@@ -3580,7 +3580,7 @@ reference to the name of the country
 sub get_geographical_area ($originid) {
 	# Getting information about the country
 	my $ecobalyse_area = "";
-	if (get_inherited_property("countries", $originid, "ecobalyse_is_part_of_eu") eq "yes") {
+	if (defined get_inherited_property("countries", $originid, "ecobalyse_is_part_of_eu") && get_inherited_property("countries", $originid, "ecobalyse_is_part_of_eu") eq "yes") {
 		$ecobalyse_area = "eu";
 	}
 	if ($originid eq "en:france") {
