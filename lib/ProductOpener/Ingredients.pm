a32b213ac53188fd589de3cf0094a2f588ba1990
benbenben2 <110821832+benbenben2@users.noreply.github.com>
2025-01-06 11:04:14 +0100

    apply suggestions

--- lib/ProductOpener/Ingredients.pm
+++ lib/ProductOpener/Ingredients.pm
@@ -3580,8 +3580,9 @@ reference to the name of the country
 sub get_geographical_area ($originid) {
 	# Getting information about the country
 	my $ecobalyse_area = "";
-	if (defined get_inherited_property("countries", $originid, "ecobalyse_is_part_of_eu")
-		&& get_inherited_property("countries", $originid, "ecobalyse_is_part_of_eu") eq "yes")
+	my $ecobalyse_is_part_of_eu_result = get_inherited_property("countries", $originid, "ecobalyse_is_part_of_eu");
+	if (defined $ecobalyse_is_part_of_eu_result
+		&& $ecobalyse_is_part_of_eu_result eq "yes")
 	{
 		$ecobalyse_area = "eu";
 	}
