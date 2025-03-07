8835c97b45dd039850c1bd524b40287c61c949d1
Nothingspecial99 <vikaspandey.vp.ap@gmail.com>
2025-03-08 01:53:46 +0530

    feat: Add button to add product if not found in search

--- lib/ProductOpener/Display.pm
+++ lib/ProductOpener/Display.pm
@@ -5803,6 +5803,13 @@ display_user_product_preferences("#preferences_selected", "#preferences_selectio
 rank_and_display_products("#search_results", products, contributor_prefs);
 JS
 		;
+		
+	my $search_terms = '';
+	if (defined single_param('search_terms')) {
+		$search_terms = remove_tags_and_quote(decode utf8 => single_param('search_terms'));
+	}
+
+	$template_data_ref->{search_terms} = ${search_terms};
 
 	process_template('web/common/includes/list_of_products.tt.html', $template_data_ref, \$html)
 		|| return "template error: " . $tt->error();
