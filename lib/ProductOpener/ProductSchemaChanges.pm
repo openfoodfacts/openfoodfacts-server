4a05d8492a1aa7b383e0578b309f0216ef7b92ab
Stéphane Gigandet <stephane@openfoodfacts.org>
2025-03-14 16:11:12 +0100

    functions to convert product schema

---
+++ lib/ProductOpener/ProductSchemaChanges.pm
@@ -0,0 +1,166 @@
+# This file is part of Product Opener.
+#
+# Product Opener
+# Copyright (C) 2011-2023 Association Open Food Facts
+# Contact: contact@openfoodfacts.org
+# Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
+#
+# Product Opener is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=head1 NAME
+
+ProductOpener::ProductSchemaChanges - convert product data between different schema versions
+
+=head1 DESCRIPTION
+
+C<ProductOpener::ProductSchemaChanges> is used to convert product data between different schema versions.
+
+It can convert older product data (e.g. from old revisions of products) to the current schema version.
+
+And for API backward compatibility, it can also convert the current product data to older schema versions.
+
+=head2 Schema version numbering
+
+Starting from March 2024, we will now include a new field in the product data called "schema_version", which will be an integer, starting at 1001.
+
+The schema version will be incremented by 1 each time we make a change to the product data schema.
+
+Products without a schema_version field will be considered to be at a schema version under 1000
+
+=head2 Schema conversion functions
+
+We will keep a list of functions that can convert product data between different schema versions.
+
+=cut
+
+package ProductOpener::ProductSchemaChanges;
+
+use ProductOpener::PerlStandards;
+use Exporter qw< import >;
+
+BEGIN {
+	use vars qw(@ISA @EXPORT_OK %EXPORT_TAGS);
+	@EXPORT_OK = qw(
+		$current_schema_version
+		&convert_product_schema
+	);    # symbols to export on request
+	%EXPORT_TAGS = (all => [@EXPORT_OK]);
+}
+
+use vars @EXPORT_OK;
+
+$current_schema_version = 1001;
+
+my (%upgrade_functions, %downgrade_functions);
+
+=head1 FUNCTIONS
+
+=head2 convert_product_schema ($product_ref, $to_version)
+
+Convert product data between different schema versions.
+
+=cut
+
+sub convert_product_schema ($product_ref, $to_version) {
+
+	my $from_version = $product_ref->{schema_version} // 999;
+
+	if ($from_version < $to_version) {
+		for (my $schema_version = $from_version; $schema_version < $to_version; $schema_version++) {
+			if (exists $upgrade_functions{$schema_version}) {
+				$upgrade_functions{$schema_version}->($product_ref);
+			}
+		}
+	}
+	elsif ($from_version > $to_version) {
+		for (my $schema_version = $from_version; $schema_version > $to_version; $schema_version--) {
+			if (exists $downgrade_functions{$schema_version}) {
+				$downgrade_functions{$schema_version}->($product_ref);
+			}
+		}
+	}
+
+	$product_ref->{schema_version} = $to_version;
+
+	return;
+}
+
+%upgrade_functions = (
+	999 =>  \&convert_schema_999_to_1000_rename_ecoscore_fields_to_environmental_score,
+	1000 =>  \&convert_schema_1000_to_1001_remove_ingredients_hierarchy,
+);
+
+%downgrade_functions = (
+	1000 =>  \&convert_schema_1000_to_999_rename_environmental_score_fields_to_ecoscore,
+	1001 =>  \&convert_schema_1001_to_1000_add_ingredients_hierarchy,
+);
+
+=headd2 999 to 1000 - Rename ecoscore fields to environmental_score fields - API v3.1
+
+2024/12/12 - 
+
+=cut
+
+sub convert_schema_999_to_1000_rename_ecoscore_fields_to_environmental_score ($product_ref) {
+
+	# 2024/12: ecoscore fields were renamed to environmental_score
+	foreach my $subfield (qw/data grade score tags/) {
+		if (defined $product_ref->{"ecoscore_" . $subfield}) {
+			# If we already have corresponding environmental_score fields, we keep them
+			if (not defined $product_ref->{"environmental_score_" . $subfield}) {
+				$product_ref->{"environmental_score_" . $subfield} = $product_ref->{"ecoscore_" . $subfield};
+			}
+			delete $product_ref->{"ecoscore_" . $subfield};
+		}
+	}
+
+	return;
+}
+
+sub convert_schema_1000_to_999_rename_environmental_score_fields_to_ecoscore ($product_ref) {
+
+	# 2024/12: ecoscore fields were renamed to environmental_score
+	foreach my $subfield (qw/data grade score tags/) {
+		if (defined $product_ref->{"environmental_score_" . $subfield}) {
+			$product_ref->{"ecoscore_" . $subfield} = $product_ref->{"environmental_score_" . $subfield};
+			delete $product_ref->{"environmental_score_" . $subfield};
+		}
+	}
+
+	return;
+}
+
+=head2 1000 to 1001 - Remove the ingredients_hierarchy field - API v3.2
+
+2012/03/14 - Remove the ingredients_hierarchy field, which was a duplicate of the ingredients_tags field
+
+=cut
+
+sub convert_schema_1000_to_1001_remove_ingredients_hierarchy ($product_ref) {
+
+	delete $product_ref->{ingredients_hierarchy};
+
+	return;
+}
+
+sub convert_schema_1001_to_1000_add_ingredients_hierarchy ($product_ref) {
+
+	# The ingredients_hierarchy array contained exactly the same data as the ingredients_tags array
+	if (exists $product_ref->{ingredients_tags}) {
+		$product_ref->{ingredients_hierarchy} = $product_ref->{ingredients_tags};
+	}
+
+	return;
+}
+
