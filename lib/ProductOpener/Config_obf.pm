d0497e6d1557c5bf11c024bc833c7cf8faeff11e
Stéphane Gigandet <stephane@openfoodfacts.org>
2025-03-28 15:13:21 +0100

    fix: enable brands taxonomy for OBF and OPF (#11686)

--- lib/ProductOpener/Config_obf.pm
+++ lib/ProductOpener/Config_obf.pm
@@ -351,6 +351,7 @@ HTML
 	data_quality data_quality_bugs data_quality_info data_quality_warnings data_quality_errors data_quality_warnings_producers data_quality_errors_producers
 	improvements
 	inci_functions
+	brands
 );
 
 # tag types (=facets) that should be indexed by web crawlers, all other tag types are not indexable
