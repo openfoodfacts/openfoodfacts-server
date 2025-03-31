387b6dee1a0aa8b84844b016c68237a269b0e667
Kout95 <antoine.bernier@outlook.fr>
2025-03-31 15:24:14 +0200

    fix: off-webcomponents-utils

--- html/js/off-webcomponents-utils.js
+++ html/js/off-webcomponents-utils.js
@@ -1,4 +1,5 @@
 /**
+
  * This is related to robotoff insight validation.
  *
  * It enables showing / hiding the div containing the web component with is_hidden css class.
@@ -6,20 +7,20 @@
  * @param {Event} eventToListenTo - Event emitted by the web-component,
  * with a state attribute. We react to "has-data" (show) and "annotated" (hide).
  * @param {string} elementId - The web component id.
- * @param {string} parentSelector - A web selector of the containing div.
+ * @param {object} options - Options.
+ * @param {string} options.parentSelector - A web selector of the containing div.
  * If omitted, we will only id the web-component.
- * @param {boolean} reloadOnAnnotated - If true, we reload the page when the insight is annotated.
+ * @param {boolean} options.reloadOnAnnotated - If true, we reload the page when the insight is annotated.
  * @returns {void}
  */
 window.listenEventToShowHideAlert = function (
   eventToListenTo,
   elementId,
-  parentSelector,
-  reloadOnAnnotated = false
+  options = {}
 ) {
   const element = document.getElementById(elementId);
-  const parentElement = parentSelector
-    ? document.querySelector(parentSelector)
+  const parentElement = options.parentSelector
+    ? document.querySelector(options.parentSelector)
     : element.parentElement;
   parentElement.classList.add("is_hidden");
   element.addEventListener(eventToListenTo, (event) => {
@@ -27,7 +28,7 @@ window.listenEventToShowHideAlert = function (
       parentElement.classList.remove("is_hidden");
     } else if (event.detail.state === "annotated") {
       // reload the page to show the new insight
-      if (reloadOnAnnotated) {
+      if (options.reloadOnAnnotated) {
         window.location.reload();
       } else {
         // hide the element after 3 seconds
