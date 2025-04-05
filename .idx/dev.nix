856fca6f11f320ce9fc8fba1b6f71c4169890cc1
Jagjeevan <jagjeevandev97@gmail.com>
2025-04-05 06:48:33 +0530

    Testing google IDX
    Signed-off-by: Jagjeevan <jagjeevandev97@gmail.com>

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,12 +1,21 @@
 { pkgs, ... }:
   {
+    channel = "stable-23.11";
+
     packages = with pkgs; [
-      nodejs
-      python3
       gnumake
       docker
       git
       gnugrep
     ];
     services.docker.enable = true;
+    idx.previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "make" "reset_owner" "make" "dev" ];
+          manager = "web";
+        };
+      };
+  };
 }
