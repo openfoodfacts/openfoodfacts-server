90506bfc53f14d79a15faf3c1419cac4ca3795d5
Kout95 <antoine.bernier@outlook.fr>
2025-04-08 16:53:06 +0200

    chore: clean code

---
+++ html/js/barcode-scanner-button.js
@@ -0,0 +1,20 @@
+// Barcode Scanner Button
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the barcode scanner button and scanner element
+  const barcodeScannerButton = document.querySelector(
+    ".barcode-scanner-button"
+  );
+  const barcodeScanner = document.querySelector("barcode-scanner");
+
+  // Handle barcode scanner button click
+  barcodeScannerButton.addEventListener("click", function () {
+    // Remove the hidden class from the barcode scanner parent element
+    barcodeScanner.parentElement.classList.remove("is_hidden");
+  });
+
+  // Handle barcode scanner button click
+  barcodeScannerButton.addEventListener("click", function () {
+    // Remove the hidden class from the barcode scanner parent element
+    barcodeScanner.parentElement.classList.remove("is_hidden");
+  });
+});
---
+++ html/js/barcode-scanner.js
@@ -0,0 +1,68 @@
+// Barcode Scanner Modal
+const barcodeScanner = document.querySelector("barcode-scanner");
+const barcodeScannerModal = document.getElementById("barcode-scanner-modal");
+const barcodeCloseButton = barcodeScannerModal.querySelector(
+  ".modal-close-button"
+);
+
+// Get references to the barcode modal button and input
+const barcodeModalButton = document.getElementById("barcode-modal-button");
+const barcodeModalInput = document.getElementById("barcode-modal-input");
+
+// Enable the barcode modal button when input is entered
+barcodeModalInput.addEventListener("input", function () {
+  const barcode = barcodeModalInput.value;
+  barcodeModalButton.disabled = !barcode;
+});
+
+// Handle barcode modal button click
+barcodeModalButton.addEventListener("click", function () {
+  const barcode = barcodeModalInput.value;
+  if (barcode) {
+    onBarcodeDetected(barcode);
+  }
+});
+
+// Function to handle barcode detection
+const onBarcodeDetected = (barcode) => {
+  const baseUrl = window.location.href.split("/")[0];
+  const url =
+    baseUrl +
+    "/cgi/search.pl?search_terms=" +
+    barcode +
+    "&search_simple=1&action=process";
+  window.location.href = url;
+};
+
+// Handle barcode scanner state changes
+barcodeScanner.addEventListener("barcode-scanner-state", function (e) {
+  document.addEventListener("DOMContentLoaded", function () {
+    const barcodeScannerButton = document.getElementById(
+      "barcode-scanner-button"
+    );
+    if (e.detail.state === "detector-not-available") {
+      barcodeScannerButton.parentElement.classList.remove("is_hidden");
+    } else if (e.detail.state === "DETECTED") {
+      onBarcodeDetected(e.detail.barcode);
+    }
+  });
+});
+
+// Handle barcode close button click
+barcodeCloseButton.addEventListener("click", function () {
+  document.getElementById("barcode-scanner-modal").classList.add("is_hidden");
+});
+
+// Function to open the barcode scanner modal
+function openModal() {
+  barcodeScannerModal.classList.remove("is_hidden");
+  barcodeScanner.setAttribute("run-scanner", true);
+}
+
+// Initialize the barcode scanner button
+document.addEventListener("DOMContentLoaded", function () {
+  const barcodeScannerButton = document.getElementById(
+    "barcode-scanner-button"
+  );
+  barcodeScannerButton.addEventListener("click", openModal);
+});
