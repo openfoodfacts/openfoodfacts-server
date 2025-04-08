a8b39c43ba3f966d9e89fba4811433abe069615f
Kout95 <antoine.bernier@outlook.fr>
2025-04-08 18:13:12 +0200

    fix

--- gulpfile.ts
+++ gulpfile.ts
@@ -19,7 +19,6 @@ const jsSrc = [
   "./html/js/stikelem.js",
   "./html/js/scrollNav.js",
   "./html/js/off-webcomponents-utils.js",
-  "./html/js/barcode-scanner*.js",
 ];
 
 const sassSrc = "./scss/**/*.scss";
@@ -38,30 +37,30 @@ const imagesSrc = [
 // nginx needs both uncompressed and compressed files as we use try_files with gzip_static always & gunzip
 
 export function icons() {
-  const processed = src("*.svg", { cwd: "./icons" })
-    .pipe(
+  const processed = src("*.svg", { cwd: "./icons" }).
+    pipe(
       svgmin({
         // @ts-ignore
         configFile: "icons/svgo.config.js",
       })
-    )
-    .pipe(dest("./html/images/icons/dist"));
+    ).
+    pipe(dest("./html/images/icons/dist"));
 
-  const compressed = processed
-    .pipe(gzip())
-    .pipe(dest("./html/images/icons/dist"));
+  const compressed = processed.
+    pipe(gzip()).
+    pipe(dest("./html/images/icons/dist"));
 
   return processed && compressed;
 }
 
 export function attributesIcons() {
-  const processed = src("*.svg", { cwd: "./html/images/attributes/src" })
-    .pipe(svgmin())
-    .pipe(dest("./html/images/attributes/dist"));
+  const processed = src("*.svg", { cwd: "./html/images/attributes/src" }).
+    pipe(svgmin()).
+    pipe(dest("./html/images/attributes/dist"));
 
-  const compressed = processed
-    .pipe(gzip())
-    .pipe(dest("./html/images/attributes/dist"));
+  const compressed = processed.
+    pipe(gzip()).
+    pipe(dest("./html/images/attributes/dist"));
 
   return processed && compressed;
 }
@@ -69,21 +68,23 @@ export function attributesIcons() {
 export function css() {
   console.log("(re)building css");
 
-  const processed = src(sassSrc)
-    .pipe(init())
-    .pipe(
+  const processed = src(sassSrc).
+    pipe(init()).
+    pipe(
       sass({
         errLogToConsole: true,
         outputStyle: "expanded",
         includePaths: ["./node_modules/foundation-sites/scss"],
       }).on("error", sass.logError)
-    )
-    .pipe(minifyCSS())
-    .pipe(write("."))
-    .pipe(dest("./html/css/dist"));
+    ).
+    pipe(minifyCSS()).
+    pipe(write(".")).
+    pipe(dest("./html/css/dist"));
 
-  const compressed = processed.pipe(gzip()).pipe(dest("./html/css/dist"));
-
+  const compressed = processed.
+    pipe(gzip()).
+    pipe(dest("./html/css/dist"));
+  
   return processed && compressed;
 }
 
@@ -110,13 +111,15 @@ export function copyJs() {
     "./node_modules/jsvectormap/dist/maps/world-merc.js",
     "./node_modules/select2/dist/js/select2.min.js",
     "./node_modules/jsbarcode/dist/JsBarcode.all.min.js",
-  ])
-    .pipe(init())
-    .pipe(terser())
-    .pipe(write("."))
-    .pipe(dest("./html/js/dist"));
+  ]).
+    pipe(init()).
+    pipe(terser()).
+    pipe(write(".")).
+    pipe(dest("./html/js/dist"));
 
-  const compressed = processed.pipe(gzip()).pipe(dest("./html/js/dist"));
+  const compressed = processed.
+    pipe(gzip()).
+    pipe(dest("./html/js/dist"));
 
   return processed && compressed;
 }
@@ -124,13 +127,15 @@ export function copyJs() {
 export function buildJs() {
   console.log("(re)building js");
 
-  const processed = src(jsSrc)
-    .pipe(init())
-    .pipe(terser())
-    .pipe(write("."))
-    .pipe(dest("./html/js/dist"));
+  const processed = src(jsSrc).
+    pipe(init()).
+    pipe(terser()).
+    pipe(write(".")).
+    pipe(dest("./html/js/dist"));
 
-  const compressed = processed.pipe(gzip()).pipe(dest("./html/js/dist"));
+  const compressed = processed.
+    pipe(gzip()).
+    pipe(dest("./html/js/dist"));
 
   return processed && compressed;
 }
@@ -146,14 +151,16 @@ function buildjQueryUi() {
     "./node_modules/jquery-ui/ui/safe-active-element.js",
     "./node_modules/jquery-ui/ui/widgets/autocomplete.js",
     "./node_modules/jquery-ui/ui/widgets/menu.js",
-  ])
-    .pipe(init())
-    .pipe(terser())
-    .pipe(concat("jquery-ui.js"))
-    .pipe(write("."))
-    .pipe(dest("./html/js/dist"));
+  ]).
+    pipe(init()).
+    pipe(terser()).
+    pipe(concat("jquery-ui.js")).
+    pipe(write(".")).
+    pipe(dest("./html/js/dist"));
 
-  const compressed = processed.pipe(gzip()).pipe(dest("./html/js/dist"));
+  const compressed = processed.
+    pipe(gzip()).
+    pipe(dest("./html/js/dist"));
 
   return processed && compressed;
 }
@@ -164,17 +171,17 @@ function jQueryUiThemes() {
     "./node_modules/jquery-ui/themes/base/autocomplete.css",
     "./node_modules/jquery-ui/themes/base/menu.css",
     "./node_modules/jquery-ui/themes/base/theme.css",
-  ])
-    .pipe(init())
-    .pipe(minifyCSS())
-    .pipe(concat("jquery-ui.css"))
-    .pipe(write("."))
-    .pipe(dest("./html/css/dist/jqueryui/themes/base"));
+  ]).
+    pipe(init()).
+    pipe(minifyCSS()).
+    pipe(concat("jquery-ui.css")).
+    pipe(write(".")).
+    pipe(dest("./html/css/dist/jqueryui/themes/base"));
 
-  const compressed = processed
-    .pipe(gzip())
-    .pipe(dest("./html/css/dist/jqueryui/themes/base"));
-
+  const compressed = processed.
+    pipe(gzip()).
+    pipe(dest("./html/css/dist/jqueryui/themes/base"));
+  
   return processed && compressed;
 }
 
@@ -186,13 +193,15 @@ function copyCss() {
     "./node_modules/@yaireo/tagify/dist/tagify.css",
     "./node_modules/cropperjs/dist/cropper.css",
     "./node_modules/select2/dist/css/select2.min.css",
-  ])
-    .pipe(init())
-    .pipe(minifyCSS())
-    .pipe(write("."))
-    .pipe(dest("./html/css/dist"));
+  ]).
+    pipe(init()).
+    pipe(minifyCSS()).
+    pipe(write(".")).
+    pipe(dest("./html/css/dist"));
 
-  const compressed = processed.pipe(gzip()).pipe(dest("./html/css/dist"));
+  const compressed = processed.
+    pipe(gzip()).
+    pipe(dest("./html/css/dist"));
 
   return processed && compressed;
 }
