13ab21f7ae80d04180e2c0bf1351a318061fa47a
Stéphane Gigandet <stephane@openfoodfacts.org>
2025-05-20 15:24:54 +0200

    fix: not displayed nutrition columns #11304 (#11888)
    fixes the computation of the nutrition_data and nutrition_data_prepared
    columns that indicates if we have nutrition data, and also add a check
    to display columns anyway if we have data (for old versions of products
    that have a bad value in nutrition_data)
    
    fixes https://github.com/openfoodfacts/openfoodfacts-server/issues/11304
    
    ---------
    
    Co-authored-by: Open Food Facts Bot <contact@openfoodfacts.org>

--- lib/ProductOpener/Display.pm
+++ lib/ProductOpener/Display.pm
@@ -9591,12 +9591,17 @@ sub data_to_display_nutrition_table ($product_ref, $comparisons_ref, $request_re
 	my @displayed_product_types = ();
 	my %displayed_product_types = ();
 
-	if ((not defined $product_ref->{nutrition_data}) or ($product_ref->{nutrition_data})) {
+	if (   (not defined $product_ref->{nutrition_data})
+		or ($product_ref->{nutrition_data})
+		or has_nutrition_data_for_product_type($product_ref, ""))
+	{
 		# by default, old products did not have a checkbox, display the nutrition data entry column for the product as sold
 		push @displayed_product_types, "";
 		$displayed_product_types{as_sold} = 1;
 	}
-	if ((defined $product_ref->{nutrition_data_prepared}) and ($product_ref->{nutrition_data_prepared} eq 'on')) {
+	if (   ((defined $product_ref->{nutrition_data_prepared}) and ($product_ref->{nutrition_data_prepared} eq 'on'))
+		or (has_nutrition_data_for_product_type($product_ref, "_prepared")))
+	{
 		push @displayed_product_types, "prepared_";
 		$displayed_product_types{prepared} = 1;
 	}
