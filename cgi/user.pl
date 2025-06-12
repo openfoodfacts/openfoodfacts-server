a593760c351427b0d0003ebcc885ece93a5cdb3e
RayanGoubet <rayan.goubet@.com>
2025-06-12 15:08:54 +0200

    fix: call lang directly and esq it

--- cgi/user.pl
+++ cgi/user.pl
@@ -433,8 +433,6 @@ $template_data_ref->{userid} = $userid;
 $template_data_ref->{type} = $type;
 $template_data_ref->{crm_contact_url} = get_contact_url($user_ref);
 $template_data_ref->{org_url} = ($user_ref->{org} ? "/cgi/org.pl?type=edit&orgid=$user_ref->{org}" : '');
-$template_data_ref->{localized_donate_url} = lang("donate_link");
-$template_data_ref->{cta_donate} = lang("donate");
 
 if (($type eq "edit_owner") and ($action eq "process")) {
 	$log->info("redirecting to / after changing owner", {}) if $log->is_info();
