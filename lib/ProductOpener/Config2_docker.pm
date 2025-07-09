dc68fc6ab157eb37af18e3ae98c06d8c710f6ddf
Stéphane Gigandet <stephane@openfoodfacts.org>
2025-07-09 10:18:44 +0200

    feat: integrate Product Opener with recipe-estimator service (#12131)
    This PR:
    - allows Product Opener to make calls to external product services
    providers (e.g. recipe-estimator)
    - adds an integration test for an external service (in the test Product
    Opener is in fact calling itself, but as an external service using HTTP)
    - adds radio buttons in test_ingredients_analysis.pl to select the
    ingredient percent estimator: product_opener, recipe_estimator_glop,
    recipe_estimator_scipy
    
    Caveats:
    - the recipe_estimator services are not using exactly the product opener
    service format (the product data input and ouput is at the root of the
    request and response body, instead of being in a "product" key)
    - URL for recipe_estimator is hardcoded, it will be changed to a
    configuration option --> DONE
    - test_ingredients_analysis.pl will need to be modified to allow to
    specify key nutrition facts (currently none are set) --> DONE
    
    
    ![image](https://github.com/user-attachments/assets/7a6c1b6f-d99c-47e5-9dc8-c0f9c918eed0)
    
    ---------
    
    Co-authored-by: Alex Garel <alex@openfoodfacts.org>

--- lib/ProductOpener/Config2_docker.pm
+++ lib/ProductOpener/Config2_docker.pm
@@ -57,6 +57,8 @@ BEGIN {
 		$events_username
 		$events_password
 		$redis_url
+		$recipe_estimator_url
+		$recipe_estimator_scipy_url
 		$process_global_redis_events
 		%server_options
 		$build_cache_repo
@@ -136,6 +138,16 @@ $facets_kp_url = $ENV{FACETS_KP_URL};
 # Set this to your instance of the search service to enable writes to it
 $redis_url = $ENV{REDIS_URL};
 $process_global_redis_events = $ENV{PROCESS_GLOBAL_REDIS_EVENTS};
+
+# recipe-estimator product service
+# To test a locally running recipe-estimator with product opener in a docker dev environment:
+# - run recipe-estimator with `uvicorn recipe_estimator.main:app --reload --host 0.0.0.0`
+# $recipe_estimator_url = "http://host.docker.internal:8000/api/v3/estimate_recipe";
+$recipe_estimator_url = $ENV{RECIPE_ESTIMATOR_URL};
+$recipe_estimator_scipy_url = $ENV{RECIPE_ESTIMATOR_SCIPY_URL};
+
+#$recipe_estimator_url = "http://host.docker.internal:8000/api/v3/estimate_recipe";
+#$recipe_estimator_scipy_url = "http://host.docker.internal:8000/api/v3/estimate_recipe";
 
 %server_options = (
 	private_products => $producers_platform,    # 1 to make products visible only to the owner (producer platform)
