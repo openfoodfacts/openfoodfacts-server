6a3b5fee4972ad523707f87233e635bbe9279378
Estrella <estrella.paoli1@gmail.com>
2025-07-14 15:39:56 -0400

    feat: preferred set generation with several nutrients

--- lib/ProductOpener/Nutrition.pm
+++ lib/ProductOpener/Nutrition.pm
@@ -5,19 +5,70 @@ sub generate_nutrient_set_preferred_from_sets {
     my @nutrient_sets = @$nutrient_sets_ref;
     
     my $nutrient_set_preferred_ref = {};
+    
     if (@nutrient_sets) {
-        $nutrient_set_preferred_ref = $nutrient_sets[0];
+        @nutrient_sets = sort_sets_by_priority(@nutrient_sets);
 
-        if (exists $nutrient_set_preferred_ref->{nutrients} 
-            && ref $nutrient_set_preferred_ref->{nutrients} eq 'HASH') 
-        {
-            for my $nutrient (keys %{$nutrient_set_preferred_ref->{nutrients}}) {
-                $nutrient_set_preferred_ref->{nutrients}{$nutrient}{source} = $nutrient_set_preferred_ref->{source};
-                $nutrient_set_preferred_ref->{nutrients}{$nutrient}{source_per} = $nutrient_set_preferred_ref->{per};
-            }
+        if (%{$nutrient_sets[0]}) {
+            $nutrient_set_preferred_ref->{preparation} = $nutrient_sets[0]{preparation};
+            $nutrient_set_preferred_ref->{per} = $nutrient_sets[0]{per};
+            $nutrient_set_preferred_ref->{per_quantity} = $nutrient_sets[0]{per_quantity};
+            $nutrient_set_preferred_ref->{per_unit} = $nutrient_sets[0]{per_unit};
         }
+        
+        foreach my $nutrient_set (@nutrient_sets) {
+            if (exists $nutrient_set->{nutrients} 
+                && ref $nutrient_set->{nutrients} eq 'HASH') 
+            {
+                foreach my $nutrient (keys %{$nutrient_set->{nutrients}}) {
+                    $nutrient_set_preferred_ref->{nutrients}{$nutrient} = $nutrient_set->{nutrients}{$nutrient};
+                    $nutrient_set_preferred_ref->{nutrients}{$nutrient}{source} = $nutrient_set->{source};
+                    $nutrient_set_preferred_ref->{nutrients}{$nutrient}{source_per} = $nutrient_set->{per};
+                }
+            }
+        }  
     }
     return $nutrient_set_preferred_ref;
+}
+
+
+my %source_priority = (
+    manufacturer => 0,
+    packaging => 1,
+    usda => 2,
+    estimate => 3,
+    _default => 4,
+);
+
+my %preparation_priority = (
+    prepared => 0,
+    as_sold => 1,
+    _default => 2,
+);
+
+my %per_priority = (
+    "100g" => 0,
+    "100ml" => 0,
+    serving => 1,
+    _default => 2,
+);
+
+sub sort_sets_by_priority (@nutrient_sets) {
+    return sort {
+        my $source_a = $source_priority{$a->{source}} // $source_priority{_default};
+        my $source_b = $source_priority{$b->{source}} // $source_priority{_default};
+
+        my $per_a = $per_priority{$a->{per}} // $per_priority{_default};
+        my $per_b = $per_priority{$b->{per}} // $per_priority{_default};
+
+        my $preparation_a = $preparation_priority{$a->{preparation}} // $preparation_priority{_default};
+        my $preparation_b = $preparation_priority{$b->{preparation}} // $preparation_priority{_default};
+        
+        return 
+               $source_a <=> $source_b
+            || $per_a <=> $per_b
+            || $preparation_a <=> $preparation_b;
+    } @nutrient_sets;
 }
 
 1;
--- tests/unit/nutrition.t
+++ tests/unit/nutrition.t
@@ -36,10 +36,7 @@ my @tests = (
             preparation => "as_sold", 
             per => "100g", 
             per_quantity => "100", 
-            per_unit => "g", 
-            source => "packaging", 
-            source_description => "Information from packaging", 
-            last_updated_t => "1200000", 
+            per_unit => "g",
             nutrients => {
                 sodium => {
                     value_string => "2.0", 
@@ -52,6 +49,66 @@ my @tests = (
             }
         },
     ],
+    [
+        [
+            {
+                preparation => "as_sold", 
+                per => "100g", 
+                per_quantity => "100", 
+                per_unit => "g", 
+                source => "packaging", 
+                source_description => "Information from packaging", 
+                last_updated_t => "1200000", 
+                nutrients => {
+                    sodium => {
+                        value_string => "2.0", 
+                        value => 2, 
+                        unit => "mg", 
+                        modifier => "<="
+                    }
+                }
+            },
+            {
+                preparation => "as_sold", 
+                per => "100g", 
+                per_quantity => "100", 
+                per_unit => "g", 
+                source => "usda", 
+                source_description => "Import from usda in 2024", 
+                last_updated_t => "12450000", 
+                nutrients => {
+                    sugars => {
+                        value_string => "5.2", 
+                        value => 5.2, 
+                        unit => "mg", 
+                    }
+                }
+            }
+        ],
+        {
+            preparation => "as_sold", 
+            per => "100g", 
+            per_quantity => "100", 
+            per_unit => "g", 
+            nutrients => {
+                sodium => {
+                    value_string => "2.0", 
+                    value => 2, 
+                    unit => "mg", 
+                    modifier => "<=",
+                    source => "packaging",
+                    source_per => "100g",
+                },
+                sugars => {
+                    value_string => "5.2", 
+                    value => 5.2, 
+                    unit => "mg", 
+                    source => "usda",
+                    source_per => "100g",
+                }
+            }
+        },
+    ],
 );      
 
 foreach my $test_ref (@tests) {
@@ -59,8 +116,7 @@ foreach my $test_ref (@tests) {
 	my $nutrient_sets_ref = $test_ref->[0];
 	my $nutrient_set_preferred = $test_ref->[1];
 
-	is(generate_nutrient_set_preferred_from_sets($nutrient_sets_ref), $nutrient_set_preferred)
-;
+	is(generate_nutrient_set_preferred_from_sets($nutrient_sets_ref), $nutrient_set_preferred);
 }
 
 done_testing();
