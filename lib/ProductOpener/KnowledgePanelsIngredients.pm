87418fa36725e128bfb8dfe259eff9885f3b7118
benbenben2 <110821832+benbenben2@users.noreply.github.com>
2025-08-06 20:08:41 +0200

    small fix

--- lib/ProductOpener/KnowledgePanelsIngredients.pm
+++ lib/ProductOpener/KnowledgePanelsIngredients.pm
@@ -228,7 +228,7 @@ sub create_ingredients_added_sugars_panel ($product_ref, $target_lc, $target_cc,
 					= format_subdomain($subdomain)
 					. "/facets"
 					. canonicalize_taxonomy_tag_link($target_lc, 'categories', $category_id)
-					. canonicalize_taxonomy_tag_link($target_lc, 'data-quality-info',
+					. canonicalize_taxonomy_tag_link($target_lc, 'data_quality_info',
 					"en:ingredients-completed-at-least-for-one-language")
 					. $no_added_sugars_link
 					. $no_sweeteners_link;
