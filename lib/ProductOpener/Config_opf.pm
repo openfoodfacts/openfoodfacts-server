9241cd7513491c0e664a68e0b0472ee83d36b4e2
Pierre Slamich <pierre@openfoodfacts.org>
2025-08-26 15:29:30 +0200

    fix: add export configurations for all flavors (#12230)
    - fixes: #12226
    
    ---------
    
    Co-authored-by: Manon Corneille <104015489+manon-corneille@users.noreply.github.com>
    Co-authored-by: Alex Garel <alex@openfoodfacts.org>

--- lib/ProductOpener/Config_opf.pm
+++ lib/ProductOpener/Config_opf.pm
@@ -514,6 +514,46 @@ HTML
 	last_image_t
 );
 
+# List of fields that can be imported on the producers platform
+# and that are also exported from the producers platform to the public platform
+$options{import_export_fields_groups} = [
+	[
+		"identification",
+		[
+			"code", "producer_product_id",
+			"producer_version_id", "lc",
+			"product_name", "abbreviated_product_name",
+			"generic_name",
+			"quantity_value_unit", "net_weight_value_unit",
+			"drained_weight_value_unit", "volume_value_unit",
+			"packaging",
+			"brands", "brand_owner",
+			"categories", "categories_specific",
+			"labels", "labels_specific",
+			"countries", "stores",
+			"obsolete", "obsolete_since_date",
+			"periods_after_opening"    # included for OBF imports via the producers platform
+		]
+	],
+	[
+		"origins",
+		["origins", "origin", "manufacturing_places", "producer"]
+	],
+	["packaging"],
+	[
+		"other",
+		["customer_service", "link",]
+	],
+	[
+		"images",
+		["image_front_url", "image_ingredients_url", "image_packaging_url", "image_other_url", "image_other_type",]
+	],
+];
+
+# Secondary fields that are computed by OPF from primary data
+# Those fields are only exported, they are not imported.
+# Todo: populate when calculated indicators are available on OPF
+
 # Used to generate the list of possible product attributes, which is
 # used to display the possible choices for user preferences
 $options{attribute_groups}
