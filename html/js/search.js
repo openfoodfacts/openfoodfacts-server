9b6ac8673065d20d85ad998b31e761548fffe0b6
Stéphane Gigandet <stephane@openfoodfacts.org>
2025-10-24 16:25:06 +0200

    npm lint fix

--- html/js/search.js
+++ html/js/search.js
@@ -18,6 +18,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+/*global folksonomy_url */
+
 function modifySearchCriterion(element, criterion_number) {
 	//Type of criterion
 	const selects = element.find('select');
