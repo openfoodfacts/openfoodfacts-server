1342648f950bb583b005d7940c3db60bc8c713ff
Alex Garel <alex@garel.org>
2025-11-17 09:09:08 +0100

    chore: lint perl

--- lib/ProductOpener/Products.pm
+++ lib/ProductOpener/Products.pm
@@ -3157,8 +3157,8 @@ sub process_product_edit_rules ($product_ref) {
 						$proceed_with_edit = 0;
 					}
 					# rules with conditions
-					elsif (
-						$action =~ /^(ignore|warn|block)(_if_(existing|0|greater|lesser|equal|match|regexp_match))?_?(.*)$/)
+					elsif ($action
+						=~ /^(ignore|warn|block)(_if_(existing|0|greater|lesser|equal|match|regexp_match))?_?(.*)$/)
 					{
 						my ($type, $condition, $field) = ($1, $3, $4);
 						my $default_field = $field;
