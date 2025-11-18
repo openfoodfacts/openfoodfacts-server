51e05357ee4bd1ea2c37a3377f93ad25e572f6cc
Alex Garel <alex@openfoodfacts.org>
2025-11-18 12:02:26 +0100

    fix: rule to block image selection should also match without language code
    Co-authored-by: Stéphane Gigandet <stephane@openfoodfacts.org>

--- lib/ProductOpener/Config_off.pm
+++ lib/ProductOpener/Config_off.pm
@@ -431,7 +431,7 @@ $options{users_who_can_upload_small_images} = {
 			["ignore_nutriment_calcium"], ["ignore_nutriment_potassium"],
 			["ignore_serving_size"],
 			# block image selection
-			["block_if_regexp_match_id", "^(front|ingredients|nutrition|packaging)_.*"],
+			["block_if_regexp_match_id", "^(front|ingredients|nutrition|packaging).*"],
 		],
 	},
 );
