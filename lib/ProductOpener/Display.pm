openfoodfacts/openfoodfacts-server

commit b4a92fd7186b70c0e1967ece6433fa96a029521e

fix: remove some dead code - remove JQM support for old Cordova app (#12714)
Removing some dead code:
- support for jquerymobile, used in the Cordova app
- unused code and templates for product page that have been replaced
with knowledge panels

---------

Co-authored-by: Open Food Facts Bot <contact@openfoodfacts.org>

templates/web/pages/product/includes/nutrition_facts_table.tt.html
@@ -1,146 +1,0 @@
-<!-- start templates/[% component.name %] -->
-
-<!-- Nutrition facts table -->
-[% IF comparisons.defined %]
-
-	<!-- Comparison to category block -->
-    <p> [% lang('nutrition_data_comparison_with_categories') %] </p>
-
-	<!-- Comparisons with other products, categories, recommended daily values etc. -->
-	[% FOREACH comparison IN comparisons %]
-		<label style="display:inline;font-size:1rem;">
-			<input type="checkbox" id="[% comparison.col_id %]" name="[% comparison.col_id %]" value="on" [% IF comparison.checked %]checked="checked"[% END %] class="show_comparison">
-			[% comparison.name %]
-		</label>
-
-
-		[% IF comparison.count.defined %]
-			<a href=[% comparison.link %]> ([% comparison.count %] [% lang('products') %])</a>
-		[% END %]
-
-		<br>
-
-	[% END %]
-
-	<br>
-	<input type="radio" id="nutrition_data_compare_percent" value="compare_percent" name="nutrition_data_compare_type" checked>
-	<label for="nutrition_data_compare_percent">[% lang('nutrition_data_compare_percent') %]</label>
-	<input type="radio" id="nutrition_data_compare_value" value="compare_value" name="nutrition_data_compare_type">
-	<label for="nutrition_data_compare_value">[% lang('nutrition_data_compare_value') %]</label>
-
-	<p class="note">&rarr; [% lang('nutrition_data_comparison_with_categories_note') %]</p>
-
-<initjs>
-\$('input:radio[name=nutrition_data_compare_type]').change(function () {
-
-	if (\$('input:radio[name=nutrition_data_compare_type]:checked').val() == 'compare_value') {
-		\$(".compare_percent").hide();
-		\$(".compare_value").show();
-	}
-	else {
-		\$(".compare_value").hide();
-		\$(".compare_percent").show();
-	}
-
-}
-);
-
-
-\$(".show_comparison").change(function () {
-	if (\$(this).prop('checked')) {
-		\$("." + \$(this).attr("id")).show();
-	}
-	else {
-		\$("." + \$(this).attr("id")).hide();
-	}
-}
-);
-</initjs>
-
-[% END %]
-
-<!--  Stats for categories -->
-[% IF category_stats.defined %]
-	<div>
-		<input id="show_stats" type="checkbox">
-		<label for="show_stats">
-			[% lang('show_category_stats') %]
-			<span class="show-for-xlarge-up">
-				[% separator_before_colon(lc) %]: [% lang('show_category_stats_details') %]
-			</span>
-		</label>
-	</div>
-
-<initjs>
-if (\$.cookie('show_stats') == '1') {
-	\$('#show_stats').prop('checked',true);
-}
-else {
-	\$('#show_stats').prop('checked',false);
-}
-
-if (\$('#show_stats').prop('checked')) {
-	\$(".stats").show();
-}
-else {
-	\$(".stats").hide();
-}
-
-\$("#show_stats").change(function () {
-	if (\$('#show_stats').prop('checked')) {
-		\$.cookie('show_stats', '1', { expires: 365 });
-		\$(".stats").show();
-	}
-	else {
-		\$.cookie('show_stats', null);
-		\$(".stats").hide();
-	}
-}
-);
-
-</initjs>	
-
-[% END %]
-
-<table id="[% nutrition_table.id %]_data_table" class="data_table">
-<caption style="display:none">[% nutrition_table.header.name %]</caption>
-
-	<!--  Generating table header -->
-	<thead class="nutriment_header">
-		<tr>
-			<th scope="col">[% nutrition_table.header.name %]</th>
-			[% FOREACH column IN nutrition_table.header.columns %]
-				<th class="nutriment_value [% column.class %]" scope="col">[% column.name %]</th>
-			[% END %]
-		</tr>
-	</thead>
-
-	<tbody>
-		<!--  Generating table body -->
-		[% FOREACH row IN nutrition_table.rows %]
-
-			<tr id="nutriment_[% row.nid %]_tr" class="[% IF row.level == 0 %]nutriment_main[% ELSIF row.level == 1 %]nutriment_sub[% ELSIF row.level == 2 %]nutriment_sub nutriment_sub_sub[% END %]">
-				<td class="nutriment_label">
-					[% row.name %]
-				</td>
-
-				[% FOREACH column IN row.columns %]
-					[% IF column.type == 'normal' %]
-						<td class="nutriment_value [% column.class %]"[% IF column.rdfa.defined %][% column.rdfa %][% END %]>
-							[% column.value %]
-						</td>
-					[% ELSE %]
-						<td class="nutriment_value [% column.class %]">
-							<span class="compare_percent">[% column.percent %]</span>
-							<span class="compare_value" style="display:none">[% column.value %]</span>
-						</td>
-					[% END %]
-				[% END %]
-			</tr>
-
-		[% END %]
-	</tbody>
-
-</table>
-
-<!-- end templates/[% component.name %] -->

lib/ProductOpener/API.pm
@@ -851,22 +851,6 @@ sub customize_response_for_product ($request_ref, $product_ref, $fields_comma_se
 			next;
 		}
 
-		# Allow apps to request a HTML nutrition table by passing &fields=nutrition_table_html
-		if ($field eq "nutrition_table_html") {
-			$customized_product_ref->{$field} = display_nutrition_table($product_ref, undef, $request_ref);
-			next;
-		}
-
-		# Environmental-Score details in simple HTML
-		if ($field eq "environmental_score_details_simple_html") {
-			if ((1 or $show_environmental_score) and (defined $product_ref->{environmental_score_data})) {
-				$customized_product_ref->{$field}
-					= display_environmental_score_calculation_details_simple_html($request_ref->{cc},
-					$product_ref->{environmental_score_data});
-			}
-			next;
-		}
-
 		# fields in %language_fields can have different values by language
 		# by priority, return the first existing value in the language requested,
 		# possibly multiple languages if sent ?lc=fr,nl for instance,

templates/web/common/includes/display_product_api.tt.html
@@ -1,29 +1,0 @@
-[% Lang.app_please_take_pictures.$request_lc %]
-<button onclick="captureImage();" data-icon="off-camera">[% Lang.app_take_a_picture.$request_lc %]</button>
-<div id="upload_image_result"></div>
-<p>[% Lang.app_take_a_picture_note.$request_lc %]</p>
-
-[% IF api_version >= 0.1 %]
-    <form id="product_fields" action="javascript:void(0);">
-        <div data-role="fieldcontain" class="ui-hide-label" style="border-bottom-width: 0;">
-            [% FOREACH field IN app_fields %]
-                [% value = Lang.$field.$request_lc %]
-                <label for="[% field %]">[% Lang.$field.$request_lc %]</label>
-                <input type="text" name="[% field %]" id="[% field %]" value="" placeholder="[% value %]">
-            [% END %]
-        </div>
-
-        <div id="save_button">
-            <input type="submit" id="save" name="save" value="[% Lang.save.$request_lc %]">
-        </div>
-        <div id="saving" style="display:none">
-            <img src="loading2.gif" style="margin-right:10px"> [% Lang.saving.$request_lc %]
-        </div>
-        <div id="saved" style="display:none">
-            [% Lang.saved.$request_lc %]
-        </div>
-        <div id="not_saved" style="display:none">
-            [% Lang.not_saved.$request_lc %]
-        </div>
-    </form>
-[% END %]

templates/web/pages/product/includes/environmental_score_details.tt.html
@@ -1,249 +1,0 @@
-<!-- start templates/[% component.name %] -->
-
-<p>
-  <a data-dropdown="environmental_score_drop" aria-controls="environmental_score_drop" aria-expanded="false">[% lang('environmental_score_calculation_details') %] &raquo;</a>
-</p>
-<div id="environmental_score_drop" data-dropdown-content class="f-dropdown content large" aria-hidden="true" tabindex="-1">
-
-	<h3>[% lang('environmental_score_product_category_reference_score') %]</h3>
-
-	<div class="row">
-	<div class="small-12 columns">
-		<div class="panel environmental_score_panel" id="environmental_score_panel_lca">
-	<h4>[% display_icon('lca') %] [% lang('environmental_score_panel_lca') %]</h4>
-	
-	[% IF agribalyse.code %]
-	
-	<p>
-		[% lang('environmental_score_agribalyse_category') %]</a>[% sep %]: <a href="https://www.agribalyse.fr/app/aliments/[% agribalyse.code %]" title="
-		[% IF agribalyse.agribalyse_proxy_food_code %]
-			[% lang('environmental_score_category_proxy_match') %]
-		[% ELSE %]
-			[% lang('environmental_score_category_exact_match') %]
-		[% END %]
-		">
-		[% IF lc == "fr" %]
-			[% agribalyse.name_fr %]
-		[% ELSE %]
-			[% agribalyse.name_en %]
-		[% END %]
-		</a>
-	</p>
-	<ul>
-		<li>[% lang('environmental_score_pef_environmental_score') %][% sep %]: [% agribalyse.ef_total FILTER format("%.2f"); %] [% lang('environmental_score_lower_the_score_lower_the_impact') %]</li>
-		<li>- [% lang('environmental_score_incl_climate_change_impact') %][% sep %]: [% agribalyse.co2_total FILTER format("%.2f"); %] [% lang('environmental_score_kg_co2_eq_kg_product') %]</li>
-	</ul>
-	
-		<table id="agribalyse_impacts">
-			<caption style="text-align:left">[% lang('environmental_score_impact_detail_by_stages') %]</caption>
-			<tr>
-				<th scope="col">[% lang('stage') %]</th>
-				<th scope="colgroup" colspan="2">[% lang('environmental_score_impact') %]</th>
-			</tr>
-			<tr>
-				<td>[% display_icon('agriculture') %] [% lang('environmental_score_agriculture') %]</td>
-				<td style="text-align:right">[% (100 * agribalyse.ef_agriculture / agribalyse.ef_total) FILTER format("%.1f"); %]%</td>
-				<td style="width:220px;" class="show-for-large-up">
-					<div class="agribalyse_impact_bar_full">
-						<div class="agribalyse_impact_bar" style="width:[% round(200 * agribalyse.ef_agriculture / agribalyse.ef_total)%]px;"></div>
-					</div>
-				</td>
-			</tr>
-			<tr>
-				<td>[% display_icon('processing') %] [% lang('environmental_score_processing') %]</td>
-				<td style="text-align:right">[% (100 * agribalyse.ef_processing / agribalyse.ef_total) FILTER format("%.1f"); %]%</td>
-				<td style="width:200px;" class="show-for-large-up">
-					<div class="agribalyse_impact_bar_full">
-						<div class="agribalyse_impact_bar"  style="width:[% round(200 * agribalyse.ef_processing / agribalyse.ef_total)%]px;"></div>
-					</div>
-				</td>				
-			</tr>
-			<tr>
-				<td>[% display_icon('packaging') %] [% lang('environmental_score_packaging') %]</td>
-				<td style="text-align:right">[% (100 * agribalyse.ef_packaging / agribalyse.ef_total) FILTER format("%.1f"); %]%</td>
-				<td style="width:200px;" class="show-for-large-up">
-					<div class="agribalyse_impact_bar_full">
-						<div class="agribalyse_impact_bar"  style="width:[% round(200 * agribalyse.ef_packaging / agribalyse.ef_total)%]px;"></div>
-					</div>
-				</td>				
-			</tr>
-			<tr>
-				<td>[% display_icon('truck') %] [% lang('environmental_score_transportation') %]</td>
-				<td style="text-align:right">[% (100 * agribalyse.ef_transportation / agribalyse.ef_total) FILTER format("%.1f"); %]%</td>
-				<td style="width:200px;" class="show-for-large-up">
-					<div class="agribalyse_impact_bar_full">
-						<div class="agribalyse_impact_bar"  style="width:[% round(200 * agribalyse.ef_transportation / agribalyse.ef_total)%]px;"></div>
-					</div>
-				</td>				
-			</tr>
-			<tr>
-				<td>[% display_icon('cart') %] [% lang('environmental_score_distribution') %]</td>
-				<td style="text-align:right">[% (100 * agribalyse.ef_distribution / agribalyse.ef_total) FILTER format("%.1f"); %]%</td>
-				<td style="width:200px;" class="show-for-large-up">
-					<div class="agribalyse_impact_bar_full">
-						<div class="agribalyse_impact_bar"  style="width:[% round(200 * agribalyse.ef_distribution / agribalyse.ef_total)%]px;"></div>
-					</div>
-				</td>				
-			</tr>
-			<tr>
-				<td>[% display_icon('cooking-pan') %] [% lang('environmental_score_consumption') %]</td>
-				<td style="text-align:right">[% (100 * agribalyse.ef_consumption / agribalyse.ef_total) FILTER format("%.1f"); %]%</td>
-				<td style="width:200px;" class="show-for-large-up">
-					<div class="agribalyse_impact_bar_full">
-						<div class="agribalyse_impact_bar"  style="width:[% round(200 * agribalyse.ef_consumption / agribalyse.ef_total)%]px;background-color:#ffbb88;height:1.2rem;"></div>
-					</div>
-				</td>				
-			</tr>
-		</table>	
-	
-	<p>
-		<strong>[% lang('environmental_score_lca_score_out_of_100') %][% sep %]: [% round(agribalyse.score) %]</strong>
-	</p>
-	
-	[% ELSE %]
-	<div class="panel warning">
-		<strong>[% lang('environmental_score_no_agribalyse_category_match') %]</strong><br><br>
-		[% lang('environmental_score_edit_category_to_more_granular') %]<br><br>
-		[% lang('environmental_score_platform_prompt_environmental_score_modal') %]
-	</div>
-	[% END %]
-	
-	</div>
-	</div>
-	</div>
-
-	<h3>[% lang('environmental_score_additional_bonuses_and_maluses') %]</h3>
-	
-	<div class="row" data-equalizer="environmental_score1">
-	
-	<div class="small-12 large-6 columns">
-		 <div class="panel environmental_score_panel" id="environmental_score_panel_production_system" data-equalizer-watch="environmental_score1">
-	<h4>[% display_icon('agriculture') %] [% lang('environmental_score_production_system') %]</h4>
-	
-	[% IF adjustments.production_system.value.defined && adjustments.production_system.value > 0 %]
-		
-		<ul>
-		[% FOREACH label IN adjustments.production_system.labels %]
-			<li>[% display_taxonomy_tag("labels",label) %]</li>
-		[% END %]
-		</ul>
-	
-		<p><strong>[% lang('environmental_score_production_system') %][% sep %]: +[% adjustments.production_system.value %]</strong></p>
-	[% ELSE %]
-		<p>[% lang('environmental_score_no_labels_taken_into_account') %]</p>
-		
-		[% IF adjustments.production_system.warning %]
-			<div class="panel warning">
-				[% lang('environmental_score_please_add_the_labels') %]<br><br>
-				[% lang('environmental_score_platform_prompt_environmental_score_modal') %]
-			</div>
-		[% END %]		
-		
-	[% END %]
-	</div>
-	</div>
-	
-	<div class="small-12 large-6 columns">	
-		 <div class="panel environmental_score_panel" id="environmental_score_panel_origins" data-equalizer-watch="environmental_score1">
-	<h4>[% display_icon('public') %] [% lang('environmental_score_origins_of_ingredients') %]</h4>
-	
-	[% IF adjustments.origins_of_ingredients.aggregated_origins %]
-		<ul>
-			[% FOREACH origin IN adjustments.origins_of_ingredients.aggregated_origins %]
-				<li>[% display_taxonomy_tag("origins",origin.origin) %][% sep %]: [% round(origin.percent) %]%</li>
-			[% END %]
-		</ul>
-	[% END %]
-	
-	[% IF adjustments.origins_of_ingredients.warning %]
-		<div class="panel warning">
-			<strong>[% lang('environmental_score_ingredients_not_indicated') %]</strong><br><br>
-			[% lang('environmental_score_please_add_the_ingredients') %]<br><br>
-			[% lang('environmental_score_platform_prompt_environmental_score_modal') %]
-		</div>
-	[% END %]
-
-	<p>
-		<strong>[% lang('environmental_score_environmental_policy') %][% sep %]: [% IF adjustments.origins_of_ingredients.epi_value.defined && adjustments.origins_of_ingredients.epi_value > 0 %]+[% END %][% round(adjustments.origins_of_ingredients.epi_value) %]</strong><br>
-		<strong>[% lang('environmental_score_transportation') %][% sep %]: [% IF adjustments.origins_of_ingredients.epi_value.defined && adjustments.origins_of_ingredients.transportation_value > 0 %]+[% END %][% round(adjustments.origins_of_ingredients.transportation_value) %]</strong><br>
-	</p>
-	</div>
-	</div>
-	</div>
-	
-	<div class="row" data-equalizer="environmental_score2">
-	
-	<div class="small-12 large-6 columns">
-		 <div class="panel environmental_score_panel" id="environmental_score_panel_threatened_species" data-equalizer-watch="environmental_score2">
-	<h4>[% display_icon('monkey_unhappy') %] [% lang('environmental_score_threatened_species') %]</h4>
-	
-	[% IF adjustments.threatened_species.value %]
-		<p>[% lang('environmental_score_ingredients_whose_cultivation_threatens_species') %][% sep %]:</p>
-		<ul>
-			<li>[% display_taxonomy_tag("ingredients",adjustments.threatened_species.ingredient) %]</li>
-		</ul>
-		<p><strong>[% lang('environmental_score_threatened_species') %][% sep %]: [% adjustments.threatened_species.value %]</strong></p>
-	[% ELSE %]
-		<p>[% lang('environmental_score_no_species_threatening_ingredients') %]</p>
-	[% END %]
-	
-	[% IF adjustments.threatened_species.warning %]
-		<div class="panel warning">
-			<strong>[% lang('environmental_score_ingredients_unknown') %]</strong><br><br>
-			[% lang('environmental_score_edit_for_more_precise_environmental_score') %]<br><br>
-			[% lang('environmental_score_platform_prompt_environmental_score_modal') %]
-		</div>
-	[% END %]	
-	
-	</div>
-	</div>
-	
-	<div class="small-12 large-6 columns">
-		 <div class="panel environmental_score_panel" id="environmental_score_panel_packaging" data-equalizer-watch="environmental_score2">
-	
-	<h4>[% display_icon('packaging') %] [% lang('environmental_score_packaging') %]</h4>
-	
-	[% IF adjustments.packaging.warning %]
-		<div class="panel warning">
-			<strong>
-				[% IF adjustments.packaging.warning == "packaging_data_missing" %]
-					[% lang('environmental_score_no_packaging_information') %]
-				[% ELSE %]
-					[% lang('environmental_score_unprecise_packaging_information') %]
-				[% END %]
-			</strong>
-			<br><br>
-			[% lang('environmental_score_edit_for_more_precise_environmental_score') %]<br><br>
-			[% lang('environmental_score_platform_prompt_environmental_score_modal') %]
-		</div>
-	[% END %]
-
-	[% IF adjustments.packaging.packagings && adjustments.packaging.packagings.size %]
-		<ul>
-			[% FOREACH packaging IN adjustments.packaging.packagings %]
-				<li>
-					[% display_taxonomy_tag("packaging_shapes",packaging.shape) %]
-					([% lang('environmental_score_packaging_ratio') %][% sep %]: [% packaging.environmental_score_shape_ratio %])
-					-
-					[% display_taxonomy_tag("packaging_materials",packaging.material) %]
-					([% lang('environmental_score_packaging_score') %][% sep %]: [% packaging.environmental_score_material_score %])
-				</li>
-			[% END %]
-		</ul>
-		<p>[% lang('environmental_score_score_of_all_components') %][% sep %]: [% adjustments.packaging.score %]</p>	
-	[% END %]
-
-	<p><strong>[% lang('environmental_score_packaging') %][% sep %]: [% IF adjustments.packaging.value.defined && adjustments.packaging.value > 0 %]+[% END %][% adjustments.packaging.value %]</strong></p>
-	
-	</div>
-	</div>
-	</div>
-	
-	<h3>[% lang('environmental_score_final_score') %]</h3>
-
-	<p><strong>[% lang('environmental_score_score') %][% sep %]: [% round(score) %]</strong>
-	- <strong>[% lang('environmental_score_grade') %][% sep %]: [% grade FILTER ucfirst %]</strong></p>
-
-</div>
-
-<!-- end templates/[% component.name %] -->

commit 4c04090761c1c82e53324414a55f404467bbf324

fix: missing template for categories nutrition table (#12782)
Fixes bug
https://github.com/openfoodfacts/openfoodfacts-server/issues/12770
introduced by
https://github.com/openfoodfacts/openfoodfacts-server/pull/12714

I'll try to add some tests.

---------

Co-authored-by: Open Food Facts Bot <contact@openfoodfacts.org>

lib/ProductOpener/Display.pm
@@ -9915,7 +9915,10 @@ CSS
 
 Generates HTML to display a nutrition table.
 
-Use  data produced by data_to_display_nutrition_table
+Use data produced by data_to_display_nutrition_table
+
+This function is no longer used to display the nutrition table on product pages (replaced by a knowledge panel),
+but it is used to show average nutrition tables for categories.
 
 =head3 Arguments
 

templates/web/pages/product/includes/nutrition_facts_table.tt.html
@@ -1,0 +1,146 @@
+<!-- start templates/[% component.name %] -->
+
+<!-- Nutrition facts table -->
+[% IF comparisons.defined %]
+
+	<!-- Comparison to category block -->
+    <p> [% lang('nutrition_data_comparison_with_categories') %] </p>
+
+	<!-- Comparisons with other products, categories, recommended daily values etc. -->
+	[% FOREACH comparison IN comparisons %]
+		<label style="display:inline;font-size:1rem;">
+			<input type="checkbox" id="[% comparison.col_id %]" name="[% comparison.col_id %]" value="on" [% IF comparison.checked %]checked="checked"[% END %] class="show_comparison">
+			[% comparison.name %]
+		</label>
+
+
+		[% IF comparison.count.defined %]
+			<a href=[% comparison.link %]> ([% comparison.count %] [% lang('products') %])</a>
+		[% END %]
+
+		<br>
+
+	[% END %]
+
+	<br>
+	<input type="radio" id="nutrition_data_compare_percent" value="compare_percent" name="nutrition_data_compare_type" checked>
+	<label for="nutrition_data_compare_percent">[% lang('nutrition_data_compare_percent') %]</label>
+	<input type="radio" id="nutrition_data_compare_value" value="compare_value" name="nutrition_data_compare_type">
+	<label for="nutrition_data_compare_value">[% lang('nutrition_data_compare_value') %]</label>
+
+	<p class="note">&rarr; [% lang('nutrition_data_comparison_with_categories_note') %]</p>
+
+<initjs>
+\$('input:radio[name=nutrition_data_compare_type]').change(function () {
+
+	if (\$('input:radio[name=nutrition_data_compare_type]:checked').val() == 'compare_value') {
+		\$(".compare_percent").hide();
+		\$(".compare_value").show();
+	}
+	else {
+		\$(".compare_value").hide();
+		\$(".compare_percent").show();
+	}
+
+}
+);
+
+
+\$(".show_comparison").change(function () {
+	if (\$(this).prop('checked')) {
+		\$("." + \$(this).attr("id")).show();
+	}
+	else {
+		\$("." + \$(this).attr("id")).hide();
+	}
+}
+);
+</initjs>
+
+[% END %]
+
+<!--  Stats for categories -->
+[% IF category_stats.defined %]
+	<div>
+		<input id="show_stats" type="checkbox">
+		<label for="show_stats">
+			[% lang('show_category_stats') %]
+			<span class="show-for-xlarge-up">
+				[% separator_before_colon(lc) %]: [% lang('show_category_stats_details') %]
+			</span>
+		</label>
+	</div>
+
+<initjs>
+if (\$.cookie('show_stats') == '1') {
+	\$('#show_stats').prop('checked',true);
+}
+else {
+	\$('#show_stats').prop('checked',false);
+}
+
+if (\$('#show_stats').prop('checked')) {
+	\$(".stats").show();
+}
+else {
+	\$(".stats").hide();
+}
+
+\$("#show_stats").change(function () {
+	if (\$('#show_stats').prop('checked')) {
+		\$.cookie('show_stats', '1', { expires: 365 });
+		\$(".stats").show();
+	}
+	else {
+		\$.cookie('show_stats', null);
+		\$(".stats").hide();
+	}
+}
+);
+
+</initjs>	
+
+[% END %]
+
+<table id="[% nutrition_table.id %]_data_table" class="data_table">
+<caption style="display:none">[% nutrition_table.header.name %]</caption>
+
+	<!--  Generating table header -->
+	<thead class="nutriment_header">
+		<tr>
+			<th scope="col">[% nutrition_table.header.name %]</th>
+			[% FOREACH column IN nutrition_table.header.columns %]
+				<th class="nutriment_value [% column.class %]" scope="col">[% column.name %]</th>
+			[% END %]
+		</tr>
+	</thead>
+
+	<tbody>
+		<!--  Generating table body -->
+		[% FOREACH row IN nutrition_table.rows %]
+
+			<tr id="nutriment_[% row.nid %]_tr" class="[% IF row.level == 0 %]nutriment_main[% ELSIF row.level == 1 %]nutriment_sub[% ELSIF row.level == 2 %]nutriment_sub nutriment_sub_sub[% END %]">
+				<td class="nutriment_label">
+					[% row.name %]
+				</td>
+
+				[% FOREACH column IN row.columns %]
+					[% IF column.type == 'normal' %]
+						<td class="nutriment_value [% column.class %]"[% IF column.rdfa.defined %][% column.rdfa %][% END %]>
+							[% column.value %]
+						</td>
+					[% ELSE %]
+						<td class="nutriment_value [% column.class %]">
+							<span class="compare_percent">[% column.percent %]</span>
+							<span class="compare_value" style="display:none">[% column.value %]</span>
+						</td>
+					[% END %]
+				[% END %]
+			</tr>
+
+		[% END %]
+	</tbody>
+
+</table>
+
+<!-- end templates/[% component.name %] -->