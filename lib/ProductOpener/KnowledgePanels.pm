a77ae5bca5b1e6d815d2798467380ee094c5b49b
Alex Garel <alex@garel.org>
2026-02-10 17:36:54 +0100

    test: refactor maintain knowledge panel and test

--- lib/ProductOpener/KnowledgePanels.pm
+++ lib/ProductOpener/KnowledgePanels.pm
@@ -52,6 +52,7 @@ BEGIN {
 		&create_panel_from_json_template
 		&add_taxonomy_properties_in_target_languages_to_object
 		&create_reuse_card_panel
+		&create_maintain_card_panel
 
 	);    # symbols to export on request
 	%EXPORT_TAGS = (all => [@EXPORT_OK]);
@@ -1082,22 +1083,68 @@ Created for products in specific categories that have maintenance URLs, for user
 
 =head4 product reference $product_ref
 
-Loaded from the MongoDB database, Storable files, or the OFF API.
+The panel is added in the product
 
 =head4 language code $target_lc
 
-Returned attributes contain both data and strings intended to be displayed to users.
-This parameter sets the desired language for the user facing strings.
-
 =head4 country code $target_cc
 
-Used to select maintenance resources that are relevant for the user.
+=head4 options reference $options_ref
+
+=head4 request reference $request_ref
+
+=head3 Return value
+
+1 if panel created, 0 otherwise
 
 =cut
 
 sub create_maintain_card_panel ($product_ref, $target_lc, $target_cc, $options_ref, $request_ref) {
 
 	$log->debug("create maintain card panel", {code => $product_ref->{code}}) if $log->is_debug();
+
+	my $sub_panel_count = 0;
+
+	$sub_panel_count
+		+= create_epargnonsnosressources_panel($product_ref, $target_lc, $target_cc, $options_ref, $request_ref);
+
+	my $panel_data_ref = {};
+
+	# Create the maintain_card panel only if we have created at least one sub-panel
+	if ($sub_panel_count > 0) {
+		create_panel_from_json_template("maintain_card", "api/knowledge-panels/maintain/maintain_card.tt.json",
+			$panel_data_ref, $product_ref, $target_lc, $target_cc, $options_ref, $request_ref);
+		return 1;
+	}
+
+	return 0;
+}
+
+=head2 create_epargnonsnosressources_panel ( $product_ref, $target_lc, $target_cc, $options_ref, $request_ref)
+
+Link to epargnonsnosressource website
+
+=head3 Arguments
+
+=head4 product reference $product_ref
+
+The panel is added in the product
+
+=head4 language code $target_lc
+
+=head4 country code $target_cc
+
+=head4 options reference $options_ref
+
+=head4 request reference $request_ref
+
+=head3 Return value
+
+1 if panel created, 0 otherwise
+
+=cut
+
+sub create_epargnonsnosressources_panel ($product_ref, $target_lc, $target_cc, $options_ref, $request_ref) {
 
 	my $panel_data_ref = {};
 
@@ -1108,13 +1155,12 @@ sub create_maintain_card_panel ($product_ref, $target_lc, $target_cc, $options_r
 
 	# Add the name of the most specific category (last in categories_hierarchy) to the panel data
 	my $category_id;
-	if (ref($product_ref->{categories_hierarchy}) eq 'ARRAY'
-		&& @{$product_ref->{categories_hierarchy}})
+	if (
+		not(ref($product_ref->{categories_hierarchy}) eq 'ARRAY'
+			and @{$product_ref->{categories_hierarchy}})
+		)
 	{
-		$category_id = $product_ref->{categories_hierarchy}[-1];
-		if (defined $category_id && $category_id ne q{}) {
-			$panel_data_ref->{category_name} = display_taxonomy_tag_name($target_lc, "categories", $category_id);
-		}
+		return 0;
 	}
 
 	# Check if the product has a category with a maintenance URL
@@ -1122,6 +1168,8 @@ sub create_maintain_card_panel ($product_ref, $target_lc, $target_cc, $options_r
 		= get_inherited_property_from_categories_tags($product_ref, "epargnonsnosressources_fr_link:en");
 
 	if (defined $maintenance_url) {
+		$category_id = $product_ref->{categories_hierarchy}[-1];
+		$panel_data_ref->{category_name} = display_taxonomy_tag_name($target_lc, "categories", $category_id);
 		$panel_data_ref->{maintenance_url} = $maintenance_url;
 		$panel_data_ref->{category_with_maintenance_url} = $category_with_url;
 
@@ -1137,14 +1185,8 @@ sub create_maintain_card_panel ($product_ref, $target_lc, $target_cc, $options_r
 				$options_ref,
 				$request_ref
 			);
+			return 1;
 		}
-	}
-
-	# Create the maintain_card panel only if we have created at least one sub-panel
-	if (defined $product_ref->{"knowledge_panels_" . $target_lc}{maintenance_advice_fr_epargnonsnosressources}) {
-		create_panel_from_json_template("maintain_card", "api/knowledge-panels/maintain/maintain_card.tt.json",
-			$panel_data_ref, $product_ref, $target_lc, $target_cc, $options_ref, $request_ref);
-		return 1;
 	}
 
 	return 0;
