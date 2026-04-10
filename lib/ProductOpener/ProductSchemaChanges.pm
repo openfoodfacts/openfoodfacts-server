36c2f910950985fa919eacfa005800dabd8d9caa
Stéphane Gigandet <stephane@openfoodfacts.org>
2026-04-10 16:14:16 +0200

    remove states_hierarchy

--- lib/ProductOpener/ProductSchemaChanges.pm
+++ lib/ProductOpener/ProductSchemaChanges.pm
@@ -1036,7 +1036,7 @@ sub convert_schema_1004_to_1003_refactor_tags ($product_ref) {
 	# Also generate states and states_hierarchy
 	if (defined $product_ref->{states_tags}) {
 		$product_ref->{states_hierarchy} = $product_ref->{states_tags};
-		$product_ref->{$tagtype}
+		$product_ref->{states}
 				= list_taxonomy_tags_in_language($target_lc, $tagtype, $product_ref->{states_tags});
 	}
 
