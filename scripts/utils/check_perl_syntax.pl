9c53778ce6f0ce06b281624f21a0a76d7a7b4276
VaiTon <eyadlorenzo@gmail.com>
2026-05-21 02:00:37 +0200

    Use check_perl_syntax.pl for Perl checks
    Replace make-based compilation with scripts/utils/check_perl_syntax.pl.
    
    Add check_perl_args and check_perl_fast targets and introduce
    CHECK_PERL_EXCLUDES to control excluded files.

--- scripts/utils/check_perl_syntax.pl
+++ scripts/utils/check_perl_syntax.pl
@@ -30,7 +30,7 @@ use POSIX ":sys_wait_h";
 use Term::ANSIColor;
 use Getopt::Long qw(:config pass_through);
 
-# 0. Parse options
+# Parse options
 my $no_fast_fail = 0;
 GetOptions("no-fast-fail" => \$no_fast_fail);
 
@@ -42,84 +42,94 @@ if (!$max_workers) {
 }
 my $check_perl_excludes_file = $ENV{CHECK_PERL_EXCLUDES} || '.check_perl_excludes';
 
-# Load excludes and check for stale entries and format
-my %excludes;
-my $stale_excludes = 0;
-if (-f $check_perl_excludes_file) {
-	print colored("Checking entries in $check_perl_excludes_file\n", "bold white");
-	open my $fh, '<', $check_perl_excludes_file or die "Cannot open $check_perl_excludes_file: $!";
-	while (<$fh>) {
-		chomp;
-		# Skip blank lines and comments
-		next if /^\s*$/ || /^\s*#/;
-
-		if (!-e $_) {
-			print colored("error: ", "bold red") . "Stale exclude (file not found): $_\n";
-			$stale_excludes = 1;
+sub load_excludes {
+	my ($file) = @_;
+	my %excludes;
+	if (-f $file) {
+		open my $fh, '<', $file or die "Cannot open $file: $!";
+		while (<$fh>) {
+			chomp;
+			# Skip blank lines and comments
+			next if /^\s*$/ || /^\s*#/;
+			$excludes{$_} = 1;
 		}
-		$excludes{$_} = 1;
+		close $fh;
 	}
-	close $fh;
-}
-if ($stale_excludes) {
-	exit 1;
+	return %excludes;
 }
 
-# 2. Find files to check using native File::Find
+my %excludes = load_excludes($check_perl_excludes_file);
+
+# Warn if any excludes don't exist
+for my $exclude (keys %excludes) {
+	if (!-e $exclude) {
+		print colored("warning: ", "bold yellow") . "Excluded file not found: $exclude\n";
+	}
+}
+
+# Find files to check using native File::Find
 my @files;
 my @roots = @ARGV;
 if (!@roots) {
 	@roots = ('.');
 }
 
-# Identify explicitly passed files and warn if they are in the excludes list
-my %explicit_files;
-foreach my $arg (@roots) {
-	if (-f $arg) {
-		my $path = $arg;
-		$path =~ s/^\.\///;    # Normalize
-		if ($excludes{$path}) {
-			print colored("warning: ", "bold yellow")
-				. "File '$path' is explicitly specified but is in the excludes list. Skipping.\n";
-		}
-		else {
-			$explicit_files{$path} = 1;
-		}
-	}
+# Normalize "./path" to "path"
+sub normalize_path {
+	my ($path) = @_;
+	$path =~ s/^\.\///;
+	return $path;
 }
 
-find(
-	{
-		wanted => sub {
-			my $path = $File::Find::name;
-			$path =~ s/^\.\///;    # Normalize "./path" to "path"
+# Warn if explicitly specified files are in the excludes list
+my %explicit_files;
+foreach my $arg (@roots) {
+	if (!-f $arg) {
+		next;
+	}
+	my $path = $arg;
+	$path = normalize_path($path);
+	if ($excludes{$path}) {
+		print colored("warning: ", "bold yellow")
+			. "File '$path' is explicitly specified but is in the excludes list. Skipping.\n";
+	}
+	else {
+		$explicit_files{$path} = 1;
+	}
 
-			# If the file was explicitly passed on the command line and not excluded, we always check it
-			if ($explicit_files{$path}) {
-				push @files, $path if $path =~ /\.(pl|pm|t)$/;
-				return;
-			}
+}
 
-			# Prune hidden directories (except .) and obsolete directories
-			if (-d $_) {
-				if (($path ne '.' && $path =~ m{(^|/)\.}) || $path =~ m{(^|/)obsolete($|/)}) {
-					$File::Find::prune = 1;
-					return;
-				}
-			}
-			# Filter for Perl files
-			return unless -f $_;
-			return unless $path =~ /\.(pl|pm|t)$/;
+# Collect files to check, applying excludes and pruning directories
+sub on_wanted {
+	my $path = $File::Find::name;
+	$path = normalize_path($path);
 
-			# Skip explicitly excluded files
-			return if $excludes{$path};
+	# If the file was explicitly passed on the command line and not excluded, we always check it
+	if ($explicit_files{$path}) {
+		push @files, $path if $path =~ /\.(pl|pm|t)$/;
+		return;
+	}
 
-			push @files, $path;
-		},
-		no_chdir => 1,
-	},
-	@roots
-);
+	# Prune hidden directories (except .) and obsolete directories
+	if (-d $_) {
+		if (($path ne '.' && $path =~ m{(^|/)\.}) || $path =~ m{(^|/)obsolete($|/)}) {
+			$File::Find::prune = 1;
+			return;
+		}
+	}
+	# Filter for Perl files
+	return unless -f $_;
+	return unless $path =~ /\.(pl|pm|t)$/;
+
+	# Skip explicitly excluded files
+	return if $excludes{$path};
+
+	push @files, $path;
+	return;
+}
+
+# Run the find with our custom wanted function
+find({wanted => \&on_wanted, no_chdir => 1,}, @roots);
 
 if (!@files) {
 	print colored("No Perl files to check.\n", "bold white");
@@ -127,7 +137,7 @@ if (!@files) {
 }
 
 my $total = scalar @files;
-print colored("Checking Perl syntax of $total files using $max_workers workers ...\n", "bold white");
+print colored("Checking $total files using $max_workers workers ...\n", "bold white");
 
 my %running;    # pid => { file => ..., pipe => ... }
 my @failed;
@@ -136,40 +146,42 @@ my $started = 0;
 my $fast_failing = 0;
 
 sub wait_for_worker {
-	# We use a blocking wait to get any finished child
 	my $pid = wait();
-	if ($pid > 0) {
-		my $job = delete $running{$pid};
-		my $file = $job->{file};
-		my $fh = $job->{pipe};
+	if ($pid <= 0) {
+		return;    # No children
+	}
 
-		# Read any output from the pipe
-		my $output = do {local $/; <$fh>};
-		close $fh;
+	my $job = delete $running{$pid};
+	my $file = $job->{file};
+	my $fh = $job->{pipe};
 
-		if ($? != 0 && !$fast_failing) {
-			# Check if it was a real failure or if we killed it
-			# (If we killed it, $? will indicate a signal)
-			my $was_signaled = $? & 127;
+	# Read any output from the pipe
+	my $output = do {local $/; <$fh>};
+	close $fh;
 
-			if (!$was_signaled) {
-				print "\n" . colored("error: ", "bold red") . "Syntax error in $file:\n";
-				# Indent each line of the output with a tab and color it red
-				my $indented_output = $output;
-				$indented_output =~ s/^/\t/mg;
-				print colored($indented_output, "red") . "\n";
-				push @failed, $file;
+	if ($? != 0 && !$fast_failing) {
+		# Check if it was a real failure or if we killed it
+		# (If we killed it, $? will indicate a signal)
+		my $was_signaled = $? & 127;
 
-				if (!$no_fast_fail) {
-					$fast_failing = 1;
-					print colored("warning: ", "bold yellow")
-						. "Fast-fail enabled, terminating remaining workers... (pass --no-fast-fail to disable)\n";
-					kill 'KILL', keys %running;
-				}
+		if (!$was_signaled) {
+			print "\n" . colored("error: ", "bold red") . "Syntax error in $file:\n";
+			# Indent each line of the output with a tab and color it red
+			my $indented_output = $output;
+			$indented_output =~ s/^/\t/mg;
+			print colored($indented_output, "red") . "\n";
+			push @failed, $file;
+
+			if (!$no_fast_fail) {
+				$fast_failing = 1;
+				print colored("warning: ", "bold yellow")
+					. "Fast-fail enabled, terminating remaining workers... (pass --no-fast-fail to disable)\n";
+				kill 'KILL', keys %running;
 			}
 		}
-		$finished++;
 	}
+	$finished++;
+	return;
 }
 
 # Set up signal handling for graceful termination
@@ -237,7 +249,7 @@ while (keys %running > 0) {
 	wait_for_worker();
 }
 
-print "\n";
+print "\r" . (" " x 80) . "\r";
 
 if (@failed) {
 	print colored("error: ", "bold red") . "Check failed. See above for details.\n";
