1277b291c98a9f25ca670ac903f2517dae7538fd
Stéphane Gigandet <stephane@openfoodfacts.org>
2026-06-12 16:59:30 +0200

    forest footprint 2026 wip

---
+++ lib/ProductOpener/ForestFootprint2026.pm
@@ -0,0 +1,581 @@
+# This file is part of Product Opener.
+#
+# Product Opener
+# Copyright (C) 2011-2026 Association Open Food Facts
+# Contact: contact@openfoodfacts.org
+# Address: 21 rue des Iles, 94100 Saint-Maur des Fossés, France
+#
+# Product Opener is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=head1 NAME
+
+ProductOpener::ForestFootprint2026 - compute the forest footprint of a food product using the 2026 algorithm
+
+=head1 SYNOPSIS
+
+C<ProductOpener::ForestFootprint2026> is used to compute the forest footprint of a food product
+using the new 2026 algorithm based on direct ingredient-to-primary-ingredient mapping.
+
+=head1 DESCRIPTION
+
+The module implements a new forest footprint computation algorithm that differs from the original
+ForestFootprint module. Instead of using soy-based calculations, it uses:
+
+- Direct ingredient-to-ingredient_category mapping with equivalence factors
+- Ingredient_category to primary_ingredient mapping with equivalence factors
+- Origin-based footprint values for each primary ingredient (cocoa, coffee, palm-oil)
+- Label-based risk reduction percentages
+
+The computation formula is:
+    footprint = percent_estimate * equivalence_1 * equivalence_2 * origin_footprint * (risk_factor / 100)
+
+=cut
+
+package ProductOpener::ForestFootprint2026;
+
+use ProductOpener::PerlStandards;
+use Exporter qw< import >;
+
+use Log::Any qw($log);
+
+BEGIN {
+	use vars qw(@ISA @EXPORT_OK %EXPORT_TAGS);
+	@EXPORT_OK = qw(
+
+		&load_forest_footprint_2026_data
+		&compute_forest_footprint_2026
+		&get_forest_footprint_2026_ingredient_footprint
+
+	);    # symbols to export on request
+	%EXPORT_TAGS = (all => [@EXPORT_OK]);
+}
+
+use vars @EXPORT_OK;
+
+use ProductOpener::Config qw/:all/;
+use ProductOpener::Tags qw/:all/;
+use ProductOpener::ProductsTags qw/:all/;
+
+use Text::CSV();
+
+my %forest_footprint_2026_data = (
+	ingredients => {},
+	ingredient_categories => {},
+	primary_ingredients => {},
+	labels_risk => {},
+	origins_footprint => {},
+);
+
+=head1 FUNCTIONS
+
+=head2 load_forest_footprint_2026_data ()
+
+Loads data needed to compute the forest footprint using the 2026 algorithm.
+
+=cut
+
+sub load_forest_footprint_2026_data() {
+
+	my $errors = 0;
+
+	my $data_dir = "$data_root/external-data/forest-footprint/2026";
+
+	load_ingredients_equivalence($data_dir);
+	load_ingredient_categories_equivalence($data_dir);
+	load_labels_risk($data_dir);
+	load_origins_footprint($data_dir);
+
+	$log->info("loaded forest footprint 2026 data",
+		{ingredients => scalar(keys %{$forest_footprint_2026_data{ingredients}}),
+			ingredient_categories => scalar(keys %{$forest_footprint_2026_data{ingredient_categories}}),
+			labels_risk => scalar(keys %{$forest_footprint_2026_data{labels_risk}}),
+			origins_footprint => scalar(keys %{$forest_footprint_2026_data{origins_footprint}}),})
+		if $log->is_info();
+
+	return;
+}
+
+sub load_ingredients_equivalence ($) {
+
+	my ($data_dir) = @_;
+
+	# Try loading populated file first, then fall back to original
+	my $tsv_file = "$data_dir/ingredient.ingredient_category.equivalence.populated.tsv";
+	if (not -e $tsv_file) {
+		$tsv_file = "$data_dir/ingredient.ingredient_category.equivalence.tsv";
+	}
+
+	$log->debug("opening ingredients equivalence TSV file", {file => $tsv_file})
+		if $log->is_debug();
+
+	my $csv_options_ref = {binary => 1, sep_char => "\t"};
+	my $csv = Text::CSV->new($csv_options_ref)
+		or die("Cannot use CSV: " . Text::CSV->error_diag());
+
+	if (open(my $io, "<:encoding(UTF-8)", $tsv_file)) {
+
+		my $header_ref = $csv->getline($io);
+		$csv->column_names(@$header_ref);
+
+		while (my $row_ref = $csv->getline_hr($io)) {
+
+			next if not defined $row_ref->{ingredient_fr};
+			next if $row_ref->{ingredient_fr} eq "";
+
+			my $ingredient_id = $row_ref->{ingredient_id};
+			my $ingredient_fr = $row_ref->{ingredient_fr};
+			my $ingredient_category_id = $row_ref->{ingredient_category_id};
+			my $equivalence = $row_ref->{equivalence} + 0;
+
+			if ($ingredient_id eq "") {
+				$log->warn("ingredient_fr has no ingredient_id", {ingredient_fr => $ingredient_fr})
+					if $log->is_warn();
+				next;
+			}
+
+			$forest_footprint_2026_data{ingredients}{$ingredient_id} = {
+				ingredient_fr => $ingredient_fr,
+				ingredient_category_id => $ingredient_category_id,
+				equivalence => $equivalence,
+			};
+		}
+
+		close($io);
+	}
+	else {
+		die("Could not open ingredients equivalence TSV $tsv_file: $!");
+	}
+
+	return;
+}
+
+sub load_ingredient_categories_equivalence ($) {
+
+	my ($data_dir) = @_;
+
+	my $tsv_file = "$data_dir/ingredient_category.primary_ingredient.equivalence.populated.tsv";
+	if (not -e $tsv_file) {
+		$tsv_file = "$data_dir/ingredient_category.primary_ingredient.equivalence.tsv";
+	}
+
+	$log->debug("opening ingredient categories equivalence TSV file", {file => $tsv_file})
+		if $log->is_debug();
+
+	my $csv_options_ref = {binary => 1, sep_char => "\t"};
+	my $csv = Text::CSV->new($csv_options_ref)
+		or die("Cannot use CSV: " . Text::CSV->error_diag());
+
+	if (open(my $io, "<:encoding(UTF-8)", $tsv_file)) {
+
+		my $header_ref = $csv->getline($io);
+		$csv->column_names(@$header_ref);
+
+		while (my $row_ref = $csv->getline_hr($io)) {
+
+			next if not defined $row_ref->{ingredient_category_fr};
+			next if $row_ref->{ingredient_category_fr} eq "";
+
+			my $ingredient_category_id = $row_ref->{ingredient_category_id};
+			my $primary_ingredient_id = $row_ref->{primary_ingredient_id};
+			my $equivalence = $row_ref->{equivalence} + 0;
+
+			if ($ingredient_category_id eq "") {
+				$log->warn("ingredient_category_fr has no ingredient_category_id",
+					{ingredient_category_fr => $row_ref->{ingredient_category_fr}})
+					if $log->is_warn();
+				next;
+			}
+
+			$forest_footprint_2026_data{ingredient_categories}{$ingredient_category_id} = {
+				primary_ingredient_id => $primary_ingredient_id,
+				equivalence => $equivalence,
+			};
+		}
+
+		close($io);
+	}
+	else {
+		die("Could not open ingredient categories equivalence TSV $tsv_file: $!");
+	}
+
+	return;
+}
+
+sub load_labels_risk ($) {
+
+	my ($data_dir) = @_;
+
+	my $tsv_file = "$data_dir/label.primary_ingredient.risk.populated.tsv";
+	if (not -e $tsv_file) {
+		$tsv_file = "$data_dir/label.primary_ingredient.risk.tsv";
+	}
+
+	$log->debug("opening labels risk TSV file", {file => $tsv_file})
+		if $log->is_debug();
+
+	my $csv_options_ref = {binary => 1, sep_char => "\t"};
+	my $csv = Text::CSV->new($csv_options_ref)
+		or die("Cannot use CSV: " . Text::CSV->error_diag());
+
+	if (open(my $io, "<:encoding(UTF-8)", $tsv_file)) {
+
+		my $header_ref = $csv->getline($io);
+		$csv->column_names(@$header_ref);
+
+		while (my $row_ref = $csv->getline_hr($io)) {
+
+			next if not defined $row_ref->{label_fr};
+			next if $row_ref->{label_fr} eq "";
+
+			my $label_id = $row_ref->{label_id};
+
+			if ($label_id eq "") {
+				$log->warn("label_fr has no label_id", {label_fr => $row_ref->{label_fr}})
+					if $log->is_warn();
+				next;
+			}
+
+			$forest_footprint_2026_data{labels_risk}{$label_id} = {
+				cocoa => ($row_ref->{"cocoa.footprint"} // 100) + 0,
+				coffee => ($row_ref->{"coffee.footprint"} // 100) + 0,
+				"palm-oil" => ($row_ref->{"palm-oil.footprint"} // 100) + 0,
+			};
+		}
+
+		close($io);
+	}
+	else {
+		die("Could not open labels risk TSV $tsv_file: $!");
+	}
+
+	return;
+}
+
+sub load_origins_footprint ($) {
+
+	my ($data_dir) = @_;
+
+	my $tsv_file = "$data_dir/origin.primary_ingredient.footprint.populated.tsv";
+	if (not -e $tsv_file) {
+		$tsv_file = "$data_dir/origin.primary_ingredient.footprint.tsv";
+	}
+
+	$log->debug("opening origins footprint TSV file", {file => $tsv_file})
+		if $log->is_debug();
+
+	my $csv_options_ref = {binary => 1, sep_char => "\t"};
+	my $csv = Text::CSV->new($csv_options_ref)
+		or die("Cannot use CSV: " . Text::CSV->error_diag());
+
+	if (open(my $io, "<:encoding(UTF-8)", $tsv_file)) {
+
+		my $header_ref = $csv->getline($io);
+		$csv->column_names(@$header_ref);
+
+		while (my $row_ref = $csv->getline_hr($io)) {
+
+			next if not defined $row_ref->{origin_fr};
+			next if $row_ref->{origin_fr} eq "";
+
+			my $origin_id = $row_ref->{origin_id};
+
+			if ($origin_id eq "") {
+				$log->warn("origin_fr has no origin_id", {origin_fr => $row_ref->{origin_fr}})
+					if $log->is_warn();
+				next;
+			}
+
+			$forest_footprint_2026_data{origins_footprint}{$origin_id} = {
+				cocoa => ($row_ref->{"cocoa.footprint"} // 0) + 0,
+				coffee => ($row_ref->{"coffee.footprint"} // 0) + 0,
+				"palm-oil" => ($row_ref->{"palm-oil.footprint"} // 0) + 0,
+			};
+		}
+
+		close($io);
+	}
+	else {
+		die("Could not open origins footprint TSV $tsv_file: $!");
+	}
+
+	return;
+}
+
+=head2 compute_forest_footprint_2026 ( $product_ref )
+
+C<compute_forest_footprint_2026()> computes the forest footprint of a food product using the 2026 algorithm,
+and stores the details of the computation.
+
+=head3 Arguments
+
+=head4 Product reference $product_ref
+
+=head3 Return values
+
+The forest footprint and computations details are stored in the product reference passed as input parameter.
+
+Returned values:
+
+- forest_footprint_2026 : forest footprint computation details
+
+=cut
+
+sub compute_forest_footprint_2026 ($product_ref) {
+
+	$product_ref->{forest_footprint_2026} = {ingredients => [],};
+
+	if (defined $product_ref->{ingredients}) {
+		$product_ref->{forest_footprint_2026}{ingredients} = [];
+		compute_footprints_of_ingredients_2026(
+			$product_ref,
+			$product_ref->{forest_footprint_2026}{ingredients},
+			$product_ref->{ingredients}
+		);
+	}
+
+	if (scalar(@{$product_ref->{forest_footprint_2026}{ingredients}}) > 0) {
+		$product_ref->{forest_footprint_2026}{total_footprint_per_kg} = 0;
+		foreach my $ingredient_ref (@{$product_ref->{forest_footprint_2026}{ingredients}}) {
+			$product_ref->{forest_footprint_2026}{total_footprint_per_kg} += $ingredient_ref->{footprint_per_kg};
+		}
+	}
+	else {
+		delete $product_ref->{forest_footprint_2026};
+	}
+
+	return;
+}
+
+sub compute_footprints_of_ingredients_2026 ($product_ref, $footprints_ref, $ingredients_ref) {
+
+	my $ranked = 0;
+
+	my $ingredients_with_footprint = 0;
+
+	foreach my $ingredient_ref (@$ingredients_ref) {
+
+		if ($ingredient_ref->{rank}) {
+			$ranked = 1;
+		}
+		elsif ($ranked) {
+			last;
+		}
+
+		$log->debug("compute_footprints_of_ingredients_2026 - checking ingredient",
+			{ingredient_id => $ingredient_ref->{id}})
+			if $log->is_debug();
+
+		if (defined $ingredient_ref->{ingredients}) {
+			$log->debug("compute_footprints_of_ingredients_2026 - ingredient has subingredients",
+				{ingredient_id => $ingredient_ref->{id}})
+				if $log->is_debug();
+			my $sub_ingredients_with_footprint
+				= compute_footprints_of_ingredients_2026($product_ref, $footprints_ref, $ingredient_ref->{ingredients});
+			if ($sub_ingredients_with_footprint > 0) {
+				$ingredients_with_footprint += 1;
+				next;
+			}
+		}
+
+		my $ingredient_footprint_ref = get_forest_footprint_2026_ingredient_footprint($product_ref, $ingredient_ref);
+
+		if (defined $ingredient_footprint_ref) {
+			push @$footprints_ref, $ingredient_footprint_ref;
+			$ingredients_with_footprint += 1;
+		}
+	}
+
+	return $ingredients_with_footprint;
+}
+
+=head2 get_forest_footprint_2026_ingredient_footprint ( $product_ref, $ingredient_ref )
+
+C<get_forest_footprint_2026_ingredient_footprint()> computes the forest footprint for a single ingredient.
+
+=head3 Arguments
+
+=head4 Product reference $product_ref
+
+Used to determine labels and origins for risk factor.
+
+=head4 Ingredient reference $ingredient_ref
+
+The ingredient to compute the footprint for.
+
+=head3 Return values
+
+Returns a reference to a hash containing the footprint details, or undef if the ingredient
+does not have a forest footprint.
+
+=cut
+
+sub get_forest_footprint_2026_ingredient_footprint {
+
+	my ($product_ref, $ingredient_ref) = @_;
+
+	my $ingredient_id = $ingredient_ref->{id};
+
+	$log->debug("get_forest_footprint_2026_ingredient_footprint - checking ingredient",
+		{ingredient_id => $ingredient_id})
+		if $log->is_debug();
+
+	if (not defined $ingredient_id) {
+		return undef;
+	}
+
+	if (not exists $forest_footprint_2026_data{ingredients}{$ingredient_id}) {
+		$log->debug("ingredient not in forest footprint 2026 data",
+			{ingredient_id => $ingredient_id})
+			if $log->is_debug();
+		return undef;
+	}
+
+	my $ingredient_data = $forest_footprint_2026_data{ingredients}{$ingredient_id};
+
+	my $ingredient_category_id = $ingredient_data->{ingredient_category_id};
+	my $equivalence_1 = $ingredient_data->{equivalence};
+
+	if (not exists $forest_footprint_2026_data{ingredient_categories}{$ingredient_category_id}) {
+		$log->debug("ingredient category not in forest footprint 2026 data",
+			{ingredient_category_id => $ingredient_category_id})
+			if $log->is_debug();
+		return undef;
+	}
+
+	my $category_data = $forest_footprint_2026_data{ingredient_categories}{$ingredient_category_id};
+
+	my $primary_ingredient_id = $category_data->{primary_ingredient_id};
+	my $equivalence_2 = $category_data->{equivalence};
+
+	my $primary_ingredient = get_primary_ingredient_name($primary_ingredient_id);
+
+	$log->debug("found primary ingredient",
+		{ingredient_id => $ingredient_id,
+			ingredient_category_id => $ingredient_category_id,
+			primary_ingredient_id => $primary_ingredient_id,
+			primary_ingredient => $primary_ingredient,
+			equivalence_1 => $equivalence_1,
+			equivalence_2 => $equivalence_2,})
+		if $log->is_debug();
+
+	my $origin_footprint = get_origin_footprint($product_ref, $primary_ingredient);
+
+	if (not defined $origin_footprint) {
+		$log->debug("no origin footprint found", {primary_ingredient => $primary_ingredient})
+			if $log->is_debug();
+		return undef;
+	}
+
+	my $risk_factor = get_label_risk($product_ref, $primary_ingredient);
+
+	my $percent = $ingredient_ref->{percent};
+	if (not defined $percent) {
+		$percent = $ingredient_ref->{percent_estimate};
+	}
+	if (not defined $percent) {
+		$percent = 100;
+	}
+
+	my $footprint_per_kg = ($percent / 100) * $equivalence_1 * $equivalence_2 * $origin_footprint * $risk_factor;
+
+	$log->debug("computed footprint",
+		{percent => $percent,
+			equivalence_1 => $equivalence_1,
+			equivalence_2 => $equivalence_2,
+			origin_footprint => $origin_footprint,
+			risk_factor => $risk_factor,
+			footprint_per_kg => $footprint_per_kg,})
+		if $log->is_debug();
+
+	my $footprint_ref = {
+		ingredient_id => $ingredient_id,
+		ingredient_category_id => $ingredient_category_id,
+		primary_ingredient_id => $primary_ingredient_id,
+		percent => $percent,
+		equivalence_1 => $equivalence_1,
+		equivalence_2 => $equivalence_2,
+		origin_footprint => $origin_footprint,
+		risk_factor => $risk_factor,
+		footprint_per_kg => $footprint_per_kg,
+	};
+
+	return $footprint_ref;
+}
+
+sub get_primary_ingredient_name ($) {
+
+	my ($primary_ingredient_id) = @_;
+
+	if ($primary_ingredient_id =~ /^en:(.+)$/) {
+		my $name = $1;
+		# Map ingredient names to footprint categories
+		if ($name eq 'cacao') {
+			return 'cocoa';  # cacao -> cocoa for footprint lookup
+		}
+		return $name;
+	}
+
+	return $primary_ingredient_id;
+}
+
+sub get_origin_footprint {
+
+	my ($product_ref, $primary_ingredient) = @_;
+
+	my $origins_tags = $product_ref->{origins_tags};
+
+	if (not defined $origins_tags) {
+		return 0;
+	}
+
+	foreach my $origin_tag (@$origins_tags) {
+		if (exists $forest_footprint_2026_data{origins_footprint}{$origin_tag}) {
+			my $origin_data = $forest_footprint_2026_data{origins_footprint}{$origin_tag};
+			if (defined $origin_data->{$primary_ingredient}) {
+				return $origin_data->{$primary_ingredient};
+			}
+		}
+	}
+
+	return 0;
+}
+
+sub get_label_risk {
+
+	my ($product_ref, $primary_ingredient) = @_;
+
+	my $labels_tags = $product_ref->{labels_tags};
+
+	if (not defined $labels_tags) {
+		return 1;
+	}
+
+	my $risk_factor = 1;
+
+	foreach my $label_tag (@$labels_tags) {
+		if (exists $forest_footprint_2026_data{labels_risk}{$label_tag}) {
+			my $label_data = $forest_footprint_2026_data{labels_risk}{$label_tag};
+			if (defined $label_data->{$primary_ingredient}) {
+				my $label_risk = $label_data->{$primary_ingredient} / 100;
+				if ($label_risk < $risk_factor) {
+					$risk_factor = $label_risk;
+				}
+			}
+		}
+	}
+
+	return $risk_factor;
+}
+
+1;
---
+++ tests/unit/forest_footprint_2026.t
@@ -0,0 +1,178 @@
+#!/usr/bin/perl -w
+
+use Modern::Perl '2017';
+use utf8;
+
+use Test2::V0;
+use Data::Dumper;
+$Data::Dumper::Terse = 1;
+use Log::Any::Adapter 'TAP';
+
+use JSON;
+
+use ProductOpener::Config qw/:all/;
+use ProductOpener::Tags qw/:all/;
+use ProductOpener::Test qw/init_expected_results/;
+use ProductOpener::Ingredients qw/extract_ingredients_from_text/;
+use ProductOpener::ForestFootprint2026 qw/compute_forest_footprint_2026 load_forest_footprint_2026_data/;
+
+my ($test_id, $test_dir, $expected_result_dir, $update_expected_results) = (init_expected_results(__FILE__));
+
+load_forest_footprint_2026_data();
+
+my @tests = (
+
+	[
+		'empty-product',
+		{
+			lc => "en",
+		}
+	],
+	[
+		'fr-ingredients-cacao',
+		{
+			lc => "fr",
+			ingredients_text => "cacao",
+		}
+	],
+	[
+		'fr-ingredients-cacao-50-percent',
+		{
+			lc => "fr",
+			ingredients => [
+				{
+					id => "en:cacao",
+					percent => 50,
+					text => "cacao",
+				}
+			],
+		}
+	],
+	[
+		'fr-ingredients-cacao-with-origin',
+		{
+			lc => "fr",
+			ingredients => [
+				{
+					id => "en:cacao",
+					percent => 50,
+					text => "cacao",
+				}
+			],
+			origins_tags => ["en:brazil"],
+		}
+	],
+	[
+		'fr-ingredients-cacao-with-origin-and-label',
+		{
+			lc => "fr",
+			ingredients => [
+				{
+					id => "en:cacao",
+					percent => 50,
+					text => "cacao",
+				}
+			],
+			origins_tags => ["en:brazil"],
+			labels_tags => ["en:organic"],
+		}
+	],
+	[
+		'fr-ingredients-cacao-100-percent',
+		{
+			lc => "fr",
+			ingredients => [
+				{
+					id => "en:cacao",
+					percent => 100,
+					text => "cacao",
+				}
+			],
+			origins_tags => ["en:brazil"],
+			labels_tags => ["en:organic"],
+		}
+	],
+	[
+		'fr-ingredients-pate-de-cacao',
+		{
+			lc => "fr",
+			ingredients => [
+				{
+					id => "en:pate-de-cacao",
+					percent => 100,
+					text => "pâte de cacao",
+				}
+			],
+			origins_tags => ["en:brazil"],
+		}
+	],
+	[
+		'fr-ingredients-beurre-de-cacao',
+		{
+			lc => "fr",
+			ingredients => [
+				{
+					id => "en:beurre-de-cacao",
+					percent => 100,
+					text => "beurre de cacao",
+				}
+			],
+			origins_tags => ["en:brazil"],
+		}
+	],
+	[
+		'fr-ingredients-chocolate-with-utz-label',
+		{
+			lc => "fr",
+			ingredients => [
+				{
+					id => "en:cacao",
+					percent => 50,
+					text => "cacao",
+				}
+			],
+			origins_tags => ["en:brazil"],
+			labels_tags => ["en:utz"],
+		}
+	],
+
+);
+
+my $json = JSON->new->allow_nonref->canonical;
+
+foreach my $test_ref (@tests) {
+
+	my $testid = $test_ref->[0];
+	my $product_ref = $test_ref->[1];
+
+	# Run the test
+	if (defined $product_ref->{ingredients_text}) {
+		extract_ingredients_from_text($product_ref);
+	}
+
+	compute_forest_footprint_2026($product_ref);
+
+	# Save the result
+
+	if ($update_expected_results) {
+		open(my $result, ">:encoding(UTF-8)", "$expected_result_dir/$testid.json")
+			or die("Could not create $expected_result_dir/$testid.json: $!\n");
+		print $result $json->pretty->encode($product_ref);
+		close($result);
+	}
+
+	# Compare the result with the expected result
+
+	if (open(my $expected_result, "<:encoding(UTF-8)", "$expected_result_dir/$testid.json")) {
+
+		local $/;    #Enable 'slurp' mode
+		my $expected_product_ref = $json->decode(<$expected_result>);
+		is($product_ref, $expected_product_ref) or diag Dumper $product_ref;
+	}
+	else {
+		fail("could not load $expected_result_dir/$testid.json");
+		diag Dumper $product_ref;
+	}
+}
+
+done_testing();
