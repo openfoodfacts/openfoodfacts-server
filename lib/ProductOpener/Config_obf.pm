c0fb147bacd16a8dd73a61d793acdde2491496de
Pierre Slamich <pierre@openfoodfacts.org>
2026-06-23 08:53:21 +0200

    fix: Update app links in Config_obf.pm

--- lib/ProductOpener/Config_obf.pm
+++ lib/ProductOpener/Config_obf.pm
@@ -188,11 +188,11 @@ $flavor = 'obf';
 	site_name => "Open Beauty Facts",
 	product_type => "beauty",
 	og_image_url => "https://static.openbeautyfacts.org/images/logos/obf-logo-vertical-white-social-media-preview.png",
-	android_apk_app_link => "https://github.com/openfoodfacts/smooth-app/releases?utm_source=obf&utm_medium=web",
+	android_apk_app_link => "https://github.com/openfoodfacts/smooth-app/releases",
 	android_app_link =>
-		"https://play.google.com/store/apps/details?id=org.openfoodfacts.scanner&utm_source=obf&utm_medium=web",
-	ios_app_link => "https://apps.apple.com/app/open-food-facts-product-scan/id588797948?utm_source=obf&utm_medium=web",
-	facebook_page_url => "https://www.facebook.com/openfoodfacts?utm_source=obf&utm_medium=web",
+		"https://play.google.com/store/apps/details?id=org.openfoodfacts.scanner",
+	ios_app_link => "https://apps.apple.com/app/open-food-facts-product-scan/id588797948",
+	facebook_page_url => "https://www.facebook.com/openfoodfacts",
 	x_account => "OpenFoodFacts",
 	# favicon HTML and images generated with https://realfavicongenerator.net/ using the SVG icon
 	favicons => <<HTML
