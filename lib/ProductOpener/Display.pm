af53ceda5bfe7b4374ac0a475bfa3263644b2683
pranavchoudhary-tech <pranav.nstblr@gmail.com>
2026-07-31 14:20:28 +0530

    fix: satisfy perltidy for Display.pm

--- lib/ProductOpener/Display.pm
+++ lib/ProductOpener/Display.pm
@@ -9555,7 +9555,8 @@ CSS
 							if (($formatted_value . ' ') =~ /e/) {
 								# use %f (outputs extras 0 in the general case)
 								$formatted_value = sprintf("%f", $value);
-							} else {
+							}
+							else {
 								# Round numeric values to 1 decimal place to avoid 
 								# ugly calculated values like 16.6666666667 (Issue #14035)
 								if ($formatted_value =~ /^-?\d+\.\d{3,}$/) {
