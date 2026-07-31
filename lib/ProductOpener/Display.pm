a06b103481a7ccdca1433d589189d22bab6aec95
pranavchoudhary-tech <pranav.nstblr@gmail.com>
2026-07-31 22:23:53 +0530

    fix: restore single line for perltidy

--- lib/ProductOpener/Display.pm
+++ lib/ProductOpener/Display.pm
@@ -9570,10 +9570,7 @@ CSS
 										}
 									}
 									require ProductOpener::Numbers;
-									my $rounded =
-										ProductOpener::Numbers::round_to_max_decimal_places(
-											$formatted_value, $decimals
-										);
+									my $rounded = ProductOpener::Numbers::round_to_max_decimal_places($formatted_value, $decimals);
 									$formatted_value = $rounded // $formatted_value;
 								}
 							}
