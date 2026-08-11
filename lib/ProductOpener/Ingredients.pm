fc825b09b009673685187fcca694b649953f57ca
Stéphane Gigandet <stephane@openfoodfacts.org>
2026-08-11 10:50:31 +0200

    small fixes for */ and **/

--- lib/ProductOpener/Ingredients.pm
+++ lib/ProductOpener/Ingredients.pm
@@ -6966,7 +6966,9 @@ sub preparse_ingredients_text ($ingredients_lc, $text) {
 		foreach my $symbol (@symbols) {
 			# Find the last occurence of the symbol or symbol in parenthesis:  * (*)
 			# we need a negative look ahead (?!\)) to make sure we match (*) completely (otherwise we would match *)
-			if ($text =~ /^(.*)(\($symbol\)|$symbol)(?!\))\s*(:|=|\/)?\s*/i) {
+			# we use (\b|\s) before and after the symbol, to make sure we don't match a symbol that is part of another symbol
+			# e.g. * in **
+			if ($text =~ /^(.*)(\b|\s)(\($symbol\)|$symbol)(?!\))\s*(:|=|\/)?\s*(\b|\s)/i) {
 				my $after = $';
 				#print STDERR "symbol: $symbol - after: $after\n";
 				foreach my $labelid (@labels) {
@@ -6978,9 +6980,12 @@ sub preparse_ingredients_text ($ingredients_lc, $text) {
 						if ($after =~ /^($regexp)\b\s*(\([^\)]+\))?\s*\.?\s*/i) {
 							my $label = $1;
 							$text
-								=~ s/^(.*)(\($symbol\)|$symbol)(?!\))\s?(:|=)?\s?$label\s*(\([^\)]+\))?\s*\.?\s*/$1 /i;
+								=~ s/^(.*)(\b|\s)(\($symbol\)|$symbol)(?!\))\s*(:|=|\/)?\s*(\b|\s)$label\s*(\([^\)]+\))?\s*\.?\s*/$1 /i;
 							my $ingredients_lc_label = display_taxonomy_tag($ingredients_lc, "labels", $labelid);
-							$text =~ s/$symbol/ $ingredients_lc_label /g;
+							# Use (\b|\s|[^*°¹²]) before and after the label so that we don't replace ** by two times the value of *
+							# We try to match ** first, but if the corresponding label is not found, the ** line will remain in the ingredients
+							$text =~ s/(\b|\s|[^*°¹²])$symbol(\b|\s|[^*°¹²])/$1 $ingredients_lc_label $2/g;
+							#print STDERR "found label for symbol $symbol: $labelid - new text: $text\n";
 							last;
 						}
 					}
--- tests/unit/ingredients.t
+++ tests/unit/ingredients.t
@@ -958,16 +958,6 @@ puffed orange and caramelized unknown_fruit4.",
 		}
 
 	],
-	# handling of */ and **/ in the tail of the ingredients list
-	[
-		# https://se.openfoodfacts.org/product/7350056848709/%C3%B6rtsalt-original-spicemaster
-		'sv-asterisk-slash',
-		{
-			lc => 'sv',
-			ingredients_text =>
-				'Havssalt (93%)**, basilika*, timjan*, rosmarin*, lök*, salvia, oregano* och vitlök* */ ekologiskt odlat **/oraffinerat havssalt med låg natriumhalt',
-		},
-	],
 	# Recipes with ingredients by weight and volume
 	[
 		'en-ingredients-with-a-specific-density',
@@ -991,7 +981,25 @@ puffed orange and caramelized unknown_fruit4.",
 			lc => 'en',
 			ingredients_text => "cod 40g, salmon 30%, 20% tuna, mackerel (7%), 3g sardine",
 		}
-	]
+	],
+	# handling of */ and **/ in the tail of the ingredients list
+	[
+		# https://se.openfoodfacts.org/product/7350056848709/%C3%B6rtsalt-original-spicemaster
+		'sv-asterisk-slash',
+		{
+			lc => 'sv',
+			ingredients_text =>
+				'Havssalt (93%)**, basilika*, timjan*, rosmarin*, lök*, salvia, oregano* och vitlök* */ ekologiskt odlat **/oraffinerat havssalt med låg natriumhalt',
+		},
+	],
+	[
+		'en-asterisk-slash',
+		{
+			lc => 'en',
+			ingredients_text =>
+				'Sea salt (93%)**, basil*, thyme*, rosemary*, onion*, sage, oregano* and garlic* */ organically grown **/fair trade',
+		},
+	],
 );
 
 foreach my $test_ref (@tests) {
