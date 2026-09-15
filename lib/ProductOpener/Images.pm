e83e6c3037ac857b99bb05033edf78ea98e2c857
Surbhi Agarwal <SurbhiAgarwal1@users.noreply.github.com>
2026-09-15 19:30:26 +0530

    style: fix perltidy assertion formatting in Images.pm
    Signed-off-by: Surbhi Agarwal <SurbhiAgarwal1@users.noreply.github.com>

--- lib/ProductOpener/Images.pm
+++ lib/ProductOpener/Images.pm
@@ -1400,7 +1400,7 @@ sub process_image_move ($user_id, $code, $imgids, $move_to, $ownerid) {
 	defined $product_ref->{images} or $product_ref->{images} = {};
 
 	# Check permission if user is not a moderator or admin
-	if (not ($User{moderator} || $User{admin} || is_admin_user($user_id))) {
+	if (not($User{moderator} || $User{admin} || is_admin_user($user_id))) {
 		if ($move_to ne "trash") {
 			return "You must be a moderator to move images to another product.";
 		}
