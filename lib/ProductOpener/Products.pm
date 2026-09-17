792ec702fa2b901d052337fd45bfa8cffb86e2fc
John Gomersall <thegoms@gmail.com>
2026-09-17 16:39:36 +0100

    fix: Unsure product_updates are pushed to Redis when a user is deleted
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- lib/ProductOpener/Products.pm
+++ lib/ProductOpener/Products.pm
@@ -2294,9 +2294,13 @@ sub replace_user_id_in_product ($product_id, $user_id, $new_user_id, $products_c
 		$most_recent_product_ref = $product_ref;
 	}
 
-	if ((defined $most_recent_product_ref) and (not $most_recent_product_ref->{deleted})) {
+	if (defined $most_recent_product_ref) {
+		if (not $most_recent_product_ref->{deleted}) {
 		$products_collection->replace_one({"_id" => $most_recent_product_ref->{_id}},
 			$most_recent_product_ref, {upsert => 1});
+		}
+		# Always push the update to Redis, even if the product was deleted, so that the change is reflected in off-query
+		push_product_update_to_redis($most_recent_product_ref, $changes_ref->[-1], "reprocessed");
 	}
 
 	store_object("$BASE_DIRS{PRODUCTS}/$path/changes", $changes_ref);
