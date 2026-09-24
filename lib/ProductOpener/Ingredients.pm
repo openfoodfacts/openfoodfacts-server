50d7bea88801f616b035d5d348f5e02d025ed2c6
Stéphane Gigandet <stephane@openfoodfacts.org>
2026-09-24 17:05:05 +0200

    fix: german regexp for origins

--- lib/ProductOpener/Ingredients.pm
+++ lib/ProductOpener/Ingredients.pm
@@ -1068,7 +1068,7 @@ sub match_origin_of_the_ingredient_origin ($ingredients_lc, $text_ref, $matched_
 		ca => "(?:origen)",
 		cs => "(?:země původu)",
 		da => "(?:oprindelse)",
-		de => "(?:ursprungsland)?|herkunftsland)?)",
+		de => "(?:ursprungsland|herkunftsland)",
 		el => "(?:χώρα προέλευσης|προέλευση)",
 		es => "(?:origen)",
 		et => "(?:päritolu(?:riik)?)",
